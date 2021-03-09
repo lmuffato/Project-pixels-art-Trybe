c4c024c19bac9f2c3a8ab216b1f35ad567949a6b
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2021-03-09 18:20:56 -0300

    requisito 2 feito

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-    let palete = document.getElementsByClassName('color')[0];
+    let palette = document.getElementsByClassName('color');
+    
+
+    function GenerateColor(){
+    let red = parseInt(Math.random * 255);
+    let green = parseInt(Math.random * 255);
+    let blue = parseInt(Math.random * 255);
+    let color = (red, green, blue);
+}
