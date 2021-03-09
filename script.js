c0133153eff4f6749de851a35616787cea9c9a77
William Willers <w.willers@outlook.com>
2021-03-09 17:03:05 -0400

    Requisitos 4 e 5

--- script.js
+++ script.js
@@ -5,4 +5,22 @@ function setColorPallet() {
   arrayColor[2].style.backgroundColor = 'green';
   arrayColor[3].style.backgroundColor = 'blue';
 }
+
+function generatePixelFrame(rows = 5, columns = 5) {
+  const pixelBoard = document.getElementById('pixel-board');
+
+  for (let index = 0; index < rows; index += 1) {
+    const tableRow = document.createElement('tr');
+    for (let data = 0; data < columns; data += 1) {
+      const cell = document.createElement('td');
+      cell.className = 'pixel';
+      cell.style.backgroundColor = 'white';
+      tableRow.appendChild(cell);
+    }
+    pixelBoard.appendChild(tableRow);
+  }
+  pixelBoard.style.border = 'solid black 1px';
+}
+
 setColorPallet();
+generatePixelFrame();
