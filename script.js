2fbc491a29f5799f4c2eff936c9cccda4220782f
mallmann02 <leomallmann020302@gmail.com>
2021-03-09 18:54:38 -0300

    Editing script file to fit lint patterns

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-let colorsArray = ['red', 'blue', 'green', 'magenta'];
+const colorsArray = ['red', 'blue', 'green', 'magenta'];
 
-let colorPaletteContainers = document.querySelectorAll('.color');
-
-colorPaletteContainers.forEach(addColorsInPalette);
+const colorPaletteContainers = document.querySelectorAll('.color');
 
 function addColorsInPalette() {
   for (let index = 0; index < colorPaletteContainers.length; index += 1) {
@@ -12,19 +10,21 @@ function addColorsInPalette() {
   colorPaletteContainers[0].style.background = 'black';
 }
 
-for (let index = 0; index < 25; index += 1) {
-  document.querySelector('#pixel-board').appendChild(createPixelInBoard())
-}
-
 function createPixelInBoard() {
-  let pixel = document.createElement('container');
+  const pixel = document.createElement('container');
   pixel.className = 'pixel';
   return pixel;
 }
 
-colorPaletteContainers.forEach(el => el.addEventListener('click', e => selectNewElement(e.target)));
-
 function selectNewElement(newEl) {
-  colorPaletteContainers.forEach(el => el.classList.remove('selected'));
+  colorPaletteContainers.forEach((el) => el.classList.remove('selected'));
   newEl.classList.toggle('selected');
 }
+
+colorPaletteContainers.forEach(addColorsInPalette);
+
+for (let index = 0; index < 25; index += 1) {
+  document.querySelector('#pixel-board').appendChild(createPixelInBoard());
+}
+
+colorPaletteContainers.forEach((el) => el.addEventListener('click', (e) => selectNewElement(e.target)));
