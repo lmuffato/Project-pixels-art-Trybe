17d7cca4e05a046d3ec2ace43551a2b2b1a3f2ce
Flavio Franco <flavio.win@gmail.com>
2021-03-09 16:29:33 -0300

    Requisito 3 - atribuindo a primeira paleta cor preta

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 function colorSelected() {
-  let colorBlock = document.querySelectorAll('.color');
-	
+  const colorBlock = document.querySelectorAll('.color');
+
   for (let index = 0; index < colorBlock.length; index += 1) {
     switch (index) {
-			case 0:
-				colorBlock[index].style.backgroundColor = 'red';
-				break;
-			case 1:
-				colorBlock[index].style.backgroundColor = 'black';
-				break;
-			case 2:
-				colorBlock[index].style.backgroundColor = 'yellow';
-				break;
-			case 3:
-				colorBlock[index].style.backgroundColor = 'orange';
-				break;
+    case 0:
+      colorBlock[index].style.backgroundColor = 'black';
+      break;
+    case 1:
+      colorBlock[index].style.backgroundColor = 'red';
+      break;
+    case 2:
+      colorBlock[index].style.backgroundColor = 'yellow';
+      break;
+    default:
+      colorBlock[index].style.backgroundColor = 'orange';
     }
   }
 }
+
 colorSelected();
