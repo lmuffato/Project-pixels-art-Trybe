31923bb330533cafa79de6db436392990f12d5c8
Douglas <douguinhodns@gmail.com>
2021-03-09 09:07:40 -0300

    requisitos 7 e 8 concluidos

--- script.js
+++ script.js
@@ -16,4 +16,31 @@ for (let index = 1; index <= size; index += 1) {
     }
 }
 
+//requisito 6 (ajuda mútua)
 color[0].className += ' selected';
+
+//requisito 7 (ajuda mútua e em especial o Murilo Gonçalves)
+function changeClass () {
+    let elementColors = document.querySelector('#color-palette');
+    elementColors.addEventListener('click', function (e) {
+    let colorElement = e.target;
+    let selectElement = document.querySelector('.selected');
+    selectElement.classList.remove('selected');
+    colorElement.classList.add('selected');
+    })
+}
+changeClass()
+
+//requisito 8 (ajuda mútua e em especial o Murilo Gonçalves)
+function clickColor() {
+    let selectBoard = document.querySelector('#pixel-board');
+    selectBoard.addEventListener('click', function(e) {
+        let pixel = e.target;
+        console.log(e);
+        if (pixel.className === 'pixel') {
+            let colorSelected = document.querySelector('.selected').style.backgroundColor;
+            pixel.style.backgroundColor = colorSelected;
+        }
+    })
+}
+clickColor();
