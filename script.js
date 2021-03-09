00b9b59b898c4f9339d9eda18b8989732467eeed
William Willers <w.willers@outlook.com>
2021-03-09 15:10:25 -0400

    Requisitos 1 ao 3

---
+++ script.js
@@ -0,0 +1,8 @@
+function setColorPallet() {
+  const arrayColor = document.querySelectorAll('.color');
+  arrayColor[0].style.backgroundColor = 'black';
+  arrayColor[1].style.backgroundColor = 'red';
+  arrayColor[2].style.backgroundColor = 'green';
+  arrayColor[3].style.backgroundColor = 'blue';
+}
+setColorPallet();
