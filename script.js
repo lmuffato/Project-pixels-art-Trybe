d938fa4f9075b5da066666396c2a09df9e195b5c
Lucas Brito <lucasbrito.carneiro@gmail.com>
2021-03-09 17:07:51 -0300

    Nova tentativa 3

---
+++ script.js
@@ -0,0 +1,12 @@
+// pratica 3
+function setColor (){
+    let color = document.querySelectorAll('.color')
+        color[0].style.backgroundColor = 'black'
+        color[1].style.backgroundColor = 'red'
+        color[2].style.backgroundColor = 'green'
+        color[3].style.backgroundColor = 'blue'
+}
+setColor();
+
+// pratica 4
+
