d6000c681efed72c4b1736177e49e08d00fc8136
rcalvs <rafaelcalvette@gmail.com>
2021-03-09 16:47:07 -0300

    start do script

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //color[length] para alterar a cor nas paletas
+
+window.onload = function bemVindo() {
+    console.log("oi")
+
+
+
+let palleteColorBasic = ['black', 'red', 'blue', 'green'];
+
+// function palleteColorBasic(){
+//     document.getElementsByID(color)
+
+
+// }
+
+
+
+console.log(palleteColorBasic);
+}
