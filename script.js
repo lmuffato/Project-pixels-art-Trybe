81096cbb88bf0568ea0caf46c6bed873a83ebe3a
mallmann02 <leomallmann020302@gmail.com>
2021-03-09 15:43:40 -0300

    Requisito 3

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ let colorsArray = ['red', 'blue', 'green', 'magenta'];
 
 let colorPaletteContainers = document.querySelectorAll('.color');
 
-colorPaletteContainers.forEach(addColorsInPalette)
+colorPaletteContainers.forEach(addColorsInPalette);
 
 function addColorsInPalette() {
   for (let index = 0; index < colorPaletteContainers.length; index += 1) {
     colorPaletteContainers[index].style.background = colorsArray[index];
   }
+
+  colorPaletteContainers[0].style.background = 'black';
 }
