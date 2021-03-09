1b522e67dc673fb448396a509941398267729775
Guilherme Dugaich de Oliveira <guilherme.dugaich@gmail.com>
2021-03-09 16:01:47 -0300

    Requisitos 6 - pagina carregada com preto com a classe selected

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 window.onload = function () {
-let pixelBoard = document.getElementById('pixel-board');
+  const pixelBoard = document.getElementById('pixel-board');
 
+  // Criando os pixels
   for (let boardRow = 1; boardRow <= 5; boardRow += 1) {
-    let newRow = document.createElement('div');
-    newRow.className = 'pixel-row'
+    const newRow = document.createElement('div');
+    newRow.className = 'pixel-row';
     pixelBoard.appendChild(newRow);
     for (let boardColumn = 1; boardColumn <= 5; boardColumn += 1) {
-      let newPixel = document.createElement('div');
+      const newPixel = document.createElement('div');
       newPixel.className = 'pixel';
       newPixel.style.backgroundColor = 'white';
       newPixel.style.width = '40px';
       newPixel.style.height = '40px';
-      newPixel.style.border = '1px solid black'
+      newPixel.style.border = '1px solid black';
       newPixel.style.display = 'inline-block';
       newRow.appendChild(newPixel);
     }
   }
-}
+
+  document.getElementById('black').classList.add('selected');
+};
