e707b5e6bfa4784a51c6af8dde7344bb8461643c
Eicke Yumoto <eickeyumoto@gmail.com>
2021-03-09 19:44:19 -0300

    Adiciona requisitos 1 a 7

--- script.js
+++ script.js
@@ -1,3 +1,51 @@
-let teste = document.querySelector('.color');
+window.onload = document.querySelector('.color').classList.add('selected');
 
-console.log(teste);
+const selectedColor = document.getElementsByClassName('selected').style.backgroundColor;
+
+const black = document.getElementsByClassName('color')[0];
+const blue = document.getElementsByClassName('color')[1];
+const red = document.getElementsByClassName('color')[2];
+const green = document.getElementsByClassName('color')[3];
+
+function blackSelected() {
+  black.className = 'color selected';
+  blue.className = 'color';
+  red.className = 'color';
+  green.className = 'color';
+}
+
+black.addEventListener('click', blackSelected);
+
+function blueSelected() {
+  black.className = 'color';
+  blue.className = 'color selected';
+  red.className = 'color';
+  green.className = 'color';
+}
+
+blue.addEventListener('click', blueSelected);
+
+function redSelected() {
+  black.className = 'color';
+  blue.className = 'color';
+  red.className = 'color selected';
+  green.className = 'color';
+}
+red.addEventListener('click', redSelected);
+
+function greenSelected() {
+  black.className = 'color';
+  blue.className = 'color';
+  red.className = 'color';
+  green.className = 'color selected';
+}
+
+green.addEventListener('click', greenSelected);
+
+const pixels = document.getElementsByClassName('pixel');
+
+// for (let index = 0; index < pixels.length; index += 1) {
+  // pixels[index].addEventListener('click', function(){
+  // pixels[index] = selectedColor
+  // });
+// }
