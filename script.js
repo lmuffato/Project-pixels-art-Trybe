f03772b136f8f6392a89670fb27a8621280afdbd
mallmann02 <leomallmann020302@gmail.com>
2021-03-09 16:32:09 -0300

    Requisito 4

--- script.js
+++ script.js
@@ -11,3 +11,13 @@ function addColorsInPalette() {
 
   colorPaletteContainers[0].style.background = 'black';
 }
+
+for (let index = 0; index < 25; index += 1) {
+  document.querySelector('#pixel-board').appendChild(createPixelInBoard())
+}
+
+function createPixelInBoard() {
+  let pixel = document.createElement('container');
+  pixel.className = 'pixel';
+  return pixel;
+}
