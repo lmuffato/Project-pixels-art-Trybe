d201961279bf194cb101e6bf9cc0a7782cb356e4
Patrick Morais <ppternunes@gmail.com>
2021-03-09 18:27:32 -0300

    teste requisito 5

--- script.js
+++ script.js
@@ -104,7 +104,13 @@ function listenerBtn() {
   sizeBtn.addEventListener('click', generateBoardSize);
 }
 
+function initializeBoard(a) {
+  addDivsLine(a);
+  addPixels(a);
+}
+
 window.onload = function () {
+  initializeBoard(5);
   squareColors();
   listenPaletteSection();
   selectColor();
