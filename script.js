38c7cecb7bc34a4036cf4be47d3fcc0059e97351
Carlos Campo <carlosgabrielcampo@gmail.com>
2021-03-09 18:20:30 -0300

    requisito 6 e 7

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const colorPalette = document.getElementById('color-palette');
 const pixelLines = document.getElementById('pixel-board');
-let pixelSpaces = document.getElementsByClassName('lines')
+const pixelSpaces = document.getElementsByClassName('lines');
+const colorBlock = document.getElementsByClassName('color');
+
 
 
 function createPaletteBlocks() {
@@ -14,11 +16,12 @@ function createPaletteBlocks() {
 createPaletteBlocks();
 
 function blockColor() {
-  const colorBlock = document.getElementsByClassName('color');
-  colorBlock[0].style.backgroundColor = 'black';
+  let firstblock = colorBlock[0]
+  firstblock.style.backgroundColor = 'black';
+  firstblock.className = "color selected"
   for (let index = 1; index < colorBlock.length; index += 1) {
-    colorBlock[index].style.backgroundColor = 
-    `rgb(${Math.random() * 255},${Math.random() * 255},${Math.random() * 255})`;
+    colorBlock[index].style.backgroundColor = (
+    `rgb(${Math.random() * 255},${Math.random() * 255},${Math.random() * 255})`);
   }
 }
 
@@ -26,17 +29,34 @@ blockColor();
 
 function pixelBoard() {
 
-  for (let indexLine = 0; indexLine <= 4; indexLine += 1){  
-  const pixelLine = document.createElement('div')
-  pixelLine.className = "lines"
-  pixelLines.appendChild(pixelLine)
-  
-    for (let index = 0; index <= 4; index += 1){  
-    let pixel = document.createElement('div')
-    pixel.className = 'pixel'
-    pixelSpaces[indexLine].appendChild(pixel)
+  for (let indexLine = 0; indexLine <= 4; indexLine += 1) {
+    const pixelLine = document.createElement('div');
+    pixelLine.className = 'lines';
+    pixelLines.appendChild(pixelLine);
+
+    for (let index = 0; index <= 4; index += 1) {
+      const pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      pixelSpaces[indexLine].appendChild(pixel);
     }
   }
 }
-pixelBoard()
+pixelBoard();
+
+
+console.log(colorPalette.addEventListener("click",selectPixel))
+
+function selectPixel(colorChoose) {
+    const colorClass = colorChoose.target.className;
+  if (colorClass.indexOf("selected") > 0 && colorClass.indexOf("color") > 0 ) {
+    colorChoose.className ="color selected"
+    } else {
+    for(let index = 0; index < colorBlock.length; index += 1){
+        colorBlock[index].className = "color" }
+
+    colorChoose.target.className += " selected"
+    console.log(colorChoose.target)
+    }
+}
+
 
