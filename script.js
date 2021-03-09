b9efc8bded83d587f6029891b08b63041ad2ca27
renancosta21 <renancosta21@gmail.com>
2021-03-09 19:23:58 -0300

    requisito 2

---
+++ script.js
@@ -0,0 +1,11 @@
+let colorsList = ['', '', '', ''];
+
+for (let index = 0; index < colorsList.length; index += 1) {
+  colorsList[index] = document.createElement('div');
+  colorsList[index].classList.add('color');
+  document.querySelector('#color-pallete').appendChild(colorsList[index]);
+}
+document.getElementsByClassName('color')[0].style.backgroundColor = 'black';
+document.getElementsByClassName('color')[1].style.backgroundColor = 'green';
+document.getElementsByClassName('color')[2].style.backgroundColor = 'red';
+document.getElementsByClassName('color')[3].style.backgroundColor = 'blue';
