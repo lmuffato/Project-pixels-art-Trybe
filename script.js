e18f77242e1cea2f17dd4c645d71afe2da1dc3b1
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-03-09 18:39:54 -0300

    exercicio 6 teste

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
+/*
 window.onload = function () {
-    
+    selecionaCor ();
+    colocaCor ();
+    resetaCor ();
   };
 
  function selecionaCor (){
+     
     let paleta = document.querySelectorAll('.color');
 
-    for (let index = 0; index < paleta.length, index += 1){
+    for (let index = 0; index < paleta.length, index += 1) {
         paleta[index].addEventListener('click', function() {
-            let cor = document.querySelector('.cor1');       
+            paleta[index].className = 'select';  
         })
     }
-    return cor;
+
  } 
+
+
+ function colocaCor (){
+
+ }
+
+
+function resetaCor (){
+
+ }
+ */
