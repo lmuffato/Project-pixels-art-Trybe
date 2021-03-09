fbec7f896bcc0b016770c98db97627d1e11c3028
Hernanne <hernannedomiciano@gmail.com>
2021-03-09 17:14:39 -0300

    exercicios 4-5

---
+++ script.js
@@ -0,0 +1,12 @@
+const painting = document.querySelector('#pixel-board');
+
+for (let index = 0; index < 5; index += 1) {
+  const pixels = document.createElement('div');
+  pixels.className = 'lines';
+  painting.appendChild(pixels);
+  for (let index2 = 0; index2 < 5; index2 += 1) {
+    const pixelLines = document.createElement('div');
+    pixelLines.className = 'pixel';
+    pixels.appendChild(pixelLines);
+  }
+}
