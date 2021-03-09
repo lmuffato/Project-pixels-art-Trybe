9b612605fa76fc2129fb9a474b0ddaa42855431c
Orlando Flores <orlando_flores@id.uff.br>
2021-03-09 16:21:36 -0300

    Corrigindo arquivos styles.css e script.js para o lint

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-window.onload = function() {
-    lines = document.getElementsByClassName('pixel-line');
-    for (let index = 0; index < lines.length; index += 1) {
-        for(let column = 1; column <= 5; column +=1) {
-            div = document.createElement('div');
-            div.className = 'pixel';
-            lines[index].appendChild(div);
-        }
+window.onload = function () {
+  const lines = document.getElementsByClassName('pixel-line');
+  for (let index = 0; index < lines.length; index += 1) {
+    for (let column = 1; column <= 5; column += 1) {
+      const div = document.createElement('div');
+      div.className = 'pixel';
+      lines[index].appendChild(div);
     }
-}
+  }
+};
