cbf1a8f61407166867b0305eaefac654f0347bd6
Carol <carolinavpinheiro@gmail.com>
2021-03-09 20:32:41 -0300

    Adicionando requisito 7

--- script.js
+++ script.js
@@ -3,6 +3,41 @@ const colorPink = document.querySelector('#pink');
 const colorGreen = document.querySelector('#green');
 const colorOrange = document.querySelector('#orange');
 
+function addClass(color) {
+    color.classList.add('selected');
+}
+
+function removeClass(color) {
+    color.classList.remove('selected');
+}
+
 colorBlack.addEventListener('click', function (){
-    colorBlack.classList.add('selected');
+    addClass(colorBlack);
+    removeClass(colorPink);
+    removeClass(colorGreen);
+    removeClass(colorOrange);
+    console.log(colorPink); 
+})
+
+colorPink.addEventListener('click', function () {
+    addClass(colorPink);
+    removeClass(colorBlack);
+    removeClass(colorGreen);
+    removeClass(colorOrange);
+    console.log(colorPink);
+})
+
+
+colorGreen.addEventListener('click', function () {
+    addClass(colorGreen);
+    removeClass(colorBlack);
+    removeClass(colorPink);
+    removeClass(colorOrange);
+})
+
+colorOrange.addEventListener('click', function () {
+    addClass(colorOrange);
+    removeClass(colorBlack);
+    removeClass(colorPink);
+    removeClass(colorGreen);     
 })
