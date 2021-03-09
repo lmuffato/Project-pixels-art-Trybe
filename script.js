4bf9e3fea0e3379a538fae58e5b041cf60e01241
Ana Ventura <anaventura1811@gmail.com>
2021-03-09 17:41:25 -0300

    Adiciona requisitos 2, 3 e 4

---
+++ script.js
@@ -0,0 +1,5 @@
+/* function colorSelection(colors) {
+  const colors = document.getElementById('cor-verde');
+  colors.style.setItem ('color', backgroundColor);
+} */
+// document.getElementById('elemento').classList.add('classe'); // Adiciona classe.
