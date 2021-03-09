a09c35ed62f32e0998c9009f3ba72ae01b237693
lucas-chave <lcchavesr150@gmail.com>
2021-03-09 20:42:26 -0300

    adicionando correcoes ao lint

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-let paletteColor = document.querySelector('#color-palette');
-let elementTable = document.querySelector('#pixel-board');
+const paletteColor = document.querySelector('#color-palette');
+const elementTable = document.querySelector('#pixel-board');
 
 // adicionando cores a paleta de cores
 function addColorQuadOne() {
-  return (paletteColor.children[0].style.backgroundColor = 'black');
+  paletteColor.children[0].style.backgroundColor = 'black';
 }
 
 function addColorQuadTwo() {
-  return (paletteColor.children[1].style.backgroundColor = 'blue');
+  paletteColor.children[1].style.backgroundColor = 'blue';
 }
 
 function addColorQuadThree() {
-  return (paletteColor.children[2].style.backgroundColor = 'brown');
+  paletteColor.children[2].style.backgroundColor = 'brown';
 }
 
 function addColorQuadFour() {
-  return (paletteColor.children[3].style.backgroundColor = 'aqua');
+  paletteColor.children[3].style.backgroundColor = 'aqua';
 }
 
 addColorQuadOne();
@@ -24,11 +24,8 @@ addColorQuadThree();
 addColorQuadFour();
 
 // criando tabela
-for (let index = 0; index < 25; index += 1 ) {
-	let elementDiv = document.createElement('div');
-	elementDiv.className = 'pixel';
-	elementTable.appendChild(elementDiv)
+for (let index = 0; index < 25; index += 1) {
+  const elementDiv = document.createElement('div');
+  elementDiv.className = 'pixel';
+  elementTable.appendChild(elementDiv);
 }
-
-
-// sites refencia: 
