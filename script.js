4774ffcd16ae70c0017a8f7eaea3f2118c591e7d
Maria Baeta <m.martinsdg@gmail.com>
2021-03-09 20:04:21 -0300

    Atende ao requisito 2

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-window.onload {
-
+function createColor() {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+  const color = `rgb(${r}, ${g}, ${b})`;
+  return color;
 }
-function getColor () {
-  let 
+function setColors() {
+  const getDivs = document.getElementsByClassName('color');
+  for (let index = 0; index < getDivs.length; index += 1) {
+    getDivs[index].style.backgroundColor = createColor();
+  }
+}
+
+/* function getDivs() {
+  const getColor = document.getElementsByClassName('color');
+  for (let index = 0; index < getColor.length; index += 1) {
+    getColor[0].style.backgroudColor = 'black';
+  }
+  return getColor;
+}
+getDivs(); */
+
+window.onload = function() {
+  setColors();
 }
