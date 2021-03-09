18a25464043ec14c6c11a7234519b302889e6a86
Luiz Felipe Araujo <thelastnailmusic@gmail.com>
2021-03-09 16:46:06 -0300

    Colore Paleta Superior

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
-function criaPaleta(numDePaleta) {
-  for (let index = 0; index < numDePaleta; index +=1) {
+function criaPaleta(numberOfFrames) {
+  for (let index = 0; index < numberOfFrames; index +=1) {
     let pegaPaleta = document.getElementById("color-palette");
     let criaPaleta = document.createElement("div")
     criaPaleta.className = 'color';
     pegaPaleta.appendChild(criaPaleta);
   }
 }
+criaPaleta(4);
 
-function criaQuadro(numberOfQuadros) {
-  for (let index = 0; index < numberOfQuadros * numberOfQuadros; index += 1) {
+function colorePaletaSuperior() {
+  document.getElementsByClassName('color')[0].style.backgroundColor = "black";
+  document.getElementsByClassName('color')[1].style.backgroundColor = "orange";
+  document.getElementsByClassName('color')[2].style.backgroundColor = "olive";
+  document.getElementsByClassName('color')[3].style.backgroundColor = "blue";
+}
+colorePaletaSuperior();
+
+function criaQuadro(numberOfFrames) {
+  for (let index = 0; index < numberOfFrames * numberOfFrames; index += 1) {
     let criaDiv = document.createElement('div');
     criaDiv.className = 'pixel';
     let pegaDiv = document.getElementById('pixel-board');
     pegaDiv.append(criaDiv);
   }
 }
-
-criaPaleta(4);
 criaQuadro(5);
