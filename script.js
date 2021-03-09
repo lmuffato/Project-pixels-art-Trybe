a5339a59e5576cb807dcb946452a41b234392f1b
Gui-lira <gui.lira.martins@gmail.com>
2021-03-09 17:39:48 -0300

    7

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ for (let counter = 1; counter <= 25; counter += 1) {
   pixel.className = 'pixel';
   board.appendChild(pixel);
 }
+let colors = document.querySelectorAll('.color');
+for (let item in colors) {
+    colors[item].addEventListener('click', function() {
+        document.querySelector('.selected').classList.remove('selected');
+        colors[item].classList.add('selected')
+    })
+}
