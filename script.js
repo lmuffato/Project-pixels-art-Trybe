ceae48fa328dcf9b0b5eff54101f9dac119c3683
Lucas Pedroso <lepsk8@gmail.com>
2021-03-09 17:12:22 -0300

    fix Mandatory requirements finalized.

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const addEvListener = (elementNode, event, fn) => {
     console.log('O parâmetro fn deve ser uma função.');
     return;
   }
-  let node = document.querySelector(elementNode);
+  const node = document.querySelector(elementNode);
   node.addEventListener(event, fn);
-}
+};
 
 const addEvListenerNElements = (elementsNodes, event, fn) => {
   if (typeof fn !== 'function') {
@@ -13,53 +13,54 @@ const addEvListenerNElements = (elementsNodes, event, fn) => {
     return;
   }
   let nodes = document.querySelectorAll(elementsNodes);
-  for (let index = 0; index < nodes.length; index++) {
+  for (let index = 0; index < nodes.length; index += 1) {
     nodes[index].addEventListener(event, fn);
   }
-}
+};
 
 const changeColorPixel = (ev) => {
   let el = document.querySelector('.selected');
   let newColor = el.style.backgroundColor;
   let pixel = ev.target;
   pixel.style.backgroundColor = newColor;
-}
+};
 
 const toggleClassInit = (elementNode, className) => {
   let element = document.querySelector(elementNode);
   element.classList.toggle(className);
-}
+};
 
 const toggleClassEvent = (elementNode, className) => {
   let eventNode = elementNode.target;
   let colorListEl = document.getElementsByClassName('color');
-  for (const key of colorListEl) { //A prova de falhas;
+  for (const key of colorListEl) {
     let el = key;
     if (el.classList.value.includes(className)) {
-      el.classList.toggle(className);;
+      el.classList.toggle(className);
     }
   }
-  if (!eventNode.classList.value.includes(className))
+  if (!eventNode.classList.value.includes(className)) {
     eventNode.classList.toggle(className);
-}
+  }
+};
 
 const setColorsPalette = () => {
-  let black = document.querySelector('.black');
-  let red = document.querySelector('.red');
-  let green = document.querySelector('.green');
-  let blue = document.querySelector('.blue');
+  const black = document.querySelector('.black');
+  const red = document.querySelector('.red');
+  const green = document.querySelector('.green');
+  const blue = document.querySelector('.blue');
   black.style.backgroundColor = 'black';
   red.style.backgroundColor = 'red';
   green.style.backgroundColor = 'green';
   blue.style.backgroundColor = 'blue';
-}
+};
 
 const changeBackgroundColorPixels = () => {
-  let pixels = document.querySelectorAll('.pixel');
+  const pixels = document.querySelectorAll('.pixel');
   pixels.forEach((el) => {
     el.style.backgroundColor = 'white';
   });
-}
+};
 
 window.onload = () => {
   setColorsPalette();
