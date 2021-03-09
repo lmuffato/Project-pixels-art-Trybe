19cc60a291b0a5db76a086722131c5cd4bbe79b2
Guilherme de Prais <guilherme.prais@gmail.com>
2021-03-09 17:16:41 -0300

    requisito 8

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Selecina os elementos da paleta de cor, cria um EventListener de "Click" pra cada uma deles, e chama a função "addSelected" que remove a class "selected" de todos elementos, e em seguida adiciona a mesma classe somente no elemento clicado
 const colorSelector = document.querySelectorAll('.color');
 
 for (let index = 0; index < colorSelector.length; index += 1) {
@@ -9,4 +10,42 @@ function addSelected(event) {
     colorSelector[i].classList.remove('selected');
   }
   event.currentTarget.classList.add('selected');
+}
+
+const selectPixels = document.querySelectorAll('.pixel');
+
+for (let i = 0; i < selectPixels.length; i += 1) {
+  selectPixels[i].addEventListener('click', addColor);
+}
+
+const selectBlack = document.querySelector('.black');
+const selectPurple = document.querySelector('.purple');
+const selectGreen = document.querySelector('.lightgreen');
+const selectPink = document.querySelector('.pink');
+
+function addColor(event) {
+  for (let i = 0; i < colorSelector.length; i += 1) {
+    event.currentTarget.classList.remove(
+      'white',
+      'black',
+      'purple',
+      'lightgreen',
+      'pink'
+    );
+
+    if (selectBlack.classList.contains('selected')) {
+      event.currentTarget.classList.add('black');
+    }
+
+    if (selectPurple.classList.contains('selected')) {
+      event.currentTarget.classList.add('purple');
+    }
+    if (selectGreen.classList.contains('selected')) {
+      event.currentTarget.classList.add('lightgreen');
+    }
+
+    if (selectPink.classList.contains('selected')) {
+      event.currentTarget.classList.add('pink');
+    }
+  }
 }
