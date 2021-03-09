c642454febc7f250e9cd538bc225c6efaaba1ea1
William Willers <w.willers@outlook.com>
2021-03-09 17:34:12 -0400

    Adicionando classe com classListadd Req 6

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function setColorPallet() {
   const arrayColor = document.querySelectorAll('.color');
   arrayColor[0].style.backgroundColor = 'black';
-  arrayColor[0].className = 'selected';
+  arrayColor[0].classList.add('selected');
   arrayColor[1].style.backgroundColor = 'red';
   arrayColor[2].style.backgroundColor = 'green';
   arrayColor[3].style.backgroundColor = 'blue';
