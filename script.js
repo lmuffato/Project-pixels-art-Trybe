7b6c0162d446f0ae9b03f883551c2170502a2d4c
Carlos Campo <carlosgabrielcampo@gmail.com>
2021-03-09 16:57:03 -0300

    requisito 4 e 5

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const colorPalette = document.getElementById('color-palette');
+const pixelLines = document.getElementById('pixel-board');
+let pixelSpaces = document.getElementsByClassName('lines')
+
 
 function createPaletteBlocks() {
   for (let index = 0; index <= 3; index += 1) {
@@ -11,11 +14,29 @@ function createPaletteBlocks() {
 createPaletteBlocks();
 
 function blockColor() {
-  let colorBlock = document.getElementsByClassName('color');
+  const colorBlock = document.getElementsByClassName('color');
   colorBlock[0].style.backgroundColor = 'black';
   for (let index = 1; index < colorBlock.length; index += 1) {
-      colorBlock[index].style.backgroundColor = `rgb(${Math.random() * 255},${Math.random() * 255},${Math.random() * 255})`;
+    colorBlock[index].style.backgroundColor = 
+    `rgb(${Math.random() * 255},${Math.random() * 255},${Math.random() * 255})`;
   }
 }
 
 blockColor();
+
+function pixelBoard() {
+
+  for (let indexLine = 0; indexLine <= 4; indexLine += 1){  
+  const pixelLine = document.createElement('div')
+  pixelLine.className = "lines"
+  pixelLines.appendChild(pixelLine)
+  
+    for (let index = 0; index <= 4; index += 1){  
+    let pixel = document.createElement('div')
+    pixel.className = 'pixel'
+    pixelSpaces[indexLine].appendChild(pixel)
+    }
+  }
+}
+pixelBoard()
+
