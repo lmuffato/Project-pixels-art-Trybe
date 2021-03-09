f7682591da233b6c0951675352ee6e5fbae8dc4a
Bruno Mendes <mendessbruno@gmail.com>
2021-03-09 17:05:03 -0300

    alterada cor dos pixels para branco e feitos ajustes sugeridos no lint

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 function createBoardLines() {
-  let local = document.getElementById('pixel-board');
+  const local = document.getElementById('pixel-board');
   for (let index = 0; index < 5; index += 1) {
-    let line = document.createElement('div');
+    const line = document.createElement('div');
     line.className = 'box-line';
     local.appendChild(line);
   }
 }
 
 function createBoard() {
-  let local = document.querySelectorAll('.box-line');
+  const local = document.querySelectorAll('.box-line');
   for (let index = 0; index < local.length; index += 1) {
     for (let index2 = 0; index2 < 5; index2 += 1) {
-      let column = document.createElement('div');
+      const column = document.createElement('div');
       column.className = 'pixel';
       local[index].appendChild(column);
     }
   }
 }
 
-window.onload = function() {
+window.onload = function run() {
   createBoardLines();
   createBoard();
-}
+};
