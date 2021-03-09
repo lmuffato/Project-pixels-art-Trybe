1078ae668eed0929814d6e192a00cb83d94d810f
Lucas André <lucas.cancado89@gmail.com>
2021-03-09 15:37:59 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 for (let index = 0; index < 5; index += 1) {
-  let tagLinha = document.createElement('div');
+  const tagLinha = document.createElement('div');
   tagLinha.className = 'line';
+  tagLinha.style.height = '42px';
   document.querySelector('#pixel-board').appendChild(tagLinha);
   for (let i = 1; i <= 5; i += 1) {
-    let tagPixel = document.createElement('div');
+    const tagPixel = document.createElement('div');
     tagPixel.className = 'pixel';
     tagPixel.style.backgroundColor = 'white';
+    tagPixel.style.width = '40px';
+    tagPixel.style.height = '40px';
+    tagPixel.style.display = 'inline-block';
+    tagPixel.style.border = 'solid black 1px';
     document.querySelectorAll('.line')[index].appendChild(tagPixel);
   }
 }
