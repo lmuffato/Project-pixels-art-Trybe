a491d3702510af3f2c246addb2738f36ab10b93f
Gustavo Sousa <gustavohl98@gmail.com>
2021-03-09 16:09:20 -0300

    Requisito 02 - Adiciona cores a paleta

---
+++ script.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+  const colorPalette = document.getElementById('color-palette');
+  const colorDivs = document.querySelectorAll('.color');
+
+  const paletteColors = ['black', 'blue', 'red', 'green']
+
+  function setPaletteColors() {
+    for (let index = 0; index < paletteColors.length; index += 1) {
+      const colorDiv = colorDivs[index];
+      const currentColor = paletteColors[index];
+
+      colorDiv.style.backgroundColor = currentColor;
+    }
+  }
+
+  setPaletteColors()
+
+}
