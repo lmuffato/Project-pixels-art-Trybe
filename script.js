536d61cd029d99b0736f865673f0ff7e4ddca3eb
Rodrigo Queiroz <rodrigoandradequeiroz@gmail.com>
2021-03-09 18:26:51 -0300

    terceiro requisito

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-let getSquare = document.getElementById('pixel-board');
+const getSquare = document.getElementById('pixel-board');
 let makeRow = document.createElement('tr');
 let makeSquare = document.createElement('td');
-let tdClass = 'pixel'
+const tdClass = 'pixel';
 
 for (let index = 0; index < 5; index += 1) {
-    makeSquare = document.createElement('td')
-    makeRow = document.createElement('tr')
-    getSquare.appendChild(makeRow)
-    for (let indexSquare = 0; indexSquare <= 5; indexSquare += 1) {
-      getSquare.appendChild(makeSquare)
-      makeSquare = document.createElement('td')
-      makeSquare.className = tdClass
+  makeSquare = document.createElement('td');
+  makeRow = document.createElement('tr');
+  getSquare.appendChild(makeRow);
+  for (let indexSquare = 0; indexSquare <= 5; indexSquare += 1) {
+    getSquare.appendChild(makeSquare);
+    makeSquare = document.createElement('td');
+    makeSquare.className = tdClass;
   }
 }
-
