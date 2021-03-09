43c0c1dfce5baa271c5098fbf4dd39950d74ac93
Gustavo Sousa <gustavohl98@gmail.com>
2021-03-09 16:33:11 -0300

    Requisito 04 - quadro de pixels

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 // const colorPalette = document.getElementById('color-palette');
 const colorDivs = document.querySelectorAll('.color');
+const pixelBoard = document.getElementById('pixel-board');
 
 const paletteColors = ['black', 'blue', 'red', 'green'];
+const pixelColumns = 5;
+const pixelRows = 5;
 
 function setPaletteColors() {
   for (let index = 0; index < paletteColors.length; index += 1) {
@@ -12,4 +15,13 @@ function setPaletteColors() {
   }
 }
 
+function setPixels() {
+  for (let index = 1; index <= pixelColumns * pixelRows; index +=1) {
+    const pixel = document.createElement('div');    
+    pixel.className = "pixel";
+    pixelBoard.appendChild(pixel);
+  }
+}
+
 setPaletteColors();
+setPixels();
