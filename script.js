608df2efa7351adfbe1a4dc664f31238f954fea1
Giovanni <giovannitadeu08@gmail.com>
2021-03-09 19:18:39 -0300

    Resolvendo problemas com Linter - parte 3 (O retorno)

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const colorSelected = document.getElementsByClassName('color');
 const buttonElement = document.querySelector('#clear-board');
 const pixelSelected = document.getElementsByClassName('pixel');
 
-// Funções e Eventos Gerais
-for (let colorPos = 0; colorPos < 4; colorPos += 1) { //Gera os blocos de cores
+//  Funções e Eventos Gerais
+for (let colorPos = 0; colorPos < 4; colorPos += 1) { // Gera os blocos de cores
   const createBoxColors = document.createElement('li');
   createBoxColors.className = 'color';
   createBoxColors.style.backgroundColor = colors[colorPos];
@@ -14,7 +14,7 @@ for (let colorPos = 0; colorPos < 4; colorPos += 1) { //Gera os blocos de cores
 }
 
 for (let linePos = 0; linePos < 5; linePos += 1) { //  Gera a tabela
-  const linha = document.createElement('div'); 
+  const linha = document.createElement('div');
   linha.className = 'line';
   pixelBoard.appendChild(linha);
   for (let pixelPos = 0; pixelPos < 5; pixelPos += 1) {
@@ -35,11 +35,13 @@ function changeSelected(select) { //  Seleciona o pixel que vai ser pintado
 }
 
 colorPalette.addEventListener('click', function changeSelectedColor(e) { //  Troca a cor selecionada para pintar
-  changeSelected(event.target);
+  let index = e.target
+  changeSelected(index);
 });
 
 pixelBoard.addEventListener('click', function changePixelToSelectedColor(e) { //  Troca a cor do pixel para a cor selecionada
-  e.target.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
+  let index = e.target
+  index.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
 });
 
 buttonElement.addEventListener('click', function pixelsCleaner() { // Limpador de Tela
