f4a218e669d58578cbe8be176a1b1bd1660080f4
luanlsr <luanfswd@gmail.com>
2021-03-09 17:22:13 -0300

    corrigido o lint: semicolon

--- script.js
+++ script.js
@@ -5,36 +5,35 @@ const element4 = document.getElementsByClassName('color')[3];
 
 element1.addEventListener('click', () => {
   element1.classList.add('selected');
-  element2.classList.remove('selected')
-  element3.classList.remove('selected')
-  element4.classList.remove('selected')
+  element2.classList.remove('selected');
+  element3.classList.remove('selected');;
   console.log('Cliquei no PRETO');
 
 })
 
 element2.addEventListener('click', () => {
   element2.classList.add('selected');
-  element1.classList.remove('selected')
-  element3.classList.remove('selected')
-  element4.classList.remove('selected')
+  element1.classList.remove('selected');
+  element3.classList.remove('selected');
+  element4.classList.remove('selected');
   console.log('Cliquei no VERMELHO');
 })
 console.log(element2);
 
 element3.addEventListener('click', () => {
   element3.classList.add('selected');
-  element2.classList.remove('selected')
-  element1.classList.remove('selected')
-  element4.classList.remove('selected')
+  element2.classList.remove('selected');
+  element1.classList.remove('selected');
+  element4.classList.remove('selected');
   console.log('Cliquei no AZUL');
 })
 console.log(element3);
 
 element4.addEventListener('click', () => {
   element4.classList.add('selected');
-  element2.classList.remove('selected')
-  element3.classList.remove('selected')
-  element1.classList.remove('selected')
+  element2.classList.remove('selected');
+  element3.classList.remove('selected');
+  element1.classList.remove('selected');
   console.log('Cliquei no VERDE');
 })
 console.log(element4);
