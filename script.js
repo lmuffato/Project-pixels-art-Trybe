b03d14ad19205236b158f22ac65d982da0fa47a7
Gui-lira <gui.lira.martins@gmail.com>
2021-03-09 19:12:55 -0300

    exercicio 8

--- script.js
+++ script.js
@@ -4,14 +4,17 @@ for (let counter = 1; counter <= 25; counter += 1) {
   pixel.className = 'pixel';
   board.appendChild(pixel);
 }
-const colors = document.querySelectorAll('.color');
-let selected = document.querySelector('.selected')
 document.addEventListener('click', function(event) {
     if (event.target.classList.contains('color')) {
         document.querySelector('.selected').classList.remove('selected')
         event.target.classList.add('selected')
-    }
-    else if (event.target.classList.contains('pixel')) {
-        console.log(selected)
-    }
+    } 
+});
+document.querySelector('.black').style.backgroundColor = 'black';
+document.querySelector('.yellow').style.backgroundColor = 'yellow';
+document.querySelector('.red').style.backgroundColor = 'red';
+document.querySelector('.blue').style.backgroundColor = 'blue';
+document.querySelector('#pixel-board').addEventListener('click', function(event) {
+    let selectedColor = document.querySelector('.selected').style.backgroundColor;
+    event.target.style.backgroundColor = selectedColor;
 })
