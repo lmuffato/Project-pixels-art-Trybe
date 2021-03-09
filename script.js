ad3c2d63bf5f8f8be1b304b7bd813becc1f2dde5
Viegas <mvrdgs@gmail.com>
2021-03-09 17:29:19 -0300

    requisito 8

--- script.js
+++ script.js
@@ -9,4 +9,22 @@ window.onload = function script() {
   }
 
   setColor(['black', 'blue', 'green', 'yellow']);
+
+  function pixelPaint() {
+    let colorSelect = document.querySelector('#color-palette');
+    let pixelBoard = document.querySelector('#pixel-board')
+
+    colorSelect.addEventListener('click', function(event){
+      let selectedColor = event.target;
+      document.querySelector('.selected').className = 'boxes color';
+      selectedColor.className += ' selected';
+    });
+
+    pixelBoard.addEventListener('click', function(event){
+      let pixel = event.target;
+      let color = document.querySelector('.selected');
+      pixel.style.backgroundColor = color.style.backgroundColor;
+    });
+  }
+  pixelPaint();
 };
