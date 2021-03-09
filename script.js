9c1a0474cd4314b7af44b1d34e72ff51e1ccb5dd
Jodiel Briesemeister <jodielll29@gmail.com>
2021-03-09 18:52:28 -0300

    alterada a lógica dos  requisitos 6, 7 e corrigido erros do linter

--- script.js
+++ script.js
@@ -1,31 +1,49 @@
-window.onload = initialColor;
-
-function initialColor(){
-    sessionStorage.setItem("cor", "black");
-}
-
 function palette() {
-    let paleta = document.querySelectorAll(".color")
-    let cores = ["black", "blue", "yellow", "purple"];
-    for (let index = 0; index < paleta.length; index += 1){
-        paleta[index].style.backgroundColor = cores[index];
+  const color = document.querySelectorAll('.color');
+  const arrayColors = ['black', 'blue', 'yellow', 'purple'];
+  for (let index = 0; index < color.length; index += 1) {
+    color[index].style.backgroundColor = arrayColors[index];
+    if (arrayColors[index] === 'black') {
+      color[index].className += ' selected';
     }
+  }
 }
 
-palette();
-
-function getColor(obj) {
-    sessionStorage.setItem("cor", obj.style.backgroundColor)
-}
-
-function setColor(obj) {
-    let selectedColor = sessionStorage.getItem("cor")
-    obj.style.backgroundColor = selectedColor;
-}
+window.onload = palette;
 
 function clearBoard() {
-    let quadro = document.querySelectorAll(".pixel")
-    for (let index = 0; index < quadro.length; index += 1) {
-        quadro[index].style.backgroundColor = "white";
+  const cleanBtn = document.querySelector('#clear-board');
+  cleanBtn.addEventListener('click', () => {
+    const board = document.querySelectorAll('.pixel');
+    for (let index = 0; index < board.length; index += 1) {
+      board[index].style.backgroundColor = 'white';
     }
+  });
 }
+
+clearBoard();
+
+function getColor() {
+  const colorArray = document.querySelector('#color-palette');
+  colorArray.addEventListener('click', (event) => {
+    const selectedColor = event.target;
+    document.querySelector('.selected').className = 'color';
+    selectedColor.className += ' selected';
+    // console.log(selectedColor);
+    // console.log(colorArray);
+  });
+}
+
+getColor();
+
+function setColor() {
+  const pixelTable = document.querySelector('#pixel-board');
+  pixelTable.addEventListener('click', (event) => {
+    const selectPixel = event.target;
+    const color = document.querySelector('.selected').style.backgroundColor;
+    // console.log(color)
+    selectPixel.style.backgroundColor = color;
+  });
+}
+
+setColor();
