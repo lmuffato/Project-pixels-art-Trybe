fe5de386752e6e205dc23cfa98d2a906b817d5f0
Victor Ricco <victor.ricco001@gmail.com>
2021-03-09 19:50:18 -0300

    fim dia1 projeto

--- script.js
+++ script.js
@@ -12,4 +12,19 @@ window.onload = function(){
       colorSelectedString.innerText = colorName
 		}
 	});
+
+	function createTable(height, weight){
+		let table = document.querySelector('#pixel-board');
+		for(let index = 0; index < height; index += 1){
+			let tr = document.createElement('tr');
+			tr.classList.add('main')
+			table.appendChild(tr)
+			for(let index = 0; index < weight; index += 1){
+				let td = document.createElement('td');
+				td.classList.add("pixel");
+				tr.appendChild(td)
+			}
+		}
+	}
+	createTable(5, 5);
 }
