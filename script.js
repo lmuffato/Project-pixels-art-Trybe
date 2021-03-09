8689f339b2370ae83255959e21dcef08ab44f8fa
Orlando Flores <orlando_flores@id.uff.br>
2021-03-09 17:10:00 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -10,3 +10,18 @@ window.onload = function () {
   const blackPalette = document.getElementById('black-palette');
   blackPalette.className += ' selected';
 };
+
+function setSelectedTask(element) {
+  const selectedTask = document.getElementsByClassName('selected')[0];
+  selectedTask.className = 'color';
+  element.className = 'color selected';
+}
+
+const paletteColors = document.getElementsByClassName('color');
+for (let index = 0; index < paletteColors.length; index += 1) {
+  paletteColors[index].addEventListener('click', function (event) {
+    if (event.target.className === 'color') {
+      setSelectedTask(paletteColors[index]);
+    }
+  });
+}
