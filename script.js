bf7acb8669e61966273aa4e672a778351e19b454
Eduardo Seije <eduardoseije@gmail.com>
2021-03-09 17:38:09 -0300

    Requisito 06 - cor inicial definida

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function changeColor1() {
   const color1 = document.getElementById('color1');
-  color1.style.backgroundColor= 'black';  
+  color1.style.backgroundColor = 'black';  
 }
 changeColor1();
+
+ window.onload = function inicialColot() {
+  const inicialColor = document.getElementById('color1');
+  inicialColor.className = 'selected';     
+}
+inicialColor();
