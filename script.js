070f8de07005491c1c0158f158b325833eecac02
luanlsr <luanfswd@gmail.com>
2021-03-09 17:12:40 -0300

    Requisito 7 cumprido

--- script.js
+++ script.js
@@ -4,31 +4,43 @@ const element3 = document.getElementsByClassName('color')[2];
 const element4 = document.getElementsByClassName('color')[3];
 
 element1.addEventListener('click', () => {
-  console.log('Cliquei no preto');
-  //handle click
+  element1.classList.add('selected');
+  element2.classList.remove('selected')
+  element3.classList.remove('selected')
+  element4.classList.remove('selected')
+  console.log('Cliquei no PRETO');
+
 })
-console.log(element1);
 
 element2.addEventListener('click', () => {
-  console.log('Cliquei no vermelho');
-  //handle click
+  element2.classList.add('selected');
+  element1.classList.remove('selected')
+  element3.classList.remove('selected')
+  element4.classList.remove('selected')
+  console.log('Cliquei no VERMELHO');
 })
 console.log(element2);
 
 element3.addEventListener('click', () => {
-  console.log('Cliquei no azul');
-  //handle click
+  element3.classList.add('selected');
+  element2.classList.remove('selected')
+  element1.classList.remove('selected')
+  element4.classList.remove('selected')
+  console.log('Cliquei no AZUL');
 })
 console.log(element3);
 
 element4.addEventListener('click', () => {
-  console.log('Cliquei no verde');
-  //handle click
+  element4.classList.add('selected');
+  element2.classList.remove('selected')
+  element3.classList.remove('selected')
+  element1.classList.remove('selected')
+  console.log('Cliquei no VERDE');
 })
 console.log(element4);
 
 function chooseColor() {
-  let selection = document.getElementsByClassName("selected");
+  let selection = document.getElementsByClassName("selected")[0];
   let CSSprop = window.getComputedStyle(selection, null).getPropertyValue("background-color");
   return CSSprop;
 }
