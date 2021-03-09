0656663a4da01fc04812bfc778bca47a7fc44aea
Lucas Henrique Brito <lucas.silva.brito@hotmail.com.br>
2021-03-09 20:27:56 -0300

    Requisitos 1-3

---
+++ script.js
@@ -0,0 +1,6 @@
+const colors = document.querySelectorAll('.color');
+
+colors[0].style.backgroundColor = 'black';
+colors[1].style.backgroundColor = 'red';
+colors[2].style.backgroundColor = 'green';
+colors[3].style.backgroundColor = 'blue';
