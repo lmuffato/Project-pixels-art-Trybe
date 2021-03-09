9ff5eaf150b9bbbef83b831fdd83ccad5470f874
Rodrigo Queiroz <rodrigoandradequeiroz@gmail.com>
2021-03-09 18:49:15 -0300

    quinto requisito

--- script.js
+++ script.js
@@ -3,13 +3,16 @@ let makeRow = document.createElement('tr');
 let makeSquare = document.createElement('td');
 const tdClass = 'pixel';
 
-for (let index = 0; index < 5; index += 1) {
-  makeSquare = document.createElement('td');
-  makeRow = document.createElement('tr');
-  getSquare.appendChild(makeRow);
-  for (let indexSquare = 0; indexSquare <= 5; indexSquare += 1) {
-    getSquare.appendChild(makeSquare);
-    makeSquare = document.createElement('td');
-    makeSquare.className = tdClass;
+function makeTable() {
+  for (let index = 0; index < 5; index += 1) {
+    makeRow = document.createElement('tr');
+    getSquare.appendChild(makeRow);
+    for (let indexSquare = 0; indexSquare < 5; indexSquare += 1) {
+      getSquare.appendChild(makeSquare);
+      makeSquare = document.createElement('td');
+      makeSquare.className = tdClass;
+    }
   }
 }
+
+makeTable();
