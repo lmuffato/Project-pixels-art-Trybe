69619de57c583684498a2771cd8673726d5ec883
André Dantas <aluizcfd@gmail.com>
2021-03-09 18:28:42 -0300

    add requirements 4 and 5

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const colorSecond = document.querySelector('.second');
 const colorThird = document.querySelector('.third');
 const colorFourth = document.querySelector('.fourth');
 
+
+
 /* Randomizador de cores estudado e reproduzido daqui: https://stackoverflow.com/questions/1484506/random-color-generator  */
 
 function randomizeColor() {
@@ -23,3 +25,23 @@ function injectColors() {
 }
 
 injectColors();
+
+function createPixelBoard() {
+  const pixelBoard = document.querySelector('#pixel-board');
+
+  for (let i = 1; i <= 5; i += 1) {
+    const pixelBoardColumn = document.createElement('tr');
+    pixelBoardColumn.className = 'line';
+
+    pixelBoard.appendChild(pixelBoardColumn);
+
+    for (let j = 1; j <= 5; j += 1) {
+      const pixelBoardLine = document.createElement('td');
+      pixelBoardLine.className = 'pixel';
+
+      pixelBoardColumn.appendChild(pixelBoardLine);
+    }
+  }
+}
+
+createPixelBoard();
