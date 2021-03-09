e94af8567af0af85face004093713c2c83c3333a
Douglas <douguinhodns@gmail.com>
2021-03-09 02:51:21 -0300

    requisitos do 1 ao 3 concluidos

---
+++ script.js
@@ -0,0 +1,6 @@
+let color = document.querySelectorAll('.color');
+color[0].style.backgroundColor = 'black';
+color[1].style.backgroundColor = 'pink';
+color[2].style.backgroundColor = 'yellow';
+color[3].style.backgroundColor = 'purple';
+
