b2a3900b2e91266c33bbab280ef3203c863783d7
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-03-09 20:54:21 -0300

    requisito 6 e 7 mais uma vez

--- script.js
+++ script.js
@@ -7,15 +7,20 @@
 
 
 
+
  function selecionaCor (){
-     
+
+    let CorPreta = document.getElementsByClassName('selected');
+    
     let paletaCor = document.querySelectorAll('.color');
 
-    for (let index = 0; index < paleta.length; index += 1) {
+    for (let index = 0; index < paletaCor.length; index += 1) {
         paletaCor[index].addEventListener('click', function(){
         paletaCor[index].className = '.selected'; 
+        CorPreta.className = '.color'
         });
     }
+
     let selecionado = document.querySelector('.selected');
     return selecionado;
  }
@@ -23,7 +28,7 @@
 
  function colocaCor (){
 
-
+    let pixel =
 
  }
 
