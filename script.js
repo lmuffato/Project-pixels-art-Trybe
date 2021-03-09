9b9aef8b6f6cf9688cbe7af83f7bb442b1f94d47
Sérgio Martins <sergiomos2010@gmail.com>
2021-03-09 17:20:20 -0300

    criando pixel board dinamicamente e pintando os pixels

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-const color = '#fff';
+let color = 'fff';
+
+function setColor(colorName) {
+  if (colorName) {
+    color = colorName;
+  }
+}
+
+function selectColor(Event) {
+  const newColor = Event.target;
+  const isSelected = newColor.className.includes('selected');
+
+  if (!isSelected) {
+    const selectedColor = document.querySelector('.selected');
+    // referencia sobre classList https://www.w3schools.com/howto/howto_js_remove_class.asp
+    selectedColor.classList.remove('selected');
+    newColor.classList.add('selected');
+  }
+  setColor(newColor.style.backgroundColor);
+}
 
 function createColorCell(colorName) {
   const colorCell = document.createElement('li');
@@ -17,7 +36,7 @@ function createColorCell(colorName) {
   return colorCell;
 }
 
-function createColorPalette() {
+function fillColorPalette() {
   const colors = ['black', 'red', 'yellow', 'blue'];
 
   for (let colorUnit = 0; colorUnit < colors.length; colorUnit += 1) {
@@ -26,17 +45,37 @@ function createColorPalette() {
   }
 }
 
-createColorPalette();
-
-function selectColor(Event) {
-  const newColor = Event.target;
-  const isSelected = newColor.className.includes('selected');
-
-  if (!isSelected) {
-    const selectedColor = document.querySelector('.selected');
-    // referencia sobre classList https://www.w3schools.com/howto/howto_js_remove_class.asp
-    selectedColor.classList.remove('selected');
-    newColor.classList.add('selected');
-  }
-  console.log(newColor.style.backgroundColor);
+function paintPixel(Event) {
+  const pixel = Event.target;
+  pixel.style.backgroundColor = color;
 }
+
+function createPixel() {
+  const pixel = document.createElement('div');
+  pixel.className = 'pixel';
+  pixel.addEventListener('click', paintPixel);
+
+  return pixel;
+}
+
+function createPixelRow() {
+  const pixelRow = document.createElement('div');
+  pixelRow.className = 'pixel-row';
+  for (let cells = 1; cells <= 5; cells += 1) {
+    pixelRow.appendChild(createPixel());
+  }
+
+  return pixelRow;
+}
+
+function fillPixelBoard() {
+  const pixelBoard = document.getElementById('pixel-board');
+  for (let rows = 1; rows <= 5; rows += 1) {
+    pixelBoard.appendChild(createPixelRow());
+  }
+}
+
+window.onload = () => {
+  fillColorPalette();
+  fillPixelBoard();
+};
