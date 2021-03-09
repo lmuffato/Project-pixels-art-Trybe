31955178e3b7510043759d31c99a432e7b0cef32
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-03-09 16:42:33 -0300

    requisito 5 e 6

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+    
+  };
+
+ function selecionaCor (){
+    let paleta = document.querySelectorAll('.color');
+
+    paleta.addEventListener('click', function() {
+    let cor = document.querySelector('.cor1');       
+  })
+ } 
