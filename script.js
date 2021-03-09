06c3fed6549f498c43fff0ac6d703b4369639975
LuanAlexandre <luanrb19@gmail.com>
2021-03-09 19:37:57 -0400

    Dia 5.5 - projeto-pixels-art: requisito 4.v2

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 window.onload = function pixelsArt() {
   const pixelLine = document.getElementById('pixel-board');
 
-  for (let indexLine = 0; indexLine < 5; indexLine += 1) {
-    const pixel = document.createElement('div');
-    pixelLine.appendChild(pixel).className = 'pixel';
+  for (let index = 0; index < 5; index += 1) {
+    const pixel = document.createElement('tr');
+    pixelLine.appendChild(pixel);
   }
 
   for (let indexLine = 0; indexLine < 5; indexLine += 1) {
     let indexColumn = 0;
     for (indexColumn; indexColumn < 5; indexColumn += 1) {
-      const pixel = document.createElement('div');
+      const pixel = document.createElement('td');
       pixelLine.children[indexColumn].appendChild(pixel).className = 'pixel';
     }
   }
