f7f79967e84041e536d872c4dd580ad69f187089
Carol <carolinavpinheiro@gmail.com>
2021-03-09 18:39:59 -0300

    Adicionando exercício 6

---
+++ script.js
@@ -0,0 +1,8 @@
+const colorBlack = document.querySelector('#black');
+const colorPink = document.querySelector('#pink');
+const colorGreen = document.querySelector('#green');
+const colorOrange = document.querySelector('#orange');
+
+colorBlack.addEventListener('click', function (){
+    colorBlack.classList = "selected";
+})
