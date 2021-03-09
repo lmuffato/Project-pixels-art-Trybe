45a5e15bc77968cecdd2aabc26c1fc07c2b008fb
lucas-paz <lucas.hpaz@outlook.com>
2021-03-09 19:04:03 -0300

    Adiciona requisitos 3 e 4 e refatora o código

--- script.js
+++ script.js
@@ -2,13 +2,26 @@
 function paletteBox() {
     let colors = ['black', 'blue', 'green', 'yellow', 'red', 'cyan', 'brown']; //Cores da paleta
     let paletteSelect = document.getElementById('color-palette');//acessa a div mãe
-    let baxesOut = [];
     for (let index = 0; index < colors.length; index += 1) {
         let paletteCreator = document.createElement('div'); //cria uma div
         paletteCreator.style.background = colors[index];
         paletteSelect.appendChild(paletteCreator).className = 'color'; //cria divs como filhas da div mãe
-
     }
-    return boxOut;
 }
 paletteBox();
+
+function accessElement(tagId, elementType, position) {
+    let access = document.querySelectorAll(tagId);
+    let create = document.createElement(elementType);
+    let print = access[position].appendChild(create);
+    return print;
+}
+
+for (let index = 1; index < 6; index += 1) {
+    accessElement('#pixel-board', 'div', 0).classList.add('tr','pixel');
+}
+for (let index =0; index < 5; index += 1) {
+    for (let index2 = 0; index2 < 5; index2 += 1){
+    accessElement('.tr', 'div', index).classList.add('td','pixel');
+    }
+}
