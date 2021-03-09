0f81ccb477da1e038b354f86fdfef16033cbaa89
Robertson <robertson.nunes@gmail.com>
2021-03-09 19:13:52 -0300

    requisito 5

--- script.js
+++ script.js
@@ -2,11 +2,10 @@ const colorBlocks = document.querySelector('#pixel-board');
 const  n = 5
 for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
     let block = document.createElement('div');
-    block.className = 'pixel';
     colorBlocks.appendChild(block);
     for (let columnIndex = 0; columnIndex < n; columnIndex += 1) {
         const Vblocks = document.createElement('div');
-        colorBlocks.appendChild(Vblocks);
+        block.appendChild(Vblocks);
         Vblocks.classList = 'pixel';
     }
 }
