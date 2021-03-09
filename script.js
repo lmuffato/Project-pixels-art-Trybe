58c15a05aa318ac9c7bcb4fe41de355d16f94676
walvesb65 <willian.alvesb65@gmail.com>
2021-03-09 17:57:22 -0300

    add tabela de cores ok

---
+++ script.js
@@ -0,0 +1,5 @@
+let color = document.querySelectorAll('.color')
+color[0].style.backgroundColor = 'black'
+color[1].style.backgroundColor = 'green'
+color[2].style.backgroundColor = 'chartreuse'
+color[3].style.backgroundColor = 'orange'
