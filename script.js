36d009a600f45b86831c496d57c45e3a6ebfea53
Lucas André <lucas.cancado89@gmail.com>
2021-03-09 20:38:45 -0300

    Requisito 10 again

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-function sizeOfBoard() {
-  const boardSize = document.querySelector('#board-size').value;
+for (let index = 0; index < 5; index += 1) {
+  const tagLinha = document.createElement('div');
+  tagLinha.className = 'line';
+  tagLinha.style.height = '42px';
+  document.querySelector('#pixel-board').appendChild(tagLinha);
+  for (let i = 0; i < 5; i += 1) {
+    const tagPixel = document.createElement('div');
+    tagPixel.className = 'pixel';
+    tagPixel.style.backgroundColor = 'white';
+    tagPixel.style.width = '40px';
+    tagPixel.style.height = '40px';
+    tagPixel.style.display = 'inline-block';
+    tagPixel.style.border = 'solid black 1px';
+    document.querySelectorAll('.line')[index].appendChild(tagPixel);
+  }
+}
+
+function addPixel() {
+  const boardSize = document.querySelector('#board-size').value - 5;
   if (boardSize === '') { alert('Board inválido!'); }
-  for (let index = 0; index < boardSize; index += 1) {
-    const tagLinha = document.createElement('div');
-    tagLinha.className = 'line';
-    tagLinha.style.height = '42px';
-    document.querySelector('#pixel-board').appendChild(tagLinha);
+  for (let index = 0; index < 5; index += 1) {
     for (let i = 0; i < boardSize; i += 1) {
       const tagPixel = document.createElement('div');
       tagPixel.className = 'pixel';
@@ -19,7 +32,29 @@ function sizeOfBoard() {
   }
 }
 
-document.querySelector('#generate-board').addEventListener('click', sizeOfBoard);
+document.querySelector('#generate-board').addEventListener('click', addPixel);
+
+function addLine() {
+  const boardSize = document.querySelector('#board-size').value;
+  for (let index = 0; index < boardSize - 5; index += 1) {
+    const tagLinha = document.createElement('div');
+    tagLinha.className = 'line';
+    tagLinha.style.height = '42px';
+    document.querySelector('#pixel-board').appendChild(tagLinha);
+    for (let i = 0; i < boardSize; i += 1) {
+      const tagPixel = document.createElement('div');
+      tagPixel.className = 'pixel';
+      tagPixel.style.backgroundColor = 'white';
+      tagPixel.style.width = '40px';
+      tagPixel.style.height = '40px';
+      tagPixel.style.display = 'inline-block';
+      tagPixel.style.border = 'solid black 1px';
+      document.querySelectorAll('.line')[index + 5].appendChild(tagPixel);
+    }
+  }
+}
+
+document.querySelector('#generate-board').addEventListener('click', addLine);
 
 function selectColor(event) {
   for (let index = 0; index < 4; index += 1) {
