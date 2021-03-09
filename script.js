2ab02da0ee62ff68873bef6897c1b229d8630bb9
Gustavo Sousa <gustavohl98@gmail.com>
2021-03-09 16:14:56 -0300

    Requisito 03 - cor preta primeiro + correcoes lint

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-window.onload = function() {
-  const colorPalette = document.getElementById('color-palette');
-  const colorDivs = document.querySelectorAll('.color');
+// const colorPalette = document.getElementById('color-palette');
+const colorDivs = document.querySelectorAll('.color');
 
-  const paletteColors = ['black', 'blue', 'red', 'green']
+const paletteColors = ['black', 'blue', 'red', 'green'];
 
-  function setPaletteColors() {
-    for (let index = 0; index < paletteColors.length; index += 1) {
-      const colorDiv = colorDivs[index];
-      const currentColor = paletteColors[index];
+function setPaletteColors() {
+  for (let index = 0; index < paletteColors.length; index += 1) {
+    const colorDiv = colorDivs[index];
+    const currentColor = paletteColors[index];
 
-      colorDiv.style.backgroundColor = currentColor;
-    }
+    colorDiv.style.backgroundColor = currentColor;
   }
-
-  setPaletteColors()
-
 }
+
+setPaletteColors();
