06059c80dcff9e52ba1a4af4cebec4fdfca29b9a
Diogo Augusto <diogotrescastro@gmail.com>
2021-03-09 16:56:40 -0300

    Resoluçao do requisito 7

--- script.js
+++ script.js
@@ -1,7 +1,48 @@
+
+const getBlack = document.getElementById('black');
+const getYellow = document.getElementById('yellow');
+const getBlue = document.getElementById('blue');
+const getMagenta = document.getElementById('magenta');
+
 function defaultColor() {
-  const getBlack = document.getElementById('black');
-  const defaultClass = getBlack.classList.add('selected');
-  return defaultClass;
+  const defaultBlackClass = getBlack.className = 'color selected';
+  return defaultBlackClass;
 }
 
 defaultColor();
+
+function setColorBlack() {
+  getBlack.className = 'color selected';
+  getMagenta.className = 'color';
+  getYellow.className = 'color';
+  getBlue.className = 'color';
+}
+
+getBlack.addEventListener('click', setColorBlack);
+
+function setColorMagenta() {
+  getBlack.className = 'color';
+  getMagenta.className = 'color selected';
+  getYellow.className = 'color';
+  getBlue.className = 'color';
+}
+
+getMagenta.addEventListener('click', setColorMagenta);
+
+function setColorYellow() {
+  getBlack.className = 'color';
+  getMagenta.className = 'color';
+  getYellow.className = 'color selected';
+  getBlue.className = 'color';
+}
+
+getYellow.addEventListener('click', setColorYellow);
+
+function setColorBlue() {
+  getBlack.className = 'color';
+  getMagenta.className = 'color';
+  getYellow.className = 'color';
+  getBlue.className = 'color selected';
+}
+
+getBlue.addEventListener('click', setColorBlue);
