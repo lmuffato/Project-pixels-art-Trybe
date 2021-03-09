ddc9d07897231a44a95d84d6b7a7c2645a6ac1e8
Paulo H. Lopes <phlopes93@gmail.com>
2021-03-09 16:59:31 -0300

    requisito 2

---
+++ script.js
@@ -0,0 +1,7 @@
+let colorRed = document.querySelectorAll('div')[1].style.backgroundColor = 'red';
+let colorGreen = document.querySelectorAll('div')[2].style.backgroundColor = 'green';
+let colorBlue = document.querySelectorAll('div')[3].style.backgroundColor = 'blue';
+let colorYellow = document.querySelectorAll('div')[4].style.backgroundColor = 'yellow';
+
+
+
