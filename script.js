37e8243df698c63b9f64c1b7c7b1a5cf2ed948da
Cla Oliveira <clarice.coliveira@yahoo.com.br>
2021-03-09 20:44:42 -0300

    Requisito 7 - Classe 'selected'

---
+++ script.js
@@ -0,0 +1,39 @@
+const colorOne = document.getElementsByClassName('color-one')[0];
+const colorTwo = document.getElementsByClassName('color-two')[0];
+const colorThree = document.getElementsByClassName('color-three')[0];
+const colorFour = document.getElementsByClassName('color-four')[0];
+
+colorOne.addEventListener('click', () => {
+  colorOne.classList.add('selected');
+  colorTwo.classList.remove('selected');
+  colorThree.classList.remove('selected');
+  colorFour.classList.remove('selected');
+  console.log('Cliquei no preto');
+});
+
+colorTwo.addEventListener('click', () => {
+/** Source: https://javascript.info/styles-and-classes */
+  colorOne.classList.remove('selected');
+  colorTwo.classList.add('selected');
+  colorThree.classList.remove('selected');
+  colorFour.classList.remove('selected');
+  console.log('Cliquei no vermelho');
+});
+
+colorThree.addEventListener('click', () => {
+/** Source: https://javascript.info/styles-and-classes */
+  colorOne.classList.remove('selected');
+  colorTwo.classList.remove('selected');
+  colorThree.classList.add('selected');
+  colorFour.classList.remove('selected');
+  console.log('Cliquei no roxo');
+});
+
+colorFour.addEventListener('click', () => {
+/** Source: https://javascript.info/styles-and-classes */
+  colorOne.classList.remove('selected');
+  colorTwo.classList.remove('selected');
+  colorThree.classList.remove('selected');
+  colorFour.classList.add('selected');
+  console.log('Cliquei no verde');
+});
