5abcbf5a161a64bfab0ae309bc6c2be6996f0d4a
Stanrley Barbosa <3011stan@gmail.com>
2021-03-09 15:31:43 -0300

    Requisito 7 concluido

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
 let selectColorPalette = document.getElementById('color-palette');
 let selectPixelBoard = document.getElementById('pixel-board');
 let colors = ['black', 'red', 'blue', 'green'];
-
-createPalette(); // cria os 4 quadrados(cores) e os adiciona como filho da div de id = 'color-palette'
-createBoardPixel(); //cria os nxn quadrados.
-
-window.onload = function() {
+window.onload = function () {
   let setSelected = selectColorPalette.firstElementChild;
   setSelected.className = 'color selected';
 }
 
+createPalette(); // cria os 4 quadrados(cores) e os adiciona como filho da div de id = 'color-palette'
+createBoardPixel(); //cria os nxn quadrados.
+captureColor(); // seta a cor ao clicar no quadrado escolhido.
+function paintPixel() {
+  selectPixelBoard.addEventListener('click', function(event) {
+    console.log('testando');
+  })
+}
+paintPixel();
+
+function captureColor() {
+  let getElementsPalette = selectColorPalette.getElementsByClassName('color');
+  selectColorPalette.addEventListener('click', function (event) {
+    for (let i = 0; i < 4; i += 1) {
+      setColor(getElementsPalette, i, event); //acrescenta class selected ao elemento q provocou a função
+    }
+  });
+};
+
+function setColor(getElementsPalette, i, event) {
+  getElementsPalette[i].className = 'color'; //seta todas as classes das cores como apenas 'color';
+  event.target.className = 'color selected';
+};
+
 function createBoardPixel() {
-  for (let i = 0; i < 5; i += 1) { 
+  for (let i = 0; i < 5; i += 1) {
     for (let j = 0; j < 5; j += 1) {
       let newPixel = document.createElement('div');
       newPixel.className = 'pixel';
