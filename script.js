cf179d2f48adf6f55d73373666b4f6c453b96180
Luiz Felipe Araujo <thelastnailmusic@gmail.com>
2021-03-09 17:00:36 -0300

    Revisao de Erros

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function criaPaleta(numberOfFrames) {
-  for (let index = 0; index < numberOfFrames; index +=1) {
-    let pegaPaleta = document.getElementById("color-palette");
-    let criaPaleta = document.createElement("div")
+  for (let index = 0; index < numberOfFrames; index += 1) {
+    const pegaPaleta = document.getElementById('color-palette');
+    const criaPaleta = document.createElement('div');
     criaPaleta.className = 'color';
     pegaPaleta.appendChild(criaPaleta);
   }
@@ -9,21 +9,21 @@ function criaPaleta(numberOfFrames) {
 criaPaleta(4);
 
 function colorePaletaSuperior() {
-  document.getElementsByClassName('color')[0].style.backgroundColor = "black";
-  document.getElementsByClassName('color')[1].style.backgroundColor = "orange";
-  document.getElementsByClassName('color')[2].style.backgroundColor = "olive";
-  document.getElementsByClassName('color')[3].style.backgroundColor = "blue";
+  document.getElementsByClassName('color')[0].style.backgroundColor = 'black';
+  document.getElementsByClassName('color')[1].style.backgroundColor = 'orange';
+  document.getElementsByClassName('color')[2].style.backgroundColor = 'olive';
+  document.getElementsByClassName('color')[3].style.backgroundColor = 'blue';
 }
 colorePaletaSuperior();
 
 function criaQuadro(numberOfFrames) {
   for (let index = 0; index < numberOfFrames * numberOfFrames; index += 1) {
-    let criaDiv = document.createElement('div');
+    const criaDiv = document.createElement('div');
     criaDiv.className = 'pixel';
-    let pegaDiv = document.getElementById('pixel-board');
+    const pegaDiv = document.getElementById('pixel-board');
     pegaDiv.append(criaDiv);
   }
 }
 criaQuadro(5);
 
-document.getElementsByClassName('color')[0]
+// Funções para Criar os Eventos de Mouse
