fddf299509806177fe66cf567debc8a787eee0f5
Douglas <douguinhodns@gmail.com>
2021-03-09 04:23:07 -0300

    requisito 4 concluido

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ color[1].style.backgroundColor = 'pink';
 color[2].style.backgroundColor = 'yellow';
 color[3].style.backgroundColor = 'purple';
 
+let pixel = document.querySelector('#pixel-board');
+let size = 5;
+for (let index = 1; index <= size; index += 1) {
+    let box = document.createElement('span');
+    pixel.appendChild(box);
+    box.className = 'boxes';
+    for (let index2 = 1; index2 <= size; index2 += 1) {
+        let line = document.createElement('span');
+        box.appendChild(line);
+        line.className = 'pixel';
+    }
+}
