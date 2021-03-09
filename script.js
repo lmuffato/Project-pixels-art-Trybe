5624f95dd62726a43d58144a0e1a7a15298dcb02
Victor Ricco <victor.ricco001@gmail.com>
2021-03-09 15:43:07 -0300

    atualizacoes

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-window.onload{
-	
+window.onload = function(){
+  let colorSelectedString = document.querySelector('#color-select');
+  colorSelectedString.innerText = 'black';
+
+  let colorInfo = document.querySelector('#color-palette');
+
+	colorInfo.addEventListener('click', function(event){
+		let colorName = event.target.id;
+		if(colorName === 'color-palette'){
+			colorSelectedString.innerText = 'Nenhuma cor selecionada'
+		}else{
+      colorSelectedString.innerText = colorName
+		}
+	});
 }
