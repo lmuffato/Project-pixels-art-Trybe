df70bdfcd6a615572d015de120a8cfb4dad8e5a2
Felipe Muller <felipeavmuller@gmail.com>
2021-03-09 20:16:41 -0300

    teste

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 window.onload = function() {
   let initialColor = document.querySelector('.color');
   initialColor.className += ' selected';
+
+  document.getElementById('color1').style.backgroundColor = 'black';
+  document.getElementById('color2').style.backgroundColor = '#14213d';
+  document.getElementById('color3').style.backgroundColor = '#fca311';
+  document.getElementById('color4').style.backgroundColor = '#e5e5e5';
+
 }
 
-function changeColor() {
+function changeSelectedColor() {
   let currentColor = document.querySelector('.selected');
   let allColors = document.querySelector('#color-palette');
   let colors = allColors.children;
@@ -14,5 +20,4 @@ function changeColor() {
     } 
   })
 }
-
-changeColor();
+changeSelectedColor();
