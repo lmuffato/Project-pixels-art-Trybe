407c1f2e55b7ffa6fd1335cfad9363297f68c9c3
Edrei <edreimateus@gmail.com>
2021-03-09 16:04:43 -0300

    Estiliza input e botões

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function generateRandomColors() {
   const num = 4;
-  for(let index = 0; index < num; index += 1) {
+  for (let index = 0; index < num; index += 1) {
     const palette = document.querySelector('#color-palette');
     const colorDiv = document.createElement('div');
     colorDiv.className = 'color';
@@ -9,6 +9,7 @@ function generateRandomColors() {
       colorDiv.style.backgroundColor = 'black';
       colorDiv.classList.add('selected');
     } else {
+      // eslint-disable-next-line max-len
       colorDiv.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)} , ${Math.floor(Math.random() * 256)} , ${Math.floor(Math.random() * 256)})`;
     }
     palette.appendChild(colorDiv);
@@ -16,4 +17,3 @@ function generateRandomColors() {
 }
 
 generateRandomColors();
-
