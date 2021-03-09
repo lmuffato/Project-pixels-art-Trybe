69d65563f160e025e07146f3eaa6b6b29c52429c
Edmilson Bernardo <ebedmilson48@gmail.com>
2021-03-09 18:04:54 -0300

    aplica correções para os requisitos 4 e 5

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //Aplica cores na paleta;
 function toPaintbox() {
-  const box = document.querySelectorAll('.color')
-  box[0].style.backgroundColor = 'black';
-  box[1].style.backgroundColor = 'blue';
-  box[2].style.backgroundColor = 'red';
-  box[3].style.backgroundColor = 'yellow';
+    const box = document.querySelectorAll('.color')
+    box[0].style.backgroundColor = 'black';
+    box[1].style.backgroundColor = 'blue';
+    box[2].style.backgroundColor = 'red';
+    box[3].style.backgroundColor = 'yellow';
 }
 toPaintbox();
