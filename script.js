778aabcf37e3bf57c7580f3dd15c9eb598b15a11
Viegas <mvrdgs@gmail.com>
2021-03-09 17:51:14 -0300

    requisito 9

--- script.js
+++ script.js
@@ -1,30 +1,48 @@
+function getSelectedColor(event) {
+  const pixel = event.target;
+  if (pixel.className.includes('pixel')) {
+    const color = document.querySelector('.selected');
+    pixel.style.backgroundColor = color.style.backgroundColor;
+  }
+}
+
+function selectColor(event) {
+  const selectedColor = event.target;
+  document.querySelector('.selected').className = 'boxes color';
+  selectedColor.className += ' selected';
+}
+
+function pixelPaint() {
+  const colorSelect = document.querySelector('#color-palette');
+  const pixelBoard = document.querySelector('#pixel-board');
+  colorSelect.addEventListener('click', selectColor);
+  pixelBoard.addEventListener('click', getSelectedColor);
+}
+
+function setColor(colorsArray) {
+  const boxColors = document.querySelectorAll('.color');
+  const colors = colorsArray;
+
+  for (let index = 0; index < boxColors.length; index += 1) {
+    boxColors[index].style.backgroundColor = colors[index];
+  }
+}
+
+function clearPainel() {
+  const painel = document.querySelectorAll('.pixel');
+
+  for (let index = 0; index < painel.length; index += 1) {
+    painel[index].style.backgroundColor = 'white';
+  }
+}
+
+function clear() {
+  const button = document.getElementById('clear-board');
+  button.addEventListener('click', clearPainel);
+}
+
 window.onload = function script() {
-  function setColor(colorsArray) {
-    const boxColors = document.querySelectorAll('.color');
-    const colors = colorsArray;
-
-    for (let index = 0; index < boxColors.length; index += 1) {
-      boxColors[index].style.backgroundColor = colors[index];
-    }
-  }
-
   setColor(['black', 'blue', 'green', 'yellow']);
-
-  function pixelPaint() {
-    let colorSelect = document.querySelector('#color-palette');
-    let pixelBoard = document.querySelector('#pixel-board')
-
-    colorSelect.addEventListener('click', function(event){
-      let selectedColor = event.target;
-      document.querySelector('.selected').className = 'boxes color';
-      selectedColor.className += ' selected';
-    });
-
-    pixelBoard.addEventListener('click', function(event){
-      let pixel = event.target;
-      let color = document.querySelector('.selected');
-      pixel.style.backgroundColor = color.style.backgroundColor;
-    });
-  }
   pixelPaint();
+  clear();
 };
