e78439dc2c25a26c0fe9fb111dca3c1016b1f50b
nascjoao <oi@joaonasc.dev>
2021-03-09 17:32:19 -0300

    Cores da paleta sao definidas no JavaScript

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const paletteColors = document.querySelectorAll('#color-palette .color');
 
+const colors = {
+  black: 'black',
+  red: '#f72c25',
+  green: '#2d936c',
+  blue: '#4f7cac',
+};
+
+function setColorPalette(element) {
+  const colorOption = element;
+  if (colors[colorOption.id]) {
+    colorOption.style.backgroundColor = colors[colorOption.id];
+  }
+}
+
 function selectColor(element) {
   for (let index = 0; index < paletteColors.length; index += 1) {
     const color = paletteColors[index];
@@ -13,5 +27,6 @@ function selectColor(element) {
 
 for (let index = 0; index < paletteColors.length; index += 1) {
   const colorElement = paletteColors[index];
+  setColorPalette(colorElement);
   colorElement.onclick = selectColor;
 }
