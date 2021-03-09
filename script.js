d788e3507b0814872a23cf8c29e9f23c77d0988a
luanlsr <luanfswd@gmail.com>
2021-03-09 16:13:31 -0300

    corrigido requisitos 3, 4 e 5

---
+++ script.js
@@ -0,0 +1,29 @@
+const element1 = document.getElementsByClassName('color')[0]
+const element2 = document.getElementsByClassName('color')[1]
+const element3 = document.getElementsByClassName('color')[2]
+const element4 = document.getElementsByClassName('color')[3]
+
+
+element1.addEventListener('click', () => {
+  console.log('Cliquei no preto');
+  //handle click
+})
+console.log(element1)
+
+element2.addEventListener('click', () => {
+  console.log('Cliquei no vermelho');
+  //handle click
+})
+console.log(element1)
+
+element3.addEventListener('click', () => {
+  console.log('Cliquei no azul');
+  //handle click
+})
+console.log(element1)
+
+element4.addEventListener('click', () => {
+  console.log('Cliquei no verde');
+  //handle click
+})
+console.log(element1)
