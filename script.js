280438b0cec0efc84794c7778d7dc2076092712b
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-03-09 18:34:35 -0300

    Some refactoring and done 8

--- script.js
+++ script.js
@@ -14,18 +14,29 @@ function setInitialColor() {
   const firstColor = document.getElementsByClassName('color')[0];
   firstColor.classList.toggle('selected');
 }
-function changeSelectedColor(e) {
+function changePaletteColor(e) {
   const lastSelected = document.querySelector('#color-palette > .selected');
   lastSelected.classList.toggle('selected');
   e.target.classList.toggle('selected');
 }
-function createColorPalleteListener() {
-  const container = document.getElementById('color-palette');
-  container.addEventListener('click', changeSelectedColor, false);
+function createColorPaletteListener() {
+  const colorPalette = document.getElementById('color-palette');
+  colorPalette.addEventListener('click', changePaletteColor, false);
+}
+function changePixelColor(e) {
+  const selectedPaletteColor = document.querySelector('#color-palette > .selected');
+  let newColor = getComputedStyle(selectedPaletteColor).backgroundColor;  
+  e.target.style.backgroundColor = newColor;
+  
+}
+function createPixelsListener() {
+  const pixelBoard = document.getElementById('pixels-board');
+  pixelBoard.addEventListener('click', changePixelColor, false);
 }
 
 window.onload = function init() {
   createCanvas(5, 5);
   setInitialColor();
-  createColorPalleteListener();
+  createColorPaletteListener();
+  createPixelsListener();
 };
