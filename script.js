a4398f0b417f8a89b39d00550e602f001b1bb2d7
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-03-09 17:31:42 -0300

    Second requirement done

---
+++ script.js
@@ -0,0 +1,15 @@
+window.onload = () => {
+  function colorPalette() {
+    const colors = ['red', 'green', 'blue', 'black'];
+    const colorsFather = document.querySelector('#color-palette');
+
+    for (let index = 0; index < colors.length; index += 1) {
+      const elementColor = document.createElement('li');
+
+      elementColor.classList.add('color');
+      elementColor.style.backgroundColor = `${colors[index]}`;
+      colorsFather.appendChild(elementColor);
+    }
+  }
+  colorPalette();
+};
