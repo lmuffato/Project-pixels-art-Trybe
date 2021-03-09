67b9d39c90a1925280cc9707213a601bd0fd5c75
Carlos Campo <carlosgabrielcampo@gmail.com>
2021-03-09 15:48:55 -0300

    requisito 1 e 2 fix

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
-let colorPalette = document.getElementById('color-palette')
+const colorPalette = document.getElementById('color-palette');
 
-
-
-function createPaletteBlocks() { 
-
+function createPaletteBlocks() {
   for (let index = 0; index <= 3; index += 1) {
-    let paletteBlocks = document.createElement('div')
-    paletteBlocks.className = 'color'    
-    colorPalette.appendChild(paletteBlocks)
+    const paletteBlocks = document.createElement('div');
+    paletteBlocks.className = 'color';
+    colorPalette.appendChild(paletteBlocks);
   }
 }
 
-createPaletteBlocks()
+createPaletteBlocks();
 
 function blockColor() {
-
-  let colorBlock = document.getElementsByClassName('color')
-  colorBlock[0].style.backgroundColor = "black"
-  for(index = 1; index < colorBlock.length; index += 1){
-      colorBlock[index].style.backgroundColor = `rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255})`
+  let colorBlock = document.getElementsByClassName('color');
+  colorBlock[0].style.backgroundColor = 'black';
+  for (let index = 1; index < colorBlock.length; index += 1) {
+      colorBlock[index].style.backgroundColor = `rgb(${Math.random() * 255},${Math.random() * 255},${Math.random() * 255})`;
   }
 }
 
-blockColor()
+blockColor();
