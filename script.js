fc49565268ec3261df40facf71b1939d36ccb77f
Felipe Muller <felipeavmuller@gmail.com>
2021-03-09 18:20:42 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 window.onload = function() {
-  let firstColor = document.querySelector('.color');
-  firstColor.className += ' selected';
+  let initialColor = document.querySelector('.color');
+  initialColor.className += ' selected';
 }
 
+function changeColor() {
+  let currentColor = document.querySelector('.selected');
+  let allColors = document.querySelector('#color-palette');
+  let colors = allColors.children;
+  allColors.addEventListener('click', function(event) {
+    if (event.target.className === 'color') {
+      document.querySelector('.selected').className = 'color';
+      event.target.className += ' selected';
+    } 
+  })
+}
+
+changeColor();
