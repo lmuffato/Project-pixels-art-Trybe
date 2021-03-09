cd7611c63b30351fb084378c996bc1fb63f141ff
Sérgio Martins <sergiomos2010@gmail.com>
2021-03-09 16:09:07 -0300

    Requisito 7: selecinando as cores

---
+++ script.js
@@ -0,0 +1,12 @@
+function selectColor(Event) {
+  const newColor = Event.target;
+  const isSelected = newColor.className.includes('selected');
+
+  if (!isSelected) {
+    const selectedColor = document.querySelector('.selected');
+    console.log(selectedColor);
+    // referencia sobre classList https://www.w3schools.com/howto/howto_js_remove_class.asp
+    selectedColor.classList.remove('selected');
+    newColor.classList.add('selected');
+  }
+}
