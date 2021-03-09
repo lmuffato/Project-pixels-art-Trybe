1b52ef3b5a1cd8064b43b2e4b35a56007b3e0207
Elisa <elisafranca.carvalho@gmail.com>
2021-03-09 20:33:05 -0300

    Refaz quadro do requisito 2 como tabela e corrige exigencias do lint

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-window.onload = function startSession() {
-  initiateColor();
-};
-
 function initiateColor() {
   const pixels = document.getElementsByClassName('pixel');
   // Para este requisito consultei os seguintes links:
   // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/from
   // https://www.alura.com.br/artigos/javascript-quando-devo-usar-foreach-e-map?gclid=Cj0KCQiA1pyCBhCtARIsAHaY_5cscwZQUJTOMnmG6B8naM2D5eFCmquk0s-168iGWrcyGy1gb6YSgEMaAmwpEALw_wcB
-  Array.from(pixels).forEach((Element) => {
-    Element.style.backgroundColor = 'white';
+  Array.from(pixels).forEach((element) => {
+    const pixel = element;
+    pixel.style.backgroundColor = 'white';
   });
 }
+
+window.onload = function startSession() {
+  initiateColor();
+};
