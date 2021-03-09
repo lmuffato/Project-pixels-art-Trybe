6945d9307fd095f295dfb6c8d369c8eeb32a43c7
Carlos Campo <carlosgabrielcampo@gmail.com>
2021-03-09 15:39:40 -0300

    requisito 1...3

---
+++ script.js
@@ -0,0 +1,25 @@
+let colorPalette = document.getElementById('color-palette')
+
+
+
+function createPaletteBlocks() { 
+
+  for (let index = 0; index <= 3; index += 1) {
+    let paletteBlocks = document.createElement('div')
+    paletteBlocks.className = 'color'    
+    colorPalette.appendChild(paletteBlocks)
+  }
+}
+
+createPaletteBlocks()
+
+function blockColor() {
+
+  let colorBlock = document.getElementsByClassName('color')
+  colorBlock[0].style.backgroundColor = "black"
+  for(index = 1; index < colorBlock.length; index += 1){
+      colorBlock[index].style.backgroundColor = `rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255})`
+  }
+}
+
+blockColor()
