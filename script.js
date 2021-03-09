d60f549c1d5b318cdf3779dcd14dd7170f460cd5
Edrei <edreimateus@gmail.com>
2021-03-09 15:01:58 -0300

    Adiciona arquivos iniciais

---
+++ script.js
@@ -0,0 +1,19 @@
+function generateRandomColors() {
+  const num = 4;
+  for(let index = 0; index < num; index += 1) {
+    const palette = document.querySelector('#color-palette');
+    const colorDiv = document.createElement('div');
+    colorDiv.className = 'color';
+
+    if (index === 0) {
+      colorDiv.style.backgroundColor = 'black';
+      colorDiv.classList.add('selected');
+    } else {
+      colorDiv.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)} , ${Math.floor(Math.random() * 256)} , ${Math.floor(Math.random() * 256)})`;
+    }
+    palette.appendChild(colorDiv);
+  }
+}
+
+generateRandomColors();
+
