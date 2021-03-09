8c3500bc689ed183fd565705bc4af6f85f2cfbff
Edmilson Bernardo <ebedmilson48@gmail.com>
2021-03-09 16:45:35 -0300

    Adiciona requisito 2

---
+++ script.js
@@ -0,0 +1,9 @@
+//Aplica cores na paleta;
+function toPaintbox() {
+  const box = document.querySelectorAll('.color')
+  box[0].style.backgroundColor = 'red';
+  box[0].style.backgroundColor = 'blue';
+  box[0].style.backgroundColor = 'yelow';
+  box[0].style.backgroundColor = 'pink';
+}
+toPaintbox();
