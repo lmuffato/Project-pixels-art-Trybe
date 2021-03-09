a277b92af86f2b2cf2d4b1c01c7dd643230c74f7
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-03-09 17:49:51 -0300

    exercio 6 de novo

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ window.onload = function () {
  function selecionaCor (){
     let paleta = document.querySelectorAll('.color');
 
-
     for (let index = 0; index < paleta.length, index += 1){
         paleta[index].addEventListener('click', function() {
             let cor = document.querySelector('.cor1');       
-          })
+        })
     }
+    return cor;
  } 
