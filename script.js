6b46d974e6f368e740247c9c398722ccc02eba63
Robertson <robertson.nunes@gmail.com>
2021-03-09 19:38:28 -0300

    requisito 6

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// 4 - Adicione à página um quadro de pixels, com 25 pixels.
 const colorBlocks = document.querySelector('#pixel-board');
 const  n = 5
 for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
@@ -9,3 +10,6 @@ for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
         Vblocks.classList = 'pixel';
     }
 }
+
+// 6 - Defina a cor preta como cor inicial. Ao carregar a página a cor preta já deve estar selecionada para pintar os pixels
+document.querySelectorAll('.color')[0].classList.add('selected')
