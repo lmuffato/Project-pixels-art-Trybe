7436e97fe2624da4a23289e508595f1a96222996
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-03-09 20:27:36 -0300

    Fourth requirement done

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
-window.onload = () => {
-  function colorPalette() {
-    const colors = ['black', 'red', 'green', 'blue'];
-    const colorsFather = document.querySelector('#color-palette');
+function colorPalette() {
+  const colors = ['black', 'red', 'green', 'blue'];
+  const colorsFather = document.querySelector('#color-palette');
 
-    for (let index = 0; index < colors.length; index += 1) {
-      const elementColor = document.createElement('li');
+  for (let index = 0; index < colors.length; index += 1) {
+    const elementColor = document.createElement('li');
 
-      elementColor.classList.add('color');
-      elementColor.style.backgroundColor = `${colors[index]}`;
-      colorsFather.appendChild(elementColor);
-    }
+    elementColor.classList.add('color');
+    elementColor.style.backgroundColor = `${colors[index]}`;
+    colorsFather.appendChild(elementColor);
   }
-  colorPalette();
-};
+}
+colorPalette();
+
+function pixelBoard() {
+  const pixelsFather = document.querySelector('#pixel-board');
+
+  for (let index = 0; index < 5; index += 1) {
+    const pixelRow = document.createElement('tr');
+
+    for (let column = 0; column < 5; column += 1) {
+      const pixelColumn = document.createElement('td');
+
+      pixelColumn.classList.add('pixel');
+      pixelRow.appendChild(pixelColumn);
+    }
+
+    pixelsFather.appendChild(pixelRow);
+  }
+}
+pixelBoard();
