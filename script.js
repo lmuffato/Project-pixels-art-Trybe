c744fb9d78668a003f4bd15b7ab9001c743f9981
Edmilson Bernardo <ebedmilson48@gmail.com>
2021-03-09 17:21:14 -0300

    corrige semântica do html e corrige erros do arquivo .js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //Aplica cores na paleta;
 function toPaintbox() {
   const box = document.querySelectorAll('.color')
-  box[0].style.backgroundColor = 'red';
-  box[0].style.backgroundColor = 'blue';
-  box[0].style.backgroundColor = 'yelow';
-  box[0].style.backgroundColor = 'pink';
+  box[0].style.backgroundColor = 'black';
+  box[1].style.backgroundColor = 'blue';
+  box[2].style.backgroundColor = 'red';
+  box[3].style.backgroundColor = 'yellow';
 }
 toPaintbox();
