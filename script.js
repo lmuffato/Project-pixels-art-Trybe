5251775fb0a52348330c1876736f1f6c4cb289bb
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-03-09 21:04:29 -0300

    dando um clean no lint

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
-
 // gerando o quadrado 5x5
-function lineSquareCreator (lineLength) {
+function lineSquareCreator(lineLength) {
+  const board = document.getElementById('pixel-board');
   for (let i = 0; i < lineLength; i += 1) {
-    let lineSquare = document.createElement('div');
-    document.getElementById('pixel-board').appendChild(lineSquare);
+    const lineSquare = document.createElement('div');
+    board.appendChild(lineSquare);
     lineSquare.className = 'pixel-line';
   }
-  return document.getElementById('pixel-board');
+  return board;
 }
-function fillLines (lineLength) {
+function fillLines(lineLength) {
   for (let i = 0; i < lineLength; i += 1) {
+    const line = document.querySelectorAll('.pixel-line');
     for (let k = 0; k < lineLength; k += 1) {
-      let square = document.createElement('div');
+      const square = document.createElement('div');
       square.className = 'pixel';
-      let line = document.querySelectorAll('.pixel-line')[i].appendChild(square);
+      line[i].appendChild(square);
     }
   }
   return document.querySelectorAll('.pixel-line');
 }
-function boardGenerator (lineLength) {
-  let square = document.getElementById('pixel-board');
-  lineSquareCreator (lineLength);
-  fillLines (lineLength);
-  return square
+function boardGenerator(lineLength) {
+  const square = document.getElementById('pixel-board');
+  lineSquareCreator(lineLength);
+  fillLines(lineLength);
+  return square;
 }
-let board = boardGenerator (5)
-console.log(board) 
+const board = boardGenerator(5);
+console.log(board);
 // eu acho que deu certo
-
-
-
