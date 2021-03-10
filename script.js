bc76513c94e8291a950f5edd5b915343d033f9e4
Luiz Felipe Araujo <thelastnailmusic@gmail.com>
2021-03-10 09:20:41 -0300

    Colorindo errado

--- script.js
+++ script.js
@@ -1,12 +1,52 @@
 function criaPaleta(numberOfFrames) {
   for (let index = 0; index < numberOfFrames; index += 1) {
-    const pegaPaleta = document.getElementById('color-palette');
-    const criaPaleta = document.createElement('div');
-    criaPaleta.className = 'color';
-    pegaPaleta.appendChild(criaPaleta);
+    const recuperaPaleta = document.getElementById('color-palette');
+    const criaDivPaleta = document.createElement('div');
+    criaDivPaleta.className = 'color';
+    recuperaPaleta.appendChild(criaDivPaleta);
   }
 }
 criaPaleta(4);
+
+// Setando a Classe 'selected' nas cores da paleta de cima
+
+let qualCor = 'black';
+
+function selectBlack() {
+  document.getElementsByClassName('color')[0].classList.add('selected');
+  document.getElementsByClassName('color')[1].className = 'color';
+  document.getElementsByClassName('color')[2].className = 'color';
+  document.getElementsByClassName('color')[3].className = 'color';
+  qualCor = 'black';
+}
+function selectOrange() {
+  document.getElementsByClassName('color')[1].classList.add('selected');
+  document.getElementsByClassName('color')[0].className = 'color';
+  document.getElementsByClassName('color')[2].className = 'color';
+  document.getElementsByClassName('color')[3].className = 'color';
+  qualCor = 'orange';
+}
+function selectOlive() {
+  document.getElementsByClassName('color')[2].classList.add('selected');
+  document.getElementsByClassName('color')[1].className = 'color';
+  document.getElementsByClassName('color')[0].className = 'color';
+  document.getElementsByClassName('color')[3].className = 'color';
+  qualCor = 'olive';
+}
+function selectBlue() {
+  document.getElementsByClassName('color')[3].classList.add('selected');
+  document.getElementsByClassName('color')[1].className = 'color';
+  document.getElementsByClassName('color')[2].className = 'color';
+  document.getElementsByClassName('color')[0].className = 'color';
+  qualCor = 'blue';
+}
+
+document.getElementsByClassName('color')[0].addEventListener('click', selectBlack);
+document.getElementsByClassName('color')[1].addEventListener('click', selectOrange);
+document.getElementsByClassName('color')[2].addEventListener('click', selectOlive);
+document.getElementsByClassName('color')[3].addEventListener('click', selectBlue);
+
+// Setando a cor da paleta superior
 
 function colorePaletaSuperior() {
   document.getElementsByClassName('color')[0].style.backgroundColor = 'black';
@@ -16,24 +56,35 @@ function colorePaletaSuperior() {
 }
 colorePaletaSuperior();
 
+function colore() {
+  const doc = document.getElementsByClassName('pixel');
+  for (let index = 0; index < doc.length; index += 1) {
+    doc[index].style.backgroundColor = qualCor;
+  }
+}
+
 function criaQuadro(numberOfFrames) {
   for (let index = 0; index < numberOfFrames * numberOfFrames; index += 1) {
     const criaDiv = document.createElement('div');
     criaDiv.className = 'pixel';
     const pegaDiv = document.getElementById('pixel-board');
     pegaDiv.append(criaDiv);
+    criaDiv.addEventListener('click', colore);
   }
 }
 criaQuadro(5);
 
-// Funções para Criar os Eventos de Mouse
+// Funções para Botao Limpar
 
 function limpaTudo() {
-  document.getElementsByClassName('pixel').style.backgroundColor = 'white';
+  let doc = document.getElementsByClassName('pixel');
+  for (let index = 0; index < doc; index += 1) {
+    doc[index].style.backgroundColor = 'white';
+  }
 }
 
 function botaoLimpar() {
-  let bt = document.createElement("button");
+  const bt = document.createElement('button');
   bt.id = 'clear-board';
   bt.innerHTML = 'Limpar';
   document.getElementById('espacoDoBotao').appendChild(bt);
