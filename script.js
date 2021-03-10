c1051947a55f225ecc36293b75b187bf7f478ae6
maricegalla <maricegalla@gmail.com>
2021-03-09 23:33:49 -0300

    Requisito 12

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
+function rgbColor() {
+  const colorR = Math.round(Math.random() * 256);
+  const colorG = Math.round(Math.random() * 256);
+  const colorB = Math.round(Math.random() * 256);
+  return 'rgb(' + colorR + ',' + colorG + ',' + colorB + ')';
+}
+
 function colorsPalette() {
   const squareBlack = document.getElementById('black');
   squareBlack.style.backgroundColor = 'black';
   squareBlack.classList.add = 'selected';
 
   const squareBlue = document.getElementById('blue');
-  squareBlue.style.backgroundColor = 'blue';
+  squareBlue.style.backgroundColor = rgbColor();
 
   const squareRed = document.getElementById('red');
-  squareRed.style.backgroundColor = 'red';
+  squareRed.style.backgroundColor = rgbColor();
 
   const squareYellow = document.getElementById('yellow');
-  squareYellow.style.backgroundColor = 'yellow';
+  squareYellow.style.backgroundColor = rgbColor();
 }
 
 window.onload = function load() {
