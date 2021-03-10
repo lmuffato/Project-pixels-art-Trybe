4f1e7ec5812851320979534d51c2c6a2a50fe822
Vinicios Fraga <soicinivt@gmail.com>
2021-03-10 14:55:10 -0300

    Requisitos 3 e 4 e correçao no requisito 2.

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 function setbkColor() {
   const colorsBlock = document.querySelectorAll('.color');
   const bkColor = ['black', 'midnightblue', 'gold', 'darkred'];
-  for (const index in colorsBlock) {
-    colorsBlock[index].style.backgroundColor = bkColor[index];
+  for (let index = 0; index < colorsBlock.length; index += 1) {
+    const color = bkColor[index];
+    colorsBlock[index].style.backgroundColor = color;
+  }
+  return colorsBlock;
+}
+
+setbkColor();
+
+function board() {
+  const line = document.querySelectorAll('.line');
+  console.log(line);
+  for (let index = 0; index < line.length; index += 1) {
+    for (let indexToo = 0; indexToo < line.length; indexToo += 1) {
+      const pixels = document.createElement('div');
+      pixels.setAttribute('class', 'pixel');
+      line[index].appendChild(pixels);
+    }
   }
 }
-setbkColor();
+
+board();
