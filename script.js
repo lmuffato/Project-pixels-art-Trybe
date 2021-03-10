3456864dfc9ad8fcb1414127bb7a571ffd8632b1
Nathalia Zebral <natzebral@gmail.com>
2021-03-10 20:00:14 -0300

    alteracoes no index e tentativa javaScript

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function createTable  {
+const paletteColors = document.querySelectorAll('.color');
 
-}
+function colorsInpalette (){
+    paletteColors[0].style.backgroundcolor = 'rgb(0, 0, 0)';
+    paletteColors[1].style.backgroundcolor = 'rgb (140, 0, 252)';
+    paletteColors[2].style.backgroundcolor = 'rgb (1, 254, 1)';
+    paletteColors[3].style.backgroundcolor = 'rgb (255, 134, 0)';
+    }
+
+
