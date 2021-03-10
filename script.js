7617a13aa2e8d497def01e6dae621f9540ccdbf2
Eric <ericff07@gmail.com>
2021-03-10 16:00:42 -0300

    Tira o .style do js e usa o css para trocar as cores. Isso deve corrigir o problema do evaluator

--- script.js
+++ script.js
@@ -5,9 +5,3 @@ for (let line = 1; line <= 25; line += 1) {
     divPosition.className = 'pixel'
     cell.appendChild(divPosition);    
 }
-
-// Deixa todos os pixels brancos quando a página carrega
-let whitePixel = document.querySelectorAll('.pixel')
-for (let index = 0; index <= whitePixel.length; index += 1) {
-    whitePixel[index].style.backgroundColor = 'rgb(255, 255, 255)'
-}
