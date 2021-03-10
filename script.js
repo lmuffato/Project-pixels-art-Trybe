c76d9307b216c4b0984b185dcd4aec0e6114759b
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-03-10 09:24:20 -0300

    Some refactoring

--- script.js
+++ script.js
@@ -1,4 +1,47 @@
 const PIXEL_BOARD = 'pixel-board';
+const COLOR_PALETTE = 'color-palette';
+const COLOR_COUNT = 4;
+const CANVAS_SIZE = { x: 5, y: 5 };
+const CLEAR_BOARD = 'clear-board';
+
+function clearPixelBoard() {
+  const pixels = document.getElementById(PIXEL_BOARD).children;
+  for (let index = 0; index < pixels.length; index += 1) {
+    pixels[index].style.backgroundColor = 'white';
+  }
+}
+function createButtonListener() {
+  const btnClearPixelBoard = document.getElementById(CLEAR_BOARD);
+  btnClearPixelBoard.addEventListener('click', clearPixelBoard, false);
+}
+function setInitialColor() {
+  const firstColor = document.getElementsByClassName('color')[0];
+  firstColor.classList.toggle('selected');
+}
+function changePaletteColor(e) {
+  const lastSelected = document.querySelector(`#${COLOR_PALETTE} > .selected`);
+  lastSelected.classList.toggle('selected');
+  e.target.classList.toggle('selected');
+}
+function createColorPaletteListener() {
+  const colorPalette = document.getElementById(COLOR_PALETTE);
+  colorPalette.addEventListener('click', changePaletteColor, false);
+}
+function createColors(n) {
+  const colorPalette = document.getElementById(COLOR_PALETTE);
+  for (let index = 0; index < n; index += 1) {
+    const color = document.createElement('div');
+    // change from span to div you need a display flex container in css ofc
+    color.className = 'color';
+    color.innerText = getComputedStyle(color).backgroundColor;
+    colorPalette.appendChild(color);
+  }
+}
+function initializeColorPalette() {
+  createColors(COLOR_COUNT);
+  createColorPaletteListener();
+  setInitialColor();
+}
 function createPixels(n) {
   const pixelBoard = document.getElementById(PIXEL_BOARD);
   for (let index = 0; index < n; index += 1) {
@@ -7,26 +50,9 @@ function createPixels(n) {
     pixelBoard.appendChild(row);
   }
 }
-function createCanvas(x, y) {
-  createPixels(x * y);
-  // Everything else is done 'automagically' by display flex config
-}
-function setInitialColor() {
-  const firstColor = document.getElementsByClassName('color')[0];
-  firstColor.classList.toggle('selected');
-}
-function changePaletteColor(e) {
-  const lastSelected = document.querySelector('#color-palette > .selected');
-  lastSelected.classList.toggle('selected');
-  e.target.classList.toggle('selected');
-}
-function createColorPaletteListener() {
-  const colorPalette = document.getElementById('color-palette');
-  colorPalette.addEventListener('click', changePaletteColor, false);
-}
 function changePixelColor(e) {
   const selectedPaletteColor = document.querySelector(
-    '#color-palette > .selected',
+    `#${COLOR_PALETTE} > .selected`,
   );
   const newColor = getComputedStyle(selectedPaletteColor).backgroundColor;
   e.target.style.backgroundColor = newColor;
@@ -35,20 +61,13 @@ function createPixelsListener() {
   const pixelBoard = document.getElementById(PIXEL_BOARD);
   pixelBoard.addEventListener('click', changePixelColor, false);
 }
-function clearPixelBoard() {
-  const pixels = document.getElementById(PIXEL_BOARD).children;
-  for (let index = 0; index < pixels.length; index += 1) {
-    pixels[index].style.backgroundColor = 'white';
-  }
-}
-function createButtonListener() {
-  const btnClearPixelBoard = document.getElementById('clear-board');
-  btnClearPixelBoard.addEventListener('click', clearPixelBoard, false);
+function initializePixelBoard(x, y) {
+  createPixels(x * y);
+  createPixelsListener();
+  // Everything else is done 'automagically' by display flex config
 }
 window.onload = function init() {
-  createCanvas(5, 5); // hardcoded canvas size
-  setInitialColor();
-  createColorPaletteListener();
-  createPixelsListener();
+  initializePixelBoard(CANVAS_SIZE.x, CANVAS_SIZE.y);
+  initializeColorPalette();
   createButtonListener();
 };
