b181898be6a2f06e63236f82f9df308f80a5ef73
Villefor <franklincrius@gmail.com>
2021-03-10 04:07:12 -0300

    8 requisito

--- script.js
+++ script.js
@@ -39,7 +39,8 @@ function squareClean (){
 }
 squareClean ();
 
-function changeColors (){
+// assistência de Bruno Bastos, referência: https://forum.freecodecamp.org/t/addeventlistener-and-changing-background-color/193023
+function changeSelectColors (){
     let palletset = document.querySelector('#color-palette')
     palletset.addEventListener('click', function(change){
         if (change.target.className == 'color'){
@@ -48,5 +49,20 @@ function changeColors (){
     }
   })
 }
-changeColors ();
+changeSelectColors ();
 
+//referência https://pt.stackoverflow.com/questions/426718/qual-a-diferen%C3%A7a-entre-queryselector-e-o-queryselectorall#:~:text=Primeiramente%20querySelector%20e%20querySelectorAll%20s%C3%A3o,de%20elementos%20presentes%20no%20documento.
+// Também vi algumas resoluções dos colegas, em especial a do 311stan, link https://github.com/tryber/sd-010-a-project-pixels-art/pull/106
+function ColorForaChange(){
+    let setpallet = document.querySelector('#pixel-board')
+    setpallet.addEventListener('click', function(colorChanges){
+        let letsChange = document.querySelector('.selected').style.backgroundColor;
+        let pixelChange = document.querySelectorAll('.pixel');
+        for (ind in pixelChange){
+            if (pixelChange[ind] == colorChanges.target){
+                pixelChange[ind].style.backgroundColor = letsChange
+            }
+        }
+    })
+}
+ColorForaChange();
