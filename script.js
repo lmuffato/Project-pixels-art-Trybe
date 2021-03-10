1ca8c836fe42c659ce641d9b175bab020aca7baa
Rafael Ribeiro <rafittu@outlook.com>
2021-03-10 20:52:04 -0300

    feito correcao no css

--- script.js
+++ script.js
@@ -23,34 +23,11 @@ setColor();
 
 function paintBoard() {
   const pixelsList = document.querySelector('#pixel-board');
-
-  pixelsList.addEventListener('click', function(event) {
+  pixelsList.addEventListener('click', function (event) {
     event.target.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
-  })
+  });
 }
 paintBoard();
-
-// function paintBoard() {
-//   const pixelsList = document.querySelectorAll('.pixel');
-//   const selectedPixel = document.querySelectorAll('selected');
-
-//   for (let index = 0; index < pixelsList.length; index += 1) {
-//     pixelsList[index].addEventListener('click', () => {
-//       pixelsList[index].style.backgroundColor = selectedPixel.style.backgroundColor;
-      
-//       if (selectedPixel.classList.contains('box1')) {
-//         pixelsList[index].style.backgroundColor = 'black';
-//       } else if (selectedPixel.classList.contains('box2')) {
-//         pixelsList[index].style.backgroundColor = 'blue';
-//       } else if (selectedPixel.classList.contains('box3')) {
-//         pixelsList[index].style.backgroundColor = 'red';
-//       } else if (selectedPixel.classList.contains('box4')) {
-//         pixelsList[index].style.backgroundColor = 'yellow';
-//       }
-//     })
-//   }
-// }
-// paintBoard();
 
 // Requisito 9
 const button = document.getElementById('clear-board');
