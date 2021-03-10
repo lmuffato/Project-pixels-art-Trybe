64cfb7ce0c06edd4c0fd77059442121a6bccfe69
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-03-10 16:42:41 -0300

    Requisito 7:Clicar nas cores da paleta

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 const pegaIdPrimeiraSection = document.querySelector('#color-palette');
 pegaIdPrimeiraSection.firstElementChild.classList.add('selected');
+
+function selecionaCorPixel() {
+  pegaIdPrimeiraSection.addEventListener('click', function eventoDeClick(event) {
+    if (event.target.className === 'color') {
+      document.querySelector('.selected').classList.remove('selected');
+      event.target.classList.add('selected');
+    }
+  });
+}
+selecionaCorPixel();
