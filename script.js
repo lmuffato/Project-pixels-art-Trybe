47c00ef1572ef065aa757ba822bb0a4637d36d66
Mateus Martins - Turma 10 - Tribo A <mateusmartinsmck@gmail.com>
2021-03-10 11:43:44 -0300

    window onload corrigido

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-window.onload(loadPage());
-
-function createSquares () {
-  for (let i = 0; i < 4; i++) {
-    let colorPalette = document.getElementById('color-palette');
-    let makeSquare = document.createElement('td');
+function createSquares() {
+  for (let index = 0; index < 4; index += 1) {
+    const colorPalette = document.getElementById('color-palette');
+    const makeSquare = document.createElement('td');
     makeSquare.className = 'color';
     colorPalette.appendChild(makeSquare);
   }
 }
 
+function colorSquares() {
+  let colors = ['black', 'darkred', 'darkgreen', 'darkblue'];
+  let squares = document.getElementsByClassName('color');
+  for (let index = 0; index < squares.length; index += 1) {
+    squares[index].style.backgroundColor = colors[index];
+  }
+}
+
 function loadPage() {
   createSquares();
+  colorSquares();
 }
+
+window.onload = loadPage();
