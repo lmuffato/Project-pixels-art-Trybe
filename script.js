f9539a178a7c2d44c3640b93953d9a18bb2ca5bd
camiloLelis <lafirmafuerte@gmail.com>
2021-03-10 18:55:13 -0300

    projeto-project-pixels-ar- requisito 6

---
+++ script.js
@@ -0,0 +1,15 @@
+
+  /*   const alvo = document.getElementById('color1');
+    
+    function capturar(){
+        alvo.style.backgroundColor= 'red';
+        
+        console.log('testesssssss');
+    }
+    
+   alvo.addEventListener('click', capturar);
+         */
+    
+
+
+
