039df529e45665b923dc24fda6933fd059ea8a00
lucas-chave <lcchavesr150@gmail.com>
2021-03-10 19:05:36 -0300

    adicionando requisito sete

--- script.js
+++ script.js
@@ -1,27 +1,15 @@
 const paletteColor = document.querySelector('#color-palette');
 const elementTable = document.querySelector('#pixel-board');
+const colorBlack = paletteColor.children[0];
+const colorBlue = paletteColor.children[1];
+const colorBrown = paletteColor.children[2];
+const colorAqua = paletteColor.children[3];
 
 // adicionando cores a paleta de cores
-function addColorQuadOne() {
-  paletteColor.children[0].style.backgroundColor = 'black';
-}
-
-function addColorQuadTwo() {
-  paletteColor.children[1].style.backgroundColor = 'blue';
-}
-
-function addColorQuadThree() {
-  paletteColor.children[2].style.backgroundColor = 'brown';
-}
-
-function addColorQuadFour() {
-  paletteColor.children[3].style.backgroundColor = 'aqua';
-}
-
-addColorQuadOne();
-addColorQuadTwo();
-addColorQuadThree();
-addColorQuadFour();
+colorBlack.style.backgroundColor = 'black';
+colorBlue.style.backgroundColor = 'blue';
+colorBrown.style.backgroundColor = 'brown';
+colorAqua.style.backgroundColor = 'aqua';
 
 // criando tabela
 for (let index = 0; index < 25; index += 1) {
@@ -29,3 +17,17 @@ for (let index = 0; index < 25; index += 1) {
   elementDiv.className = 'pixel';
   elementTable.appendChild(elementDiv);
 }
+
+function addEvent(event) {
+  const classSelected = document.querySelector('.selected');
+  classSelected.classList.remove('selected');
+  event.target.classList.add('selected');
+}
+
+// moving class
+const element = document.querySelectorAll('.color');
+for (let index = 0; index < element.length; index += 1) {
+  element[index].addEventListener('click', addEvent);
+}
+
+// referencias: https://backefront.com.br/adicionar-classe-js-puro/, https://developer.mozilla.org/pt-BR/docs/Web/API/EventTarget/addEventListener
