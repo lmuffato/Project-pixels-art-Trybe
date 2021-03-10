c8502885771f31176e2a4d4419f55ea00a60e36b
Rodrigo Queiroz <rodrigoandradequeiroz@gmail.com>
2021-03-09 21:18:42 -0300

    requisito sete pt1

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-const getSquare = document.getElementById('pixel-board');
-let makeRow = document.createElement('tr');
-let makeSquare = document.createElement('td');
-const tdClass = 'pixel';
-
-function makeTable() {
-  for (let index = 0; index < 5; index += 1) {
+function makeTable(numeroMatriz) {
+  const getSquare = document.getElementById('pixel-board');
+  let makeRow = document.createElement('tr');
+  let makeSquare = document.createElement('td');
+  const tdClass = 'pixel';
+  for (let index = 0; index < numeroMatriz; index += 1) {
     makeRow = document.createElement('tr');
     getSquare.appendChild(makeRow);
-    for (let indexSquare = 0; indexSquare < 5; indexSquare += 1) {
+    for (let indexSquare = 0; indexSquare < numeroMatriz; indexSquare += 1) {
       makeSquare = document.createElement('td');
       getSquare.appendChild(makeSquare);
       makeSquare.className = tdClass;
@@ -15,4 +14,39 @@ function makeTable() {
   }
 }
 
-makeTable();
+function changeSelectedClass(){
+  let getColor = document.getElementsByClassName('selected')[0]
+  let blackClick = document.getElementsByClassName('color')[0]
+  let purpleClick = document.getElementsByClassName('color')[1]
+  let greyClick = document.getElementsByClassName('color')[2]
+  let silverClick = document.getElementsByClassName('color')[3]
+
+  blackClick.addEventListener('click', function () {
+    getColor = document.getElementsByClassName('selected')[0]
+    getColor.classList.remove('selected')
+    blackClick.classList.add('selected')
+  })
+
+  purpleClick.addEventListener('click', function () {
+    getColor = document.getElementsByClassName('selected')[0]
+    getColor.classList.remove('selected')
+    purpleClick.classList.add('selected')
+  })
+
+  greyClick.addEventListener('click', function () {
+    getColor = document.getElementsByClassName('selected')[0]
+    getColor.classList.remove('selected')
+    greyClick.classList.add('selected')
+  })
+
+  silverClick.addEventListener('click', function () {
+    getColor = document.getElementsByClassName('selected')[0]
+    getColor.classList.remove('selected')
+    silverClick.classList.add('selected')
+  })
+}
+
+
+
+makeTable(5);
+changeSelectedClass()
