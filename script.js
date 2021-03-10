f19b2b9a842095fbd4d45eaa146c2ef404e6608a
Beatriz Estebanez <bia.estebanez@gmail.com>
2021-03-10 08:34:13 -0300

    requisito 7 - classe selected para selecionar cor da paleta

--- script.js
+++ script.js
@@ -6,3 +6,48 @@ for (let index = 1; index <= 25; index += 1) {
 }
 
 document.getElementById('color-palette').firstElementChild.classList.add('selected');
+
+let selectedPixel = document.querySelectorAll('.pixel');
+
+/*let elemento = document.querySelectorAll('.color');
+if (elemento[2].className[1] === 'color') {
+  selectedPixel[2].style.backgroundColor = 'green';
+}*/
+// requisito 7
+
+  let element = document.querySelectorAll('.color');
+
+for (let index = 0; index < 4; index += 1) {
+  element[index].addEventListener('click', function () {
+    document.querySelector('.selected').classList.remove('selected');
+    element[index].classList.add('selected');
+  })
+}
+  /*for (let index = 0; index < 4; index += 1) {
+    element[index].addEventListener('click',function () {
+      document.querySelector('.selected').classList.remove('selected');
+      element[index].classList.add('selected');
+    });
+  /*
+    if (element[index].className === 'selected') {
+      pixel[2].style.backgroundColor = element[index].style.backgroundColor;
+    }
+ // }
+
+
+pixel[x].addEventListener('click', function () {
+
+}
+console.log(element);
+element[2].classList.add('selected');
+element[2].addEventListener('click', selectColor);
+
+
+  }
+
+let corTeste = 'black';
+if (corTeste === element[0].style.backgroundColor) {
+  document.querySelectorAll('.pixel')[1].style.backgroundColor = 'black';
+} else {
+  console.log(false);
+}*/
