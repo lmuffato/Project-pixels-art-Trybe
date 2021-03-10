b7a4cd3a0c62360f0004f8bbf82c365d8180511a
Willian Mendes <willianmendesbr@gmail.com>
2021-03-09 22:21:57 -0300

    Requisito 6, 7 e 8

---
+++ script.js
@@ -0,0 +1,38 @@
+const colors = document.getElementsByClassName('color');
+const pixels = document.getElementsByClassName('pixel');
+let color = 'black';
+
+function removeSelected() {
+  for (let i = 0; i < colors.length; i += 1) {
+    colors[i].classList.remove('selected');
+  }
+}
+
+function selectedColor(event) {
+  const paletteElement = event.currentTarget;
+  color = window.getComputedStyle(paletteElement).getPropertyValue('background-color');
+  removeSelected();
+  paletteElement.className += ' selected';
+}
+
+function addListenerPaletteColors() {
+  for (let i = 0; i < colors.length; i += 1) {
+    colors[i].addEventListener('click', selectedColor);
+  }
+}
+
+function fillPixel(event) {
+  const pixelElement = event.currentTarget;
+  pixelElement.style.backgroundColor = color;
+}
+
+function addListenerPixel() {
+  for (let i = 0; i < pixels.length; i += 1) {
+    pixels[i].addEventListener('click', fillPixel);
+  }
+}
+
+window.onload = function init() {
+  addListenerPaletteColors();
+  addListenerPixel();
+};
