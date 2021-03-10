6a95c1bc26095a5474cbdacbd35480c9e0323fe8
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2021-03-09 23:01:50 -0300

    desafio 9 feito

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let palette = document.getElementsByClassName('color');
-
-let number = 25;
+const blackPalette = palette[0];
+let palette2 = palette[1];
+let palette3 = palette[2];
+let palette4 = palette[3];
 
 function GenerateColor() {
   let red = parseInt(Math.random * 255);
@@ -19,4 +21,14 @@ function GenerateBoard(number) {
   }
 }
 
-GenerateBoard(number);
+const reset = document.getElementById('clear-board');
+
+function ResetColors(){
+  let resetButton = document.querySelectorAll('.pixel');
+  for (let index = 0; index < resetButton.length; index+= 1){
+  resetButton[index].style.backgroundColor = 'white';
+  }
+}
+reset.addEventListener('click', ResetColors);
+
+GenerateBoard(25);
