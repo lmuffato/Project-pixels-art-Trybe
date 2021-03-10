990823a2255f6c3ed5e60a3ee8852ed37ef80e2a
maricegalla <maricegalla@gmail.com>
2021-03-09 21:23:45 -0300

    Correções lint

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-function colorsPalette () {
-    let squareBlack = document.getElementById('black');
-    squareBlack.style.backgroundColor = 'black';
+function colorsPalette() {
+  const squareBlack = document.getElementById('black');
+  squareBlack.style.backgroundColor = 'black';
 
-    let squareBlue = document.getElementById('blue');
-    squareBlue.style.backgroundColor = 'blue';
+  const squareBlue = document.getElementById('blue');
+  squareBlue.style.backgroundColor = 'blue';
 
-    let squareRed = document.getElementById('red');
-    squareRed.style.backgroundColor = 'red';
+  const squareRed = document.getElementById('red');
+  squareRed.style.backgroundColor = 'red';
 
-    let squareYellow = document.getElementById('yellow');
-    squareYellow.style.backgroundColor = 'yellow';
+  const squareYellow = document.getElementById('yellow');
+  squareYellow.style.backgroundColor = 'yellow';
 }
 
-window.onload = function () {
-    colorsPalette ();
+window.onload = function load() {
+  colorsPalette();
 };
