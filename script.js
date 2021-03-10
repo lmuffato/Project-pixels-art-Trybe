0db8050efc1710ab2507f5aa32feee00cb3f7110
Eduardo Seije <eduardoseije@gmail.com>
2021-03-10 18:27:09 -0300

    Resuisito 07 - refatoramento utilizzando o event.target e loop para percorrer as cores. Requisito 08 - tentativa com o event target

--- script.js
+++ script.js
@@ -1,71 +1,34 @@
-function changeColor1() {
-  const color1 = document.getElementById('color1');
-  color1.style.backgroundColor = 'black';  
-}
-changeColor1();
-
-const color1 = document.getElementById('color1');
-const color2 = document.getElementById('color2');
-const color3 = document.getElementById('color3');
-const color4 = document.getElementById('color4');
-
-function selectColor1 () {
-  color2.className = 'color';
-  color3.className = 'color';
-  color4.className = 'color'; 
-  color1.className = 'color selected'   
-}
-
-function selectColor2 () {
-  color1.className = 'color';
-  color3.className = 'color';
-  color4.className = 'color';
-  color2.className = 'color selected';    
-}
-
-function selectColor3 () {
-  color1.className = 'color';
-  color2.className = 'color';
-  color4.className = 'color';
-  color3.className = 'color selected';    
-}
-
-function selectColor4 () {
-  color1.className = 'color';
-  color2.className = 'color';
-  color3.className = 'color';
-  color4.className = 'color selected';    
-}
-
-color1.addEventListener('click', selectColor1);
-color2.addEventListener('click', selectColor2);
-color3.addEventListener('click', selectColor3);
-color4.addEventListener('click', selectColor4);
-
-
-function changePixelColor () {
-  let pixelsToChange = document.getElementsByClassName('pixel');
-  let selectedColor = document.querySelector('.selected');
-  for (let index = 0; index < pixelsToChange.length; index += 1) {
-  pixelsToChange[index].style.backgroundColor = selectedColor.backgroundColor;  
+function colorSelect() {
+  let defaultColors = document.querySelectorAll('.color');
+  for (let index = 0; index < defaultColors.length; index += 1){
+    defaultColors[index].addEventListener('click', onClick);
+  }
+  function onClick(event) {
+    let selectedColor = event.target;
+    let defaultColorBlack = document.querySelector('.selected');
+    defaultColorBlack.classList.remove('selected');
+    selectedColor.classList.add('selected');
   }
 }
-changePixelColor();
+colorSelect(); 
 
-function setPixelEvent () {
- let pixelsToChange = document.getElementsByClassName('pixel');
-  for (let index = 0; index < pixelsToChange.length; index += 1) {
-    pixelsToChange[index].addEventListener('click', changePixelColor);
+function changePixelsColor() {
+  let pixels = document.querySelectorAll('.pixel');
+  for (index = 0; index < pixels.length; index += 1) {
+  pixels[index].addEventListener('click', onClicktoChange);
   }
-  return pixelsToChange;
+  function onClicktoChange(event) {
+    let selectedPixel = event.target;
+    let selectedColor = document.querySelector('.selected').style.backgroundColor;
+    selectedPixel.style.backgroundColor = selectedColor;
+  }
 }
-setPixelEvent()
+changePixelsColor();
 
-function clearButton () {
-  let pixels = document.getElementsByClassName('.pixel');
+/*function clearButton() {
   for (let index = 0; index < pixels.length; index += 1) {
-    pixels.style.brackgroundColor = 'white';
+  pixels[index].style.backgroundColor = 'white';
   }
 }
-let clearBtn = document.getElementsByTagName('button')[0];
-clearBtn.addEventListener('click', clearButton)
+let clearBtn = document.getElementsByTagName('button');
+clearBtn.addEventListener('click', clearButton);*/ 
