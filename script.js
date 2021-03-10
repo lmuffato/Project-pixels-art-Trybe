0cc7e5636e850e82f06b8dcb5156c0c027c3242c
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-03-09 23:47:22 -0300

    Requisito 8 ok

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-window.onload = function() {
-  const coloPalete = document.getElementById('color-palette');
+  window.onload = function() {
   
   const caixaBlack = document.querySelector('.preto');
   const caixaRed = document.querySelector('.vermelho');
   const caixaBlue = document.querySelector('.azul');
   const caixaGreen = document.querySelector('.verde');
 
-  //let caixasTD = document.getElementsByClassName('pixel');
-  //console.log(coloPalete);
-  
+  caixaBlack.style.backgroundColor = 'black';
+  caixaRed.style.backgroundColor = 'red';
+  caixaBlue.style.backgroundColor = 'blue';
+  caixaGreen.style.backgroundColor = 'green';
+
   caixaBlack.addEventListener('click', caixaPreta);
 
   function caixaPreta() {
@@ -17,18 +18,18 @@ window.onload = function() {
     caixaBlue.classList.remove('selected');
     caixaGreen.classList.remove('selected');
   }
-  
+
   caixaRed.addEventListener('click', caixaVermelha);
-  
+
   function caixaVermelha() {
     caixaRed.classList.add('selected');
     caixaBlack.classList.remove('selected');
     caixaBlue.classList.remove('selected');
     caixaGreen.classList.remove('selected');
-  }
+}
 
   caixaBlue.addEventListener('click', caixaAzul);
-  
+
   function caixaAzul() {
     caixaBlue.classList.add('selected');
     caixaBlack.classList.remove('selected');
@@ -37,7 +38,7 @@ window.onload = function() {
   }
 
   caixaGreen.addEventListener('click', caixaVerde);
-  
+
   function caixaVerde() {
     caixaGreen.classList.add('selected');
     caixaBlack.classList.remove('selected');
@@ -45,13 +46,11 @@ window.onload = function() {
     caixaRed.classList.remove('selected');
   }
 
+  const preencheCor = document.getElementById('pixel-board');
+  preencheCor.addEventListener('click', corPixel);
 
-
-
-
-
-
-
-
-
+  function corPixel(event) {
+    let corSelected = document.querySelector('.selected').style.backgroundColor;
+    event.target.style.backgroundColor = corSelected;
+  }
 }
