fc0f06dd0d792c002c10410a6fc3d4d9ee9b72d5
Lucas Henrique Brito <lucas.silva.brito@hotmail.com.br>
2021-03-10 02:26:49 -0300

    Requisito 7-9

--- script.js
+++ script.js
@@ -5,14 +5,33 @@ colors[1].style.backgroundColor = 'red';
 colors[2].style.backgroundColor = 'green';
 colors[3].style.backgroundColor = 'blue';
 
-function pixelBoard (number) {
+function pixelBoard(number) {
   for (let i = 0; i < number; i += 1) {
     for (let j = 0; j < number; j += 1) {
       const pixel = document.createElement('div');
-      pixel.className = 'pixel'
+      pixel.className = 'pixel';
       document.querySelector('#pixel-board').appendChild(pixel);
     }
   }
 }
+
+document.getElementById('color-palette').addEventListener('click', function(selected) {
+  const selectedColor = document.querySelector('.selected');
+  const newSelected = selected.target;
+  selectedColor.classList.remove('selected');
+  newSelected.classList.add('selected');
+});
+
+document.getElementById('pixel-board').addEventListener('click', function(selectedPixel) {
+  const color = document.querySelector('.selected').style.backgroundColor;
+  selectedPixel.target.style.backgroundColor = color; 
+});
+
+document.getElementById('clear-board').addEventListener('click', function() {
+  const pixel = document.querySelectorAll('.pixel');
+  for (let i = 0; i < pixel.length; i += 1) {
+    pixel[i].style.backgroundColor = 'white';
+  }
+});
 
 pixelBoard(5);
