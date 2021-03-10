4519155945b4d73d019474b51f57d9839d47aa47
Mateus Martins - Turma 10 - Tribo A <mateusmartinsmck@gmail.com>
2021-03-10 12:33:20 -0300

    Requisitos 3 e 4

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function createSquares() {
+  const colorPalette = document.getElementById('color-palette');
   for (let index = 0; index < 4; index += 1) {
-    const colorPalette = document.getElementById('color-palette');
     const makeSquare = document.createElement('td');
     makeSquare.className = 'color';
     colorPalette.appendChild(makeSquare);
@@ -8,16 +8,31 @@ function createSquares() {
 }
 
 function colorSquares() {
-  let colors = ['black', 'darkred', 'darkgreen', 'darkblue'];
-  let squares = document.getElementsByClassName('color');
+  const colors = ['black', 'darkred', 'darkgreen', 'darkblue'];
+  const squares = document.getElementsByClassName('color');
   for (let index = 0; index < squares.length; index += 1) {
     squares[index].style.backgroundColor = colors[index];
+  }
+}
+
+function createBoard() {
+  const pixelBoard = document.getElementById('pixel-board');
+  for (let index = 0; index < 5; index += 1){
+    const row = document.createElement('tr')
+    pixelBoard.appendChild(row);
+    for (let index = 0; index < 5; index += 1){
+      const collumn = document.createElement('td');
+      collumn.className = 'pixel';
+      const lastRow = pixelBoard.lastChild;
+      lastRow.appendChild(collumn);
+    }
   }
 }
 
 function loadPage() {
   createSquares();
   colorSquares();
+  createBoard();
 }
 
-window.onload = loadPage();
+window.onload = loadPage;
