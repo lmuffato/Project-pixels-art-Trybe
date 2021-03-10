f5d2fb5c11e9eaf98a50222a894ba2e5c489785a
Priscila <prihscapin@gmail.com>
2021-03-10 16:13:03 -0300

    Correção exercícios 1 a 5

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 // reqisito 9
-let takeColor = document.getElementByClassName('color');
-click.addEventListener('click', takeColor)
-
-let color = document.getElementsByClassName('pixel');
-click.addEventListener('click', color)
-
 
 
 
