4054d98c7fe896351cd8726ffbda7799fce9329d
renancosta21 <renancosta21@gmail.com>
2021-03-09 21:20:32 -0300

    requisitos 1 a 5

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const colorsList = ['', '', '', ''];
+const colorsList = [];
 
-for (let index = 0; index < colorsList.length; index += 1) {
+for (let index = 0; index < 4; index += 1) {
+  colorsList.push('');
   colorsList[index] = document.createElement('div');
   colorsList[index].classList.add('color');
   document.querySelector('#color-palette').appendChild(colorsList[index]);
@@ -9,3 +10,11 @@ document.getElementsByClassName('color')[0].style.backgroundColor = 'black';
 document.getElementsByClassName('color')[1].style.backgroundColor = 'green';
 document.getElementsByClassName('color')[2].style.backgroundColor = 'red';
 document.getElementsByClassName('color')[3].style.backgroundColor = 'blue';
+
+const pixelBoard = [];
+for (let index = 0; index < 25; index += 1) {
+  pixelBoard.push('');
+  pixelBoard[index] = document.createElement('div');
+  pixelBoard[index].classList.add('pixel');
+  document.querySelector('#pixel-board').appendChild(pixelBoard[index]);
+}
