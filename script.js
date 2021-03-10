9338e3c961356d4de82e341432a61beda1b357e3
Priscila <prihscapin@gmail.com>
2021-03-10 17:16:12 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // reqisito 9
-
-
-
-
-
-
-
+function limpaTela() {
+let clean = document.getElementById('clear-board');
+let pixels = document.getElementsByClassName('pixel');
+clean.addEventListener('click', () => {
+    pixels.style.backgroundColor = "white";
+  } 
+})
