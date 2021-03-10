75028770938d3ee0dc3d733d3b691e883c589cc2
guilhermemd <guilherme.md@gmail.com>
2021-03-10 20:39:48 -0300

    requisito 02

---
+++ script.js
@@ -0,0 +1,8 @@
+let blackBox = document.getElementsByClassName('color')[0];
+blackBox.style.backgroundColor = 'black';
+let greenBox = document.getElementsByClassName('color')[1];
+greenBox.style.backgroundColor = 'green';
+let yellowBox = document.getElementsByClassName('color')[2];
+yellowBox.style.backgroundColor = 'yellow';
+let purpleBox = document.getElementsByClassName('color')[3];
+purpleBox.style.backgroundColor = 'purple';
