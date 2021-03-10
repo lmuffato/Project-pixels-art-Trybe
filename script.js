3c474bd61cda911b5313ade9d7e545a65d24acc9
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-03-10 09:45:30 -0300

    Requisito 8 concluído: paleta de cor faz com que o pixel seja colorido

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-window.onload = () => {
-  const colorPalette = document.getElementById('color-palette');
-  const colorElements = document.querySelectorAll('.color');
+// window.onload = () => {
+// };
+const colorPalette = document.getElementById('color-palette');
+const colorElements = document.querySelectorAll('.color');
+const pixelBoard = document.getElementById('pixel-board');
 
-  colorPalette.addEventListener('click', (event) => {
-    if (event.target.id !== 'color-palette') {
-      colorElements.forEach((element) => {
-        element.classList.remove('selected');
-        event.target.classList.add('selected');
-      });
-    }
-  });
-};
+function selecElementPallete(event) {
+  const elementTarget = event.target;
+  if (elementTarget.id !== 'color-palette') {
+    colorElements.forEach((element) => {
+      element.classList.remove('selected');
+      elementTarget.classList.add('selected');
+    });
+  }
+}
+
+function changeColor(event) {
+  const elementTarget = event.target;
+  if (elementTarget.id !== 'pixel-board') {
+    const color = document.querySelector('.selected').dataset.color;
+    elementTarget.style.backgroundColor = color;
+    console.log(elementTarget);
+  }
+}
+
+colorPalette.addEventListener('click', selecElementPallete);
+pixelBoard.addEventListener('click', changeColor);
+
+
