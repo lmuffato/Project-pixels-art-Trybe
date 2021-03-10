9250a3dd93e79afd4df05e8a95a58769379fed5c
AndreBarroso <andre_barroso17@hotmail.com>
2021-03-10 02:41:11 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -13,12 +13,40 @@ function adicionaPixel(number) {
   }
 }
 
-function selectColor(cor) {
+// Requisitos 6 e 7
+function modifyColorToColorSelected(colorSelected) {
   const color = document.querySelectorAll('.color');
-  if (cor === undefined) {
+  if (colorSelected === undefined) {
     color[0].className = `${color[0].className} selected`;
+  } else {
+    const selecionada = document.querySelector(`.${colorSelected}`);
+    selecionada.className = `${selecionada.className} selected`;
+    console.log(selecionada.className);
   }
 }
 
+function removeColorSelectedBerfore() {
+  const color = document.querySelectorAll('.color');
+  for (let i = 0; i < color.length; i += 1) {
+    console.log(color[i].className);
+    color[i].className = color[i].className.replace(/selected/, '');
+    console.log('agora é ', color[i].className);
+    console.log('');
+  }
+}
+
+function getColorString(evento) {
+  removeColorSelectedBerfore();
+  modifyColorToColorSelected(evento.target.className.split(' ')[1]);
+}
+
+function selectColor() {
+  const color = document.querySelectorAll('.color');
+  color.forEach(function (item) {
+    item.addEventListener('click', getColorString);
+  });
+}
+
 adicionaPixel(25);
+modifyColorToColorSelected();
 selectColor();
