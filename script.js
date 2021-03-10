894065035c4a48ad83a5e86299b35813738a0989
Orlando Flores <orlando_flores@id.uff.br>
2021-03-09 21:14:02 -0300

    Salvando sem passar no requisito 8

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.onload = function () {
+window.onload = () => {
   const lines = document.getElementsByClassName('pixel-line');
   for (let index = 0; index < lines.length; index += 1) {
     for (let column = 1; column <= 5; column += 1) {
@@ -9,19 +9,35 @@ window.onload = function () {
   }
   const blackPalette = document.getElementById('black-palette');
   blackPalette.className += ' selected';
+
 };
 
-function setSelectedTask(element) {
+  function setSelectedTask(element) {
   const selectedTask = document.getElementsByClassName('selected')[0];
   selectedTask.className = 'color';
   element.className = 'color selected';
-}
+  }
 
-const paletteColors = document.getElementsByClassName('color');
-for (let index = 0; index < paletteColors.length; index += 1) {
-  paletteColors[index].addEventListener('click', function (event) {
+  const paletteColors = document.getElementsByClassName('color');
+  for (let index = 0; index < paletteColors.length; index += 1) {
+  paletteColors[index].addEventListener('click', (event) => {
     if (event.target.className === 'color') {
-      setSelectedTask(paletteColors[index]);
+    setSelectedTask(paletteColors[index]);
     }
   });
-}
+  }
+
+  function setPixelColor(element) {
+    const selectedTask = document.querySelector('.selected');
+    const color = window.getComputedStyle(selectedTask).backgroundColor;
+    element.style.backgroundColor = color;
+  }
+
+  const pixels = document.querySelectorAll('.pixel');
+  for (let index = 0; index < pixels.length; index += 1) {
+    pixels[index].addEventListener('click', () => {
+      setPixelColor(pixels[index]);
+    });
+  }
+
+
