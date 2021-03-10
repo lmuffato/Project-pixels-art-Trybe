6e54f49418f24282164d7452bc6c5cad685969d9
JuliaBaptista <juliabaptistadeoliveira@gmail.com>
2021-03-10 18:48:52 -0300

    Correçao requisito 6

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ function blackSelected() {
 function selectedColors() {
   const setColors = document.getElementsByClassName('color');
   for (let index = 0; index < setColors.length; index += 1) {
-      setColors[index].addEventListener('click', function (event) {
-          if (event.target.className === 'color') {
-              const firstSelected = document.querySelector('.selected');
-              selected.className = 'color';
-              setColors[index].className = 'color selected';
-          }
-      });      
+    setColors[index].addEventListener('click', function (event) {
+      if (event.target.className === 'color') {
+        const firstSelected = document.querySelector('.selected');
+        firstSelected.className = 'color';
+        setColors[index].className = 'color selected';
+      }
+    }); 
   }
 }
 
