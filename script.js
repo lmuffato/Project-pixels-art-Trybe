ea3631bbcf83ecdb8b0a2bf4178bfa046641aa61
Luiz Otavio <luiz.otavio273@gmail.com>
2021-03-09 23:08:50 -0300

    Requesitos 4 e 5 - Adiciona quadro de pixels com largura e altura de 40px com uma borda de 1px

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Requesito 2 e 3
 let paletColors = ['black', '#14213d', '#fca311', '#e5e5e5'];
 let tableColors = document.createElement('tr');
 document.querySelector('table#color-palette').appendChild(tableColors);
@@ -7,3 +8,16 @@ for (let index = 0; index < paletColors.length; index += 1) {
   document.getElementsByClassName('color')[index].style.background = paletColors[index];
 }
 
+//Requesito 4 e 5
+let amountFrame = 6
+
+for (let index = 1; index < amountFrame; index += 1) {
+  let framePixels = document.createElement('tr');
+  document.getElementById('pixel-board').appendChild(framePixels);
+
+  for (let counter = 1; counter < amountFrame; counter += 1) {
+    document.getElementsByTagName('tr')[index].appendChild(document.createElement('td')).className = 'pixel';
+  }
+}
+
+//Requesito 6
