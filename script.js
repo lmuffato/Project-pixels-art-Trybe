300f261629e3ac17c35f099697b13555a653f925
Marcelo Maurício <mmauricio.design@gmail.com>
2021-03-10 11:15:23 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.onload = function () {
+window.onload = () => {
   document.getElementById('black').classList.add('selected');
 };
 
@@ -14,6 +14,8 @@ function selectColorPaint(event) {
   console.log(currentColor);
 }
 
+// Na função paintingPixels eu utilizei o getComputedStyle e o getPropertyValue graças a dica do André Jaques no slack (https://trybecourse.slack.com/archives/C01L16B9XC7/p1615328182310300) e ai eu consegui aplicar ela a minha lógica.
+
 function paintingPixels(event) {
   const pixelColor = event.target;
   const colorPickup = document.querySelector('.selected');
@@ -23,7 +25,16 @@ function paintingPixels(event) {
   console.log(pixelColor);
   console.log(colorStyles);
 }
+// Na função clearBoard eu utilizei o código do Leonardo Mallmann (https://github.com/mallmann02), onde o forEach vai aplicar a função setAttribute para cada .pixel encontrado. A função troca o valor para a opção setada. Pois eu estava fazendo com o .style.backgroundColor e não estava indo, pq ele já tinha aquele atributo.
+
+function clearBoard() {
+  const coloredPixels = document.querySelectorAll('.pixel');
+  coloredPixels.forEach((px) => px.setAttribute('style', 'background-color: white;'));
+}
 
 document.getElementById('color-palette').addEventListener('click', selectColorPaint);
-
 document.getElementById('pixel-board').addEventListener('click', paintingPixels);
+
+const button = document.getElementById('clear-board');
+
+button.addEventListener('click', clearBoard);
