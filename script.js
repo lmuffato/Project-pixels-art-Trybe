d1cf86592c45b0ad6fd5a06f5133ae25e6b8dcd6
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-03-10 16:17:41 -0300

    Requisito 7 - ESlint(4)

--- script.js
+++ script.js
@@ -16,33 +16,33 @@ function createPixBox(size) {
 createPixBox(5);
 
 function eventChangingColorsPalette() {
-  var getClassSelected = document.getElementsByClassName('selected')[0];
-  var colorBlack = document.getElementsByClassName('color')[0];
-  var colorRed = document.getElementsByClassName('color')[1];
-  var colorBlue = document.getElementsByClassName('color')[2];
-  var colorGreen = document.getElementsByClassName('color')[3];
+  let getClassSelected = document.getElementsByClassName('selected')[0];
+  let colorBlack = document.getElementsByClassName('color')[0];
+  let colorRed = document.getElementsByClassName('color')[1];
+  let colorBlue = document.getElementsByClassName('color')[2];
+  let colorGreen = document.getElementsByClassName('color')[3];
 
-  colorBlack.addEventListener('click', function script () {
+  colorBlack.addEventListener('click', function() {
     getClassSelected = document.getElementsByClassName('selected')[0];
     getClassSelected.classList.remove('selected');
     colorBlack.classList.add('selected');
-    });
+  });
 
-  colorRed.addEventListener('click', function script () {
+  colorRed.addEventListener('click', function() {
     getClassSelected = document.getElementsByClassName('selected')[0];
     getClassSelected.classList.remove('selected');
     colorRed.classList.add('selected');
-    });
+  });
 
-  colorBlue.addEventListener('click', function script () {
+  colorBlue.addEventListener('click', function() {
     getClassSelected = document.getElementsByClassName('selected')[0];
     getClassSelected.classList.remove('selected');
     colorBlue.classList.add('selected');
-    });
-  colorGreen.addEventListener('click', function script () {
+  });
+  colorGreen.addEventListener('click', function() {
     getClassSelected = document.getElementsByClassName('selected')[0];
     getClassSelected.classList.remove('selected');
     colorGreen.classList.add('selected');
-    });
+  });
 }
 eventChangingColorsPalette();
