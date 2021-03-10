35e9deb6b1e8cb1bdd72508858bb1c986b467104
Eduardo Seije <eduardoseije@gmail.com>
2021-03-10 19:16:18 -0300

    Requisito 09 - utilizando o metodo location.reload para recarregar a página e comentários ao código

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+// Após muita pesquisa e estudo o event.target permitiu refatorar o código para ficar menor
+// mesmo assim o problema para pintar pixels estava no html que não tinha background color, que estava definido no css. 
+
 function colorSelect() {
   let defaultColors = document.querySelectorAll('.color');
   for (let index = 0; index < defaultColors.length; index += 1){
     defaultColors[index].addEventListener('click', onClick);
   }
   function onClick(event) {
-    let selectedColor = event.target;
+    let selectedColor = event.target; // event.target - determina que a variável sera o alvo do evento em questão, ou seja, o que vai sofrer o resultado da função.
     let defaultColorBlack = document.querySelector('.selected');
     defaultColorBlack.classList.remove('selected');
     selectedColor.classList.add('selected');
@@ -25,11 +28,7 @@ function changePixelsColor() {
 changePixelsColor();
 
 function clearButton() {
-  let pixels = document.querySelectorAll('.pixels');
-  let clearBtn = document.getElementById('clear-board');
-  for (let index = 0; index < pixels.length; index += 1) {
-    pixels[index].style.backgroundColor = 'white';
-    clearBtn.addEventListener('click', clearButton); 
-  }
+  document.location.reload() // o método reload recarrega toda a página para sua configuração inicial, tornando os pixels brancos.
 }
-clearButton();
+let clearBtn = document.getElementsByTagName('button')[0];
+clearBtn.addEventListener('click', clearButton);
