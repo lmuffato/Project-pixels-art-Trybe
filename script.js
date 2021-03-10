eefc880a516a042ab3ac0c1adebd874a2d8b9591
Priscila <prihscapin@gmail.com>
2021-03-10 19:19:17 -0300

    Requisitos 1 a 5 agora vai!

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+//requisito 4
+function criaPixels (numberOfPixels) {
+   for (let index = 0; index < numberOfPixels * numberOfPixels; index +=1) {
+   const fazDiv = document.createElement('div');
+   fazDiv.className = 'pixel tr';
+   const divMae = document.getElementById('pixel-board');
+   divMae.append(fazDiv);
+}
+}
+criaPixels(5)
+
+
 // reqisito 9
 
 //let clean = document.getElementById('clear-board');
