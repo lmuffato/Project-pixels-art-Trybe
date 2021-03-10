81cf9257144ce2821f96d09287887f22737e0b93
Lucas Henrique Brito <lucas.silva.brito@hotmail.com.br>
2021-03-09 21:23:56 -0300

    Requisito 4-6

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ colors[0].style.backgroundColor = 'black';
 colors[1].style.backgroundColor = 'red';
 colors[2].style.backgroundColor = 'green';
 colors[3].style.backgroundColor = 'blue';
+
+function pixelBoard (number) {
+  for (let i = 0; i < number; i += 1) {
+    for (let j = 0; j < number; j += 1) {
+      const pixel = document.createElement('div');
+      pixel.className = 'pixel'
+      document.querySelector('#pixel-board').appendChild(pixel);
+    }
+  }
+}
+
+pixelBoard(5);
