590ed6c3748dc501b79a60e8c776d51a919fb33d
Beatriz Estebanez <bia.estebanez@gmail.com>
2021-03-10 11:28:51 -0300

    acresceta funçao para armazenar a currentColor e coloca para inicar com o preto

--- script.js
+++ script.js
@@ -8,26 +8,32 @@ for (let index = 1; index <= 25; index += 1) {
 
 // adiciona classe selected à cor preta
 document.getElementById('color-palette').firstElementChild.classList.add('selected');
+let currentColor = armazenaCor ();
 
 // recupera os pixels e a paleta de cores
 let selectedPixel = document.querySelectorAll('.pixel');
 const element = document.querySelectorAll('.color');
-let currentColor;
 
+// adiciona evento: ao clicar, troca na paleta a classe 'selected' e armazena a cor a ser usada
 for (let index = 0; index < 4; index += 1) {
   element[index].addEventListener('click', function () {
     document.querySelector('.selected').classList.remove('selected');
     element[index].classList.add('selected');
-    currentColor = window.getComputedStyle(document.querySelector('.selected'),null).getPropertyValue("background-color");
+    currentColor = armazenaCor ();
   })
 }
 
+// adiciona evento: troca a cor do pixel quando clicado
 for (let indexPixel = 0; indexPixel < 25; indexPixel += 1) {
   selectedPixel[indexPixel].addEventListener('click', function () {
     selectedPixel[indexPixel].style.backgroundColor = currentColor;
   })
 }
 
+function armazenaCor () {
+  let corArmazenar = window.getComputedStyle(document.querySelector('.selected'),null).getPropertyValue("background-color");
+  return corArmazenar;
+}
 
   /* if (element[index].className === 'selected') {
       pixel[2].style.backgroundColor = element[index].style.backgroundColor;
