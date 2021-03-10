619cdfd27c80ad396aff707e5573582d35c99330
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2021-03-09 21:45:22 -0300

    Requisitos 8 e 9

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 window.onload = function () {
 
   function setColor() {
-    const colorPalette = document.getElementById("color-palette");
+    const colorPalette = document.getElementById('color-palette');
     colorPalette.addEventListener('click', function (e) {
       const colorElement = e.target
-      const colors = document.querySelectorAll(".color");
+      const colors = document.querySelectorAll('.color');
       for (let index = 0; index < colors.length; index += 1) {
         colors[index].classList.remove('selected');
       }
@@ -14,11 +14,43 @@ window.onload = function () {
   }
   setColor();
 
-  function setInicialColor () {
-    const initialColor = document.querySelector("#black");
-    initialColor.classList.add("selected")
+  function setInicialColor() {
+    const initialColor = document.querySelector('#black');
+    initialColor.classList.add('selected')
+
+    setInicialColor();
   }
 
-  setInicialColor();
+  function setPixelColor() {
+    let board = document.getElementById('pixel-board');
+    board.addEventListener('click', function (e) {
+
+      const pixel = e.target
+
+      let selectedColor = document.querySelector(".selected")
+      console.log(selectedColor.id)
+
+
+      if (pixel.localName === 'div') {
+        pixel.style.backgroundColor = selectedColor.id
+      }
+
+    })
+  }
+
+  setPixelColor();
+
+  function clearBoard() {
+    let pixel = document.querySelectorAll('.pixel')
+    let button = document.querySelector('#clear-board')
+    button.addEventListener('click', function () {
+      for (let index = 0; index < pixel.length; index += 1) {
+        if (pixel[index].style.backgroundColor != "white")
+          pixel[index].style.backgroundColor = "white"
+      }
+    })
+  }
+
+clearBoard()
 
 }
