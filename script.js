6772a9529a9af2d2919d246e837dc1c40686ede4
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-03-09 23:05:41 -0300

    Requisito 4 e 5 - O 4 ESlint revisado(2) e o Requisito 5, colocando a altura e largura feito

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-function createPixBox () {
-    const getPixelBoard = document.querySelector('#pixel-board');
 
-    for (let index = 1; index < 25; index += 1) {
-        const pixelItem = document.createElement('div');
-        pixelItem.className = 'pixel';
-        getPixelBoard.appendChild(pixelItem);
-    }
-}
-
-createPixBox();
