aa3ad4beb4c4958e594742092a4db6d9f958d94f
defauth98 <neto.daniribeiro@gmail.com>
2021-03-10 12:34:01 -0300

    Requisito 08: Pintando o pixel com a cor selecionada

--- script.js
+++ script.js
@@ -12,4 +12,24 @@ function selectColor() {
   }
 }
 
+function drawColor() {
+  const pixelsListElements = document.getElementsByClassName('pixel');
+
+  for (let elementIndex = 0; elementIndex < pixelsListElements.length; elementIndex += 1) {
+    pixelsListElements[elementIndex].addEventListener('click', () => {
+      const selectElement = document.getElementsByClassName('selected')[0];
+      if (selectElement.classList.contains('background-black')) {
+        pixelsListElements[elementIndex].style.backgroundColor = 'black';
+      } else if (selectElement.classList.contains('background-red')) {
+        pixelsListElements[elementIndex].style.backgroundColor = 'red';
+      } else if (selectElement.classList.contains('background-blue')) {
+        pixelsListElements[elementIndex].style.backgroundColor = 'blue';
+      } else if (selectElement.classList.contains('background-yellow')) {
+        pixelsListElements[elementIndex].style.backgroundColor = 'yellow';
+      }
+    });
+  }
+}
+
 selectColor();
+drawColor();
