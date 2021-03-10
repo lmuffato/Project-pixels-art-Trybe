0c9c6ad86748099084b0cc0bb791a19bbb12d606
Carol <carolinavpinheiro@gmail.com>
2021-03-10 13:48:24 -0300

    Adicionando requisito 8

--- script.js
+++ script.js
@@ -4,40 +4,42 @@ const colorGreen = document.querySelector('#green');
 const colorOrange = document.querySelector('#orange');
 
 function addClass(color) {
-    color.classList.add('selected');
+  color.classList.add('selected');
 }
 
-function removeClass(color) {
-    color.classList.remove('selected');
+function removeClass(color1, color2, color3) {
+  color1.classList.remove('selected');
+  color2.classList.remove('selected');
+  color3.classList.remove('selected');
 }
 
-colorBlack.addEventListener('click', function (){
-    addClass(colorBlack);
-    removeClass(colorPink);
-    removeClass(colorGreen);
-    removeClass(colorOrange);
-    console.log(colorPink); 
-})
+colorBlack.addEventListener('click', () => {
+  addClass(colorBlack);
+  removeClass(colorPink, colorGreen, colorOrange);
+});
 
-colorPink.addEventListener('click', function () {
-    addClass(colorPink);
-    removeClass(colorBlack);
-    removeClass(colorGreen);
-    removeClass(colorOrange);
-    console.log(colorPink);
-})
+colorPink.addEventListener('click', () => {
+  addClass(colorPink);
+  removeClass(colorBlack, colorGreen, colorOrange);
+});
 
+colorGreen.addEventListener('click', () => {
+  addClass(colorGreen);
+  removeClass(colorBlack, colorPink, colorOrange);
+});
 
-colorGreen.addEventListener('click', function () {
-    addClass(colorGreen);
-    removeClass(colorBlack);
-    removeClass(colorPink);
-    removeClass(colorOrange);
-})
+colorOrange.addEventListener('click', () => {
+  addClass(colorOrange);
+  removeClass(colorBlack, colorPink, colorGreen);
+});
 
-colorOrange.addEventListener('click', function () {
-    addClass(colorOrange);
-    removeClass(colorBlack);
-    removeClass(colorPink);
-    removeClass(colorGreen);     
-})
+const pixels = document.querySelectorAll('.pixel');
+
+function getColorPalette(e) {
+  e.target.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
+}
+
+for (let index = 0; index < pixels.length; index += 1) {
+  let eachPixel = pixels[index];
+  eachPixel.addEventListener('click', getColorPalette);
+}
