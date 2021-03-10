bf76c7d7e641fd8dcf64d875ae0fc201308eefda
luanlsr <luanfswd@gmail.com>
2021-03-09 22:59:28 -0300

    terminado requisito 9

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const element3 = document.getElementsByClassName('color')[2];
 const element4 = document.getElementsByClassName('color')[3];
 
 // window.onload () => {
-  // element1.classList.add('selected');
+// element1.classList.add('selected');
 
 element1.addEventListener('click', () => {
   element1.classList.add('selected');
@@ -12,8 +12,7 @@ element1.addEventListener('click', () => {
   element3.classList.remove('selected');
   element4.classList.remove('selected');
   console.log('Cliquei no PRETO');
-
-})
+});
 
 element2.addEventListener('click', () => {
   element2.classList.add('selected');
@@ -21,7 +20,7 @@ element2.addEventListener('click', () => {
   element3.classList.remove('selected');
   element4.classList.remove('selected');
   console.log('Cliquei no VERMELHO');
-})
+});
 console.log(element2);
 
 element3.addEventListener('click', () => {
@@ -30,7 +29,7 @@ element3.addEventListener('click', () => {
   element1.classList.remove('selected');
   element4.classList.remove('selected');
   console.log('Cliquei no AZUL');
-})
+});
 console.log(element3);
 
 element4.addEventListener('click', () => {
@@ -39,24 +38,40 @@ element4.addEventListener('click', () => {
   element3.classList.remove('selected');
   element1.classList.remove('selected');
   console.log('Cliquei no VERDE');
-})
+});
 console.log(element4);
 
 function chooseColor() {
   const selection = document.querySelector('.selected');
-  let bgColor = window.getComputedStyle(selection, null).getPropertyValue("background-color");
+  const bgColor = window.getComputedStyle(selection, null).getPropertyValue("background-color");
   return bgColor;
-  
 }
 
-document.addEventListener('click', clique);
-function clique(e){
-  let section = document.querySelector('section');
-  let header = document.querySelector('header');
-  let body = document.querySelector('body');
-  let h1 = document.querySelector('h1');
-  if (e.target !== body && e.target !== header && e.target !== section && e.target !== h1){
- e.target.style.backgroundColor = chooseColor();
+
+document.addEventListener('click', fillPixel);
+function fillPixel(e){
+  const section = document.querySelector('section');
+  const header = document.querySelector('header');
+  const body = document.querySelector('body');
+  const h1 = document.querySelector('h1');
+  const button = document.querySelector('button');
+  if (e.target !== body && e.target !== header && e.target !== section && e.target !== h1 && e.target !== button){
+    e.target.style.backgroundColor = chooseColor();
+  };
+}
+  const clear = document.getElementById('clear-board');
+
+  clear.addEventListener('click', cleaning)
+
+  function cleaning() {
+  let pix = document.querySelectorAll('.pixel');
+  // consultei o pullRequest https://github.com/tryber/sd-010-a-project-pixels-art/pull/119/files 
+  // para obter uma idéia de como fazer essa parte abaixo e então percebi que o pix recebe um Array
+  // com todos os elementos de class pixel. Logo eu teria que fazer um loop para percorrê-lo.
+  // O Jodial usou um for simples. Eu pesquisei no canal do CFB Cursos e aprendi sobre a utilização 
+  // do for of.
+  for (let pixels of pix){
+    pixels.style.backgroundColor = 'white'
+    console.log('Cliquei no Limpar');
   }
 };
-
