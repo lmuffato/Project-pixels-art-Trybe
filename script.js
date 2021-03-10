f71e5dc0c35255bee29d3baf438e36b1b8f68abb
guilhermemd <guilherme.md@gmail.com>
2021-03-10 20:42:57 -0300

    requisito 02 - troca let por const

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let blackBox = document.getElementsByClassName('color')[0];
+const blackBox = document.getElementsByClassName('color')[0];
 blackBox.style.backgroundColor = 'black';
-let greenBox = document.getElementsByClassName('color')[1];
+const greenBox = document.getElementsByClassName('color')[1];
 greenBox.style.backgroundColor = 'green';
-let yellowBox = document.getElementsByClassName('color')[2];
+const yellowBox = document.getElementsByClassName('color')[2];
 yellowBox.style.backgroundColor = 'yellow';
-let purpleBox = document.getElementsByClassName('color')[3];
+const purpleBox = document.getElementsByClassName('color')[3];
 purpleBox.style.backgroundColor = 'purple';
