dd6f2da578366b017fb15aad0570013a7f4788f2
Guilherme Sucupira Oliveira <guilherme.so23@gmail.com>
2021-03-09 21:50:41 -0300

    Concluido exercicio 6

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-window.onload = function() {
+window.onload = function () {
   const color = ['black', 'red', 'blue', 'yellow'];
-  colorButton();
   function colorButton() {
     const colorBlock = document.querySelectorAll('.color');
     for (let index = 0; index < colorBlock.length; index += 1) {
       colorBlock[index].style.background = color[index];
     }
   }
+  function selectColor() {
+    document.querySelectorAll('.color')[0].classList.add("selected");
+  }
+  colorButton();
+  selectColor();
 }
