858f8f73517b8900396a7ae40abc73167279c640
André Jaques Batista <andrejaques.b@gmail.com>
2021-03-10 20:37:21 -0300

    change the script window.onload to the defer attribute in html

--- script.js
+++ script.js
@@ -1,39 +1,37 @@
 // Scripts start
-window.onload = function () {
-    /* ------------ Colors Container ------------ */
-    const colors = document.querySelectorAll('.color');
+/* ------------ Colors Container ------------ */
+const colors = document.querySelectorAll('.color');
 
-    function clickToSelectColor(e) {
-        let selectedItem = document.querySelector('.selected');
-        selectedItem.classList.remove('selected');
-        e.target.classList.add('selected');
-    }
+function clickToSelectColor(e) {
+    let selectedItem = document.querySelector('.selected');
+    selectedItem.classList.remove('selected');
+    e.target.classList.add('selected');
+}
 
-    for (let color = 0; color < colors.length; color += 1) {
-        colors[color].addEventListener('click', clickToSelectColor);
-    }
+for (let color = 0; color < colors.length; color += 1) {
+    colors[color].addEventListener('click', clickToSelectColor);
+}
 
-    /* -------------- Pixels Container -------------- */
-    const pixels = document.querySelectorAll('.pixel');
+/* -------------- Pixels Container -------------- */
+const pixels = document.querySelectorAll('.pixel');
 
-    function clickToSetColor(e) {
-        let selectedItem = document.querySelector('.selected');
-        let selectedColor = window.getComputedStyle(selectedItem).getPropertyValue('background-color');
-        e.target.style.backgroundColor = selectedColor;
-    }
+function clickToSetColor(e) {
+    let selectedItem = document.querySelector('.selected');
+    let selectedColor = window.getComputedStyle(selectedItem).getPropertyValue('background-color');
+    e.target.style.backgroundColor = selectedColor;
+}
 
+for (let pixel = 0; pixel < pixels.length; pixel += 1) {
+    pixels[pixel].addEventListener('click', clickToSetColor);
+}
+
+/* -------------- Clean Button ----------------- */
+const clearButton = document.querySelector('#clear-board')
+
+function clickToClean() {
     for (let pixel = 0; pixel < pixels.length; pixel += 1) {
-        pixels[pixel].addEventListener('click', clickToSetColor);
+        pixels[pixel].style.backgroundColor = 'white';
     }
+}
 
-    /* -------------- Clean Button ----------------- */
-    const clearButton = document.querySelector('#clear-board')
-
-    function clickToClean() {
-        for (let pixel = 0; pixel < pixels.length; pixel += 1) {
-            pixels[pixel].style.backgroundColor = 'white';
-        }
-    }
-
-    clearButton.addEventListener('click', clickToClean);
-};
+clearButton.addEventListener('click', clickToClean);
