6aff679e8151204a7278de577b3433684d4e4b53
Max Rudim <max.hermsdorff@gmail.com>
2021-03-10 16:38:44 -0300

    Tentativa requisitos 6 e 7, verificar requisitos anteriore

--- script.js
+++ script.js
@@ -3,11 +3,20 @@ function palette() {
     const color = document.querySelectorAll('.color');
     const arrayColors = ['black', 'blue', 'yellow', 'green'];
     for (let index = 0; index < color.length; index += 1) {
-      color[index].style.backgroundColor = arrayColors[index];
-      if (arrayColors[index] === 'black') {
+      color[index].style.backgroundColor = arrayColors[index]; //cada uma das paletas fica com a cor correspondente ao array.
+      if (arrayColors[index] === 'black') { //seleciona a cor inicial preta.
         color[index].className += ' selected';
       }
     }
   }
   
-  window.onload = palette;
+  function getColor() {
+    const colorArray = document.querySelector('#color-palette');
+    colorArray.addEventListener('click', (event) => { // adiciona listener do evento click nas paletas
+      const selectedColor = event.target; // guarda a cor da paleta em que clicou
+      document.querySelector('.selected').className = 'color';
+      selectedColor.className += ' selected'; //adiciona a classe selected à cor da paleta que clicou.
+    });
+  }
+  
+  getColor();
