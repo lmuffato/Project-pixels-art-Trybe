11bdbb44cf9f5ea5befa260ce155f96b44819532
JuliaBaptista <juliabaptistadeoliveira@gmail.com>
2021-03-10 18:43:57 -0300

    Conclusao requisito 7

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 function blackSelected() {
-  let blackColor1 = document.getElementById('color1');
+  const blackColor1 = document.getElementById('color1');
   blackColor1.className += ' selected';
 }
 
 function selectedColors() {
-  
+  const setColors = document.getElementsByClassName('color');
+  for (let index = 0; index < setColors.length; index += 1) {
+      setColors[index].addEventListener('click', function (event) {
+          if (event.target.className === 'color') {
+              const firstSelected = document.querySelector('.selected');
+              selected.className = 'color';
+              setColors[index].className = 'color selected';
+          }
+      });      
+  }
 }
 
 window.onload = () => {
   blackSelected();
+  selectedColors();
 };
