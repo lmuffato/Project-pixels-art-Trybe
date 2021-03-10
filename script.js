2cdd1d1d41af1f3f23da69d52d3ce411c19efed1
Guilherme Sucupira Oliveira <guilherme.so23@gmail.com>
2021-03-10 15:24:01 -0300

    Concluido exercicio 9

--- script.js
+++ script.js
@@ -1,24 +1,6 @@
 const color = ['black', 'red', 'blue', 'yellow'];
 let old = 0;
 let select = 'black';
-window.onload = function init() {
-  function colorButton() {
-    const colorBlock = document.querySelectorAll('.color');
-    for (let index = 0; index < colorBlock.length; index += 1) {
-      colorBlock[index].style.background = color[index];
-      colorBlock[index].addEventListener("click", selectColor);
-    }
-  }
-
-  function colorPixels() {
-    const colorPixel = document.querySelectorAll('.pixel');
-    for (let index = 0; index < colorPixel.length; index += 1) {
-      colorPixel[index].addEventListener("click", pixels);
-    }
-  }
-  colorButton();
-  colorPixels();
-}
 
 function pixels(e) {
   e.target.style.setProperty('background', select);
@@ -34,8 +16,42 @@ function selectIndex(colorIndex) {
 
 function selectColor(e) {
   select = e.target.style.background;
-  let index = selectIndex(select);
-  document.querySelectorAll('.color')[old].classList.remove("selected");
-  document.querySelectorAll('.color')[index].classList.add("selected");
+  const index = selectIndex(select);
+  document.querySelectorAll('.color')[old].classList.remove('selected');
+  document.querySelectorAll('.color')[index].classList.add('selected');
   old = index;
 }
+
+function clearPixels() {
+  const colorPixel = document.querySelectorAll('.pixel');
+  for (let index = 0; index < colorPixel.length; index += 1) {
+    colorPixel[index].style.background = 'white';
+  }
+}
+
+function colorButton() {
+  const colorBlock = document.querySelectorAll('.color');
+  for (let index = 0; index < colorBlock.length; index += 1) {
+    colorBlock[index].style.background = color[index];
+    colorBlock[index].addEventListener('click', selectColor);
+  }
+}
+
+function colorPixels() {
+  const colorPixel = document.querySelectorAll('.pixel');
+  for (let index = 0; index < colorPixel.length; index += 1) {
+    colorPixel[index].addEventListener('click', pixels);
+  }
+}
+
+function buttonClear() {
+  const clearAll = document.getElementById('clear-board');
+  clearAll.addEventListener('click', clearPixels);
+}
+
+function init() {
+  colorButton();
+  colorPixels();
+  buttonClear();
+}
+window.onload = init;
