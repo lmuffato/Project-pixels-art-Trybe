1b956323bee010283e1f61fa185838338f0a9aa8
Orlando Flores <orlando_flores@id.uff.br>
2021-03-10 11:33:44 -0300

    requisito 10

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-function setPixels() {
-  const lines = document.getElementsByClassName('pixel-line');
-  for (let index = 0; index < lines.length; index += 1) {
-    for (let column = 1; column <= 5; column += 1) {
-      const div = document.createElement('div');
-      div.className = 'pixel';
-      lines[index].appendChild(div);
+function setPixels(number) {
+  for (let index = 1; index <= number; index += 1) {
+    const divLine = document.createElement('div');
+    const line = document.getElementById('pixel-board');
+    divLine.className = 'pixel-line';
+    line.appendChild(divLine);
+    for (let column = 1; column <= number; column += 1) {
+      const divColumn = document.createElement('div');
+      divColumn.className = 'pixel';
+      divLine.appendChild(divColumn);
     }
   }
 }
@@ -38,6 +41,49 @@ function setPixelColor() {
   }
 }
 
+function deleteBoard() {
+  const board = document.getElementById('pixel-board');
+  board.remove();
+  const section = document.createElement('section');
+  section.id = 'pixel-board';
+  document.body.appendChild(section);
+}
+
+function createInputText() {
+  const header = document.querySelector('#header');
+  const input = document.createElement('input');
+  input.id = 'board-size';
+  input.type = 'text';
+  input.min = 5;
+  input.max = 50;
+  input.placeholder = 'Digite o número de pixels';
+  input.style.marginLeft = '550px';
+  input.style.marginBottom = '15px';
+  header.appendChild(input);
+}
+
+function createInputBtn() {
+  const header = document.querySelector('#header');
+  const btnInput = document.createElement('button');
+  btnInput.id = 'generate-board';
+  btnInput.innerText = 'VQV';
+  header.appendChild(btnInput);
+  btnInput.style.marginLeft = '20px';
+  btnInput.style.padding = '10px';
+}
+
+function createBoardSize() {
+  createInputText();
+  createInputBtn();
+  const btn = document.getElementById('generate-board');
+  const input = document.getElementById('board-size');
+  btn.addEventListener('click', () => {
+    const number = input.value;
+    deleteBoard();
+    setPixels(number);
+  });
+}
+
 function createClearBtn() {
   const header = document.querySelector('#header');
   const btnClear = document.createElement('button');
@@ -51,16 +97,17 @@ function clearBoard() {
   btn.addEventListener('click', () => {
     const pixels = document.querySelectorAll('.pixel');
     for (let index = 0; index < pixels.length; index += 1) {
-      pixels[index].style.backgroundColor = 'white'
+      pixels[index].style.backgroundColor = 'white';
     }
-  })
+  });
 }
 
 window.onload = () => {
-  setPixels();
+  setPixels(5);
   setBlackSelected();
   setSelectedTask();
   setPixelColor();
+  createBoardSize();
   createClearBtn();
-  clearBoard()
+  clearBoard();
 };
