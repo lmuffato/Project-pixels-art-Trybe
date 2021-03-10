44dc415fc3c46d448dd0618783b181426db29b06
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2021-03-09 22:09:50 -0300

    desafio 4 feito

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-    let palette = document.getElementsByClassName('color');
+let palette = document.getElementsByClassName('color');
 
-    function GenerateColor(){
-    let red = parseInt(Math.random * 255);
-    let green = parseInt(Math.random * 255);
-    let blue = parseInt(Math.random * 255);
-    let color = (red, green, blue);
+let number = 25;
+
+function GenerateColor() {
+  let red = parseInt(Math.random * 255);
+  let green = parseInt(Math.random * 255);
+  let blue = parseInt(Math.random * 255);
+  let color = (red, green, blue);
+  console.log(color);
 }
+
+function GenerateBoard(number) {
+  for (let index = 0; index < number; index += 1){
+  let bigsquare = document.querySelector('#pixel-board');
+  let squares = document.createElement('div');
+  squares.className = 'pixel';
+  bigsquare.appendChild(squares);
+  }
+}
+
+GenerateBoard(number);
