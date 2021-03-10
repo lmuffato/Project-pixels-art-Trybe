2316f00a1b1d54122511ea1c3ee209cdca79021f
Marcus C. H. Filho <mchusemann@gmail.com>
2021-03-10 02:56:14 -0300

    :rocket: feat(Requirement- 10 & 11): Generates a board with size inserted in the input and validates for values 5 to 50

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-// - A `classe` `selected` deve ser adicionada à cor selecionada na paleta, ao mesmo tempo em que é removida da cor anteriormente selecionada;
+let selectedPalette = 'black';
 
-// - Somente uma das cores da paleta deve ter a `classe` `selected` de cada vez;
+function clickedPixelElement(event) {
+  event.target.style.setProperty('background-color', selectedPalette);
+}
 
-// - Note que os elementos que deverão receber a classe `selected` devem ser os mesmos elementos que possuem a classe `color`, como especificado no **requisito 2**.
+const pixels = document.getElementsByClassName('pixel');
+
+function createPixelsListeners() {
+  for (let index = 0; index < pixels.length; index += 1) {
+    pixels[index].addEventListener('click', clickedPixelElement);
+  }
+}
+
+window.onload = createPixelsListeners;
 
 const colorPalettes = document.getElementsByClassName('color');
-const pixels = document.getElementsByClassName('pixel');
-let selectedPalette = 'black';
+
 function removeSelected(value) {
   for (let index = 0; index < colorPalettes.length; index += 1) {
     if (value !== colorPalettes[index]) {
@@ -27,13 +36,41 @@ for (let index = 0; index < colorPalettes.length; index += 1) {
   colorPalettes[index].addEventListener('click', clickedPaletteElement);
 }
 
-function clickedPixelElement(event) {
-  event.target.style.setProperty('background-color', selectedPalette);
+function inputValidation() {
+  let inputValue = document.getElementById('board-size').value;
+  if (inputValue === '') {
+    alert('Board inválido!');
+    inputValue = 5;
+  }
+  if (inputValue > 50) {
+    inputValue = 50;
+  }
+  if (inputValue < 5) {
+    inputValue = 5;
+  }
+  return inputValue;
 }
 
-for (let index = 0; index < pixels.length; index += 1) {
-  pixels[index].addEventListener('click', clickedPixelElement);
+function generateBoard() {
+  const inputValue = inputValidation();
+  const pixelBoard = document.getElementById('board-body');
+  pixelBoard.innerHTML = '';
+
+  for (let indexRow = 0; indexRow < inputValue; indexRow += 1) {
+    const boardRow = document.createElement('tr');
+    boardRow.classList.add('pixel-row');
+    pixelBoard.appendChild(boardRow);
+    for (let indexColumn = 0; indexColumn < inputValue; indexColumn += 1) {
+      const boardItem = document.createElement('td');
+      boardItem.classList.add('pixel');
+      boardRow.appendChild(boardItem);
+    }
+  }
+  createPixelsListeners();
 }
+
+const vqvButtom = document.getElementById('generate-board');
+vqvButtom.addEventListener('click', generateBoard);
 
 function clearBoard() {
   for (let index = 0; index < pixels.length; index += 1) {
@@ -43,3 +80,19 @@ function clearBoard() {
 
 const clearButton = document.getElementById('clear-board');
 clearButton.addEventListener('click', clearBoard);
+
+// - Crie um input e um botão que permitam definir um quadro de pixels com tamanho entre 5 e 50. Ao clicar no botão, deve ser gerado um quadro de **N** pixels de largura e **N** pixels de altura, onde **N** é o número inserido no input;
+
+// - O input deve ter o `id` denominado `board-size` e o botão deve ter o `id` denominado `generate-board`;
+
+// - O input só deve aceitar número maiores que zero. Essa restrição **deve** ser feita usando os atributos do elemento `input`;
+
+// - O botão deve conter o texto "VQV";
+
+// - O input deve estar posicionado entre a paleta de cores e o quadro de pixels;
+
+// - O botão deve estar posicionado ao lado do input;
+
+// - Se nenhum valor for colocado no input ao clicar no botão, mostre um `alert` com o texto: "Board inválido!";
+
+// - O novo quadro deve ter todos os pixels preenchidos com a cor branca.
