dfd8ad0202c89814edac7f1ed495dc9ff555e2c2
Caio Oliveira <parafrenteeadiante@gmail.com>
2021-03-10 19:03:27 -0300

    enviando o requisito 8

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function addCliclListener() {
+function addPaletteListener() {
   const colorElement = document.getElementsByClassName('color');
 
   for (let index = 0; index < colorElement.length; index += 1) {
@@ -13,4 +13,19 @@ function changeSelected(eventOrigin) {
   clickedNow.classList.add('selected');
 }
 
-addCliclListener();
+function addPixelListener() {
+  const pixels = document.getElementsByClassName('pixel');
+  
+  for (let index = 0; index < pixels.length; index +=1) {
+    pixels[index].addEventListener('click', applyColor);
+  }
+}
+
+function applyColor(eventOrigin){
+  const selectedColor = document.querySelector('.selected').style.backgroundColor;
+  const clickedPixel = eventOrigin.target;
+  clickedPixel.style.backgroundColor = selectedColor;
+}
+
+addPaletteListener();
+addPixelListener();
