39f0835c94225d8f17c20595768e7b332c484cc2
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-03-10 18:38:13 -0300

    Requisitos - Adicionando as Referencias Usadas

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Exercício feito com a ajuda do Maurício Viegas
+// Link: https://trybecourse.slack.com/archives/C01L16B9XC7/p1615335717341100
 function createPixBox(size) {
   const getPixelBoard = document.querySelector('#pixel-board');
 
@@ -15,6 +17,8 @@ function createPixBox(size) {
 }
 createPixBox(5);
 
+// Exercício feito com a ajuda do código do Rodrigo Queiroz
+// Link: https://github.com/tryber/sd-010-a-project-pixels-art/blob/c8502885771f31176e2a4d4419f55ea00a60e36b/script.js
 function eventChangingColorsPalette() {
   let getClassSelected = document.getElementsByClassName('selected')[0];
   let colorBlack = document.getElementsByClassName('color')[0];
@@ -45,16 +49,21 @@ function eventChangingColorsPalette() {
 }
 eventChangingColorsPalette();
 
+// Exercício feito com a ajuda do código do Rodrigo Queiroz
+// Link: https://github.com/tryber/sd-010-a-project-pixels-art/blob/837d089ec94cbc9a9cbb9132f92a142476b27442/script.js
 // Window.getComputedStyle = retorna um objeto contendo os valores de todas as propriedades Css de um elemento
 function PaintingPixel(event) {
-  let getClassSelected = document.getElementsByClassName('selected') [0];
+  const getClassSelected = document.getElementsByClassName('selected')[0];
   const eventTarget = event.target;
-  let setColorPixel = window.getComputedStyle(getClassSelected).getPropertyValue('background-color');
+  let setColorPixel;
+  setColorPixel = window.getComputedStyle(getClassSelected).getPropertyValue('background-color');
   eventTarget.style.backgroundColor = setColorPixel;
 }
 
+// Exercício feito com a ajuda do código do Rodrigo Queiroz
+// Link: https://github.com/tryber/sd-010-a-project-pixels-art/blob/837d089ec94cbc9a9cbb9132f92a142476b27442/script.js
 function eventpaintingPixel() {
-  let getPixel = document.querySelectorAll('.pixel');
+  const getPixel = document.querySelectorAll('.pixel');
   for (let index = 0; index < getPixel.length; index += 1) {
     getPixel[index].addEventListener('click', PaintingPixel);
   }
