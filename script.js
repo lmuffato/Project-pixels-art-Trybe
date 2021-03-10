7f6665456ec60daa43e2cda2fda367929bdedc5a
luanlsr <luanfswd@gmail.com>
2021-03-10 16:30:40 -0300

    Corrigido erros do requisito 8 com ajuda do Renzo Sevilha

--- script.js
+++ script.js
@@ -46,32 +46,29 @@ function chooseColor() {
   const bgColor = window.getComputedStyle(selection, null).getPropertyValue("background-color");
   return bgColor;
 }
+//nessa parte tive uma grande ajuda do Renzo Sevilla para reduzir o código e melhorar uns bugs que tava dando ao colorir todo o conteúdo do document
+const pix = document.querySelectorAll('.pixel');
 
+for (const pixer of pix) {
+  pixer.addEventListener('click', fillPixel);
+}
+function fillPixel(e) {
 
-document.addEventListener('click', fillPixel);
-function fillPixel(e){
-  const section = document.querySelector('section');
-  const header = document.querySelector('header');
-  const body = document.querySelector('body');
-  const h1 = document.querySelector('h1');
-  const button = document.querySelector('button');
-  if (e.target !== body && e.target !== header && e.target !== section && e.target !== h1 && e.target !== button){
-    e.target.style.backgroundColor = chooseColor();
-  };
+  e.target.style.backgroundColor = chooseColor();
 }
   const clear = document.getElementById('clear-board');
 
   clear.addEventListener('click', cleaning)
 
   function cleaning() {
-  let pix = document.querySelectorAll('.pixel');
-  // consultei o pullRequest https://github.com/tryber/sd-010-a-project-pixels-art/pull/119/files 
-  // para obter uma idéia de como fazer essa parte abaixo e então percebi que o pix recebe um Array
-  // com todos os elementos de class pixel. Logo eu teria que fazer um loop para percorrê-lo.
-  // O Jodial usou um for simples. Eu pesquisei no canal do CFB Cursos e aprendi sobre a utilização 
-  // do for of.
-  for (let pixels of pix){
-    pixels.style.backgroundColor = 'white'
+ 
+// consultei o pullRequest https://github.com/tryber/sd-010-a-project-pixels-art/pull/119/files 
+// para obter uma idéia de como fazer essa parte abaixo e então percebi que o pix recebe um Array
+// com todos os elementos de class pixel. Logo eu teria que fazer um loop para percorrê-lo.
+// O Jodial usou um for simples. Eu pesquisei no canal do CFB Cursos e aprendi sobre a utilização 
+// do for of.
+  for (const pixels of pix) {
+    pixels.style.backgroundColor = 'white';
     console.log('Cliquei no Limpar');
   }
-};
+}
