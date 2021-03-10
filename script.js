f95083ad47eaf32965d959f3374778129e5cad40
lucas-paz <lucas.hpaz@outlook.com>
2021-03-10 18:29:37 -0300

    Corrige identação e adiciona requisito 9

--- script.js
+++ script.js
@@ -1,63 +1,61 @@
-//Função para criar as divs da paleta de cores;
 function paletteBox() {
-    let colors = ['black', 'blue', 'green', 'yellow']; //Cores da paleta
-    let paletteSelect = document.getElementById('color-palette');//acessa a div mãe
-    for (let index = 0; index < colors.length; index += 1) {
-        let paletteCreator = document.createElement('div'); //cria uma div
-        paletteCreator.style.background = colors[index];
-        paletteSelect.appendChild(paletteCreator).className = 'color'; //cria divs como filhas da div mãe
-    }
+  const colors = ['black', 'blue', 'green', 'yellow'];
+  const paletteSelect = document.getElementById('color-palette');
+  for (let index = 0; index < colors.length; index += 1) {
+    const paletteCreator = document.createElement('div');
+    paletteCreator.style.background = colors[index];
+    paletteSelect.appendChild(paletteCreator).className = 'color';
+  }
 }
 paletteBox();
 
 function accessElement(tagId, elementType, position) {
-    let access = document.querySelectorAll(tagId);
-    let create = document.createElement(elementType);
-    let print = access[position].appendChild(create);
-    return print;
+  const access = document.querySelectorAll(tagId);
+  const create = document.createElement(elementType);
+  const print = access[position].appendChild(create);
+  return print;
 }
 
 for (let index = 1; index < 6; index += 1) {
-    accessElement('#pixel-board', 'div', 0).classList.add('tr');
+  accessElement('#pixel-board', 'div', 0).classList.add('tr');
 }
 for (let index = 0; index < 5; index += 1) {
-    for (let index2 = 0; index2 < 5; index2 += 1) {
-        accessElement('.tr', 'div', index).classList.add('td', 'pixel');
-    }
+  for (let index2 = 0; index2 < 5; index2 += 1) {
+    accessElement('.tr', 'div', index).classList.add('td', 'pixel');
+  }
 }
 
-let initialColor = document.querySelector('div').classList.add('selected', 'color');
-let colorsSelect = document.getElementsByClassName('color');
+const initialColor = document.querySelector('div').classList.add('selected', 'color');
+const colorsSelect = document.getElementsByClassName('color');
 
 function selectColor(eventoDeOrigem) {
-    let elementSelected = document.getElementsByClassName('selected');
-    elementSelected[0].classList.remove('selected');
-    eventoDeOrigem.target.classList.add('selected');
+  const elementSelected = document.getElementsByClassName('selected');
+  elementSelected[0].classList.remove('selected');
+  eventoDeOrigem.target.classList.add('selected');
 }
 
 for (let indexColors = 0; indexColors < colorsSelect.length; indexColors += 1) {
-    colorsSelect[indexColors].addEventListener('click', selectColor);
+  colorsSelect[indexColors].addEventListener('click', selectColor);
 }
-
 
 function paintColor(toPaint) {
-    let colorUsing = document.getElementsByClassName('selected');
-    let saveSpecifcStyle = window.getComputedStyle(colorUsing[0]);//*
-    let backgroundColor = saveSpecifcStyle.getPropertyValue('background');//*
-    toPaint.target.style.background = backgroundColor;
-    //FONTE: https://stackoverflow.com/questions/6338217/get-a-css-value-with-javascript
+  const colorUsing = document.getElementsByClassName('selected');
+  const saveSpecifcStyle = window.getComputedStyle(colorUsing[0]);// *
+  const backgroundColor = saveSpecifcStyle.getPropertyValue('background');// *
+  toPaint.target.style.background = backgroundColor;
+  // FONTE: https://stackoverflow.com/questions/6338217/get-a-css-value-with-javascript
 }
 
-let boxToPaint = document.getElementsByClassName('pixel');
+const boxToPaint = document.getElementsByClassName('pixel');
 for (let index8 = 0; index8 < boxToPaint.length; index8 += 1) {
-    boxToPaint[index8].addEventListener('click', paintColor);
+  boxToPaint[index8].addEventListener('click', paintColor);
 }
 
-let pixels = document.getElementsByClassName('pixel');
+const pixels = document.getElementsByClassName('pixel');
 function paintOfWhite() {
-    for (let index9 = 0; index9 < pixels.length; index9 += 1) {
-        pixels[index9].style.background = 'white';
-    }
+  for (let index9 = 0; index9 < pixels.length; index9 += 1) {
+    pixels[index9].style.background = 'white';
+  }
 }
-let restarButton = document.getElementById('restart');
+const restarButton = document.getElementById('restart');
 restarButton.addEventListener('click', paintOfWhite);
