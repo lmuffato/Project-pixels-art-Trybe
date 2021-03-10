2ab60cf56f762897cea976e5ea2d094a3c0a6777
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-03-10 09:31:29 -0300

    Requisito 4 - Criado a funcao que cria as linhas e colunas

---
+++ script.js
@@ -0,0 +1,16 @@
+function createPixBox(size) {
+    const getPixelBoard = document.querySelector('#pixel-board');
+
+    for (let index = 0; index < size; index += 1) {
+        const pixelLinha = document.createElement('tr');
+        pixelLinha.className = 'linha';
+
+        for (let index = 0; index < size; index += 1) {
+            const pixelColuna = document.createElement('th');
+            pixelColuna.className = 'pixel';
+            pixelLinha.appendChild(pixelColuna);
+            getPixelBoard.appendChild(pixelLinha);
+        }
+    }
+}
+createPixBox(5);
