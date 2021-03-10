d1ad236d6d6979fd14ce9fc45a5e2a3ac988a2a1
Marcus C. H. Filho <mchusemann@gmail.com>
2021-03-09 23:46:22 -0300

    :rocket: feat(Requirement-8): Paints the pixel with the color selected in the palette

--- script.js
+++ script.js
@@ -5,7 +5,8 @@
 // - Note que os elementos que deverão receber a classe `selected` devem ser os mesmos elementos que possuem a classe `color`, como especificado no **requisito 2**.
 
 const colorPalettes = document.getElementsByClassName('color');
-
+const pixels = document.getElementsByClassName('pixel');
+let selectedPalette = 'black';
 function removeSelected(value) {
   for (let index = 0; index < colorPalettes.length; index += 1) {
     if (value !== colorPalettes[index]) {
@@ -14,11 +15,20 @@ function removeSelected(value) {
   }
 }
 
-function clickedElement() {
+function clickedPaletteElement() {
   this.classList.add('selected');
+  selectedPalette = window.getComputedStyle(this).getPropertyValue('background-color');
   removeSelected(this);
 }
 
-for (let i = 0; i < colorPalettes.length; i += 1) {
-  colorPalettes[i].addEventListener('click', clickedElement, true);
+for (let index = 0; index < colorPalettes.length; index += 1) {
+  colorPalettes[index].addEventListener('click', clickedPaletteElement);
+}
+
+function clickedPixelElement(event) {
+  event.target.style.setProperty('background-color', selectedPalette);
+}
+
+for (let index = 0; index < pixels.length; index += 1) {
+  pixels[index].addEventListener('click', clickedPixelElement);
 }
