f520221851874b1412385920a70bc9f33f5f9b9d
Vinicios Fraga <soicinivt@gmail.com>
2021-03-10 16:10:49 -0300

    Correçoes e requisitos 6 e 7.

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ setbkColor();
 
 function board() {
   const line = document.querySelectorAll('.line');
-  console.log(line);
   for (let index = 0; index < line.length; index += 1) {
     for (let indexToo = 0; indexToo < line.length; indexToo += 1) {
       const pixels = document.createElement('div');
@@ -23,3 +22,21 @@ function board() {
 }
 
 board();
+
+const begin = document.querySelector('#black');
+begin.classList.add('selected');
+
+const pBoard = document.querySelector('#pixel-board');
+pBoard.addEventListener('click', function toBoard(element) {
+  const selectedColor = document.querySelector('.selected').style.backgroundColor;
+  const selectedP = element.target;
+  selectedP.style.backgroundColor = selectedColor;
+});
+
+const colorPalette = document.querySelector('#color-palette');
+colorPalette.addEventListener('click', function selectedInPalette(element) {
+  const newSelected = element.target;
+  const inSelected = document.querySelector('.selected');
+  inSelected.classList.remove('selected');
+  newSelected.classList.add('selected');
+});
