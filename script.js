2fdea55760dbb380fdc8b359540d1b58b9e78fe8
Marcelo Maurício <mmauricio.design@gmail.com>
2021-03-10 09:16:52 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 window.onload = function() {
   document.getElementById('black').classList.add('selected');
 };
+
+function cleanColor() {
+  document.querySelector('.selected').classList.remove('selected');
+}
+
+function selectColorPaint(event) {
+  const currentColor = event.target;
+  cleanColor();
+
+  currentColor.classList.add('selected');
+}
+
+document.getElementById('color-palette').addEventListener('click', selectColorPaint);
