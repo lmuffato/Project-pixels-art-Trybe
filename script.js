c520415f81f318445883b244fa9f7710f59cca80
lucas-paz <lucas.hpaz@outlook.com>
2021-03-10 17:48:12 -0300

    Adiciona a função 8

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Função para criar as divs da paleta de cores;
 function paletteBox() {
-    let colors = ['black', 'blue', 'green', 'yellow', 'red', 'cyan', 'brown']; //Cores da paleta
+    let colors = ['black', 'blue', 'green', 'yellow']; //Cores da paleta
     let paletteSelect = document.getElementById('color-palette');//acessa a div mãe
     for (let index = 0; index < colors.length; index += 1) {
         let paletteCreator = document.createElement('div'); //cria uma div
@@ -29,13 +29,25 @@ for (let index = 0; index < 5; index += 1) {
 let initialColor = document.querySelector('div').classList.add('selected', 'color');
 let colorsSelect = document.getElementsByClassName('color');
 
-function selectColor (eventoDeOrigem) {
+function selectColor(eventoDeOrigem) {
     let elementSelected = document.getElementsByClassName('selected');
-        elementSelected[0].classList.remove('selected');
+    elementSelected[0].classList.remove('selected');
     eventoDeOrigem.target.classList.add('selected');
 }
 
-for (let indexColors =0; indexColors < colorsSelect.length; indexColors += 1) {
+for (let indexColors = 0; indexColors < colorsSelect.length; indexColors += 1) {
     colorsSelect[indexColors].addEventListener('click', selectColor);
 }
 
+
+function paintColor(toPaint) {
+    let colorUsing = document.getElementsByClassName('selected');
+    let saveSpecifcStyle = window.getComputedStyle(colorUsing[0]);
+    let backgroundColor = saveSpecifcStyle.getPropertyValue('background');
+    toPaint.target.style.background = backgroundColor;
+}
+
+let boxToPaint = document.getElementsByClassName('pixel');
+for (let index8 = 0; index8 < boxToPaint.length; index8 += 1) {
+    boxToPaint[index8].addEventListener('click', paintColor);
+}
