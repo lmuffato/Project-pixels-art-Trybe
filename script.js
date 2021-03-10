9bbb726cfe01aa3cb04a7a589f74e83ffcc18b22
Priscila <prihscapin@gmail.com>
2021-03-10 16:01:45 -0300

    Requisitos 4 &  5

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-// reqisito 1
+// reqisito 9
+let takeColor = document.getElementByClassName('color');
+click.addEventListener('click', takeColor)
+
+let color = document.getElementsByClassName('pixel');
+click.addEventListener('click', color)
+
+function (paint) {
+    if 
+
+
+}
 
 
 
