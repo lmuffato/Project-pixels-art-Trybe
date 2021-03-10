ddfe1ef1813dcedff9649287e4b2223f242b066b
Paulo Vitor Borges <pvtrybe209@gmail.com>
2021-03-10 18:15:22 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
-function renameSelected(selectedElement) {
-  const color = document.getElementsByName('color');
+function renameSelected() {
+  const color = document.getElementsByClassName('selected');
   for (let index = 0; index < color.length; index += 1) {
-    if (selectedElement !== color[index]) {
-      color[index].classList.remove('selected');
-    }
+    color[index].classList.remove('selected');
   }
 }
 function greenColor() {
   const greenColorElement = document.getElementById('verde');
-  greenColorElement.addEventListener('click', () => {
+  greenColorElement.addEventListener('click', () => { 
     greenColorElement.className += ' selected';
-    renameSelected(greenColorElement);
+    renameSelected();
   });
 }
 function blueColor() {
   const blueColorElement = document.getElementById('azul');
   blueColorElement.addEventListener('click', () => {
+    renameSelected();
     blueColorElement.className += ' selected';
-    renameSelected(blueColorElement);
   });
 }
 function yellowColor() {
   const yellowColorElement = document.getElementById('amarelo');
   yellowColorElement.addEventListener('click', () => {
+    renameSelected();
     yellowColorElement.className += ' selected';
-    renameSelected(yellowColorElement);
   });
 }
 function blackColor() {
   const blackColorElement = document.getElementById('preto');
   blackColorElement.addEventListener('click', () => {
+    renameSelected();
     blackColorElement.className += ' selected';
-    renameSelected(blackColorElement);
   });
 }
 window.onload = function onload() {
