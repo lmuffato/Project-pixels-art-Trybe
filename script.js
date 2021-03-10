63b068e6d3518362c88582c6021be9cea8a60cd4
Paulo H. Lopes <phlopes93@gmail.com>
2021-03-10 11:27:32 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-
-
-let colorFirst = document.querySelectorAll('div')[1]
+let colorFirst = document.querySelectorAll('div')[1];
 colorFirst.style.backgroundColor = 'red';
 
-let colorSecond = document.querySelectorAll('div')[2]
+let colorSecond = document.querySelectorAll('div')[2];
 colorSecond.style.backgroundColor = 'green';
 
-let colorThird = document.querySelectorAll('div')[3]
+let colorThird = document.querySelectorAll('div')[3];
 colorThird.style.backgroundColor = 'blue';
 
-let colorFourth = document.querySelectorAll('div')[4]
+let colorFourth = document.querySelectorAll('div')[4];
 colorFourth.style.backgroundColor = 'yellow';
 
 colorFirst.style.backgroundColor = 'black';
@@ -17,15 +15,24 @@ colorFirst.style.backgroundColor = 'black';
 let framePixel = document.getElementById('pixel-board');
 
 for (i = 1; i <= 25; i +=1 ) {
-   let pixel = document.createElement('div')
-   pixel.className = 'pixel'
+   let pixel = document.createElement('div');
+   pixel.className = 'pixel';
 
-   framePixel.appendChild(pixel)
+   framePixel.appendChild(pixel);
 }
 
-colorFirst.className = 'color selected';
+let classColor = document.querySelectorAll('.color');
+function clean() {
+    for (let i = 0; i < classColor.length; i += 1) {
+    classColor[i].classList.remove('selected') 
+   
+   }      
+}
 
+for (let i = 0; i < classColor.length; i += 1) {
+    classColor[i].addEventListener('click',(clique)=>{ 
+    clean()
+    clique.target.classList.add('selected')
 
-
-
-
+    })
+}
