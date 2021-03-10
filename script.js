81ed837fa05ad53205c67b4bb96473a5592aa10d
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-03-10 12:04:04 -0300

    requisito 9

--- script.js
+++ script.js
@@ -28,7 +28,7 @@
     for (let index = 0; index < paletaCor.length; index += 1) {
         paletaCor[index].addEventListener('click', function(event){
             //paletaCor[index] = event.target;?
-            let paletaCorSelecionada = event.target;
+            let paletaCorSelecionada = event.target; // como o André Jaques me explicou, aqui estou dizendo que o alvo é o alvo do evento em questão.
             SelecionadoAntes.classList.remove = 'selected';
             paletaCorSelecionada.classList.add = 'selected';
         });
@@ -44,7 +44,7 @@
     for (let index = 0; index < pixelArray.length; index += 1) {
         pixelArray[index].addEventListener('click', function(event){
             //pixelArray[index] = event.target;?
-            let selecionaPixel = event.target; // como o André Jaques me disse, aqui estou dizendo que o alvo é o alvo do evento em questão.
+            let selecionaPixel = event.target; // como o André Jaques me explicou, aqui estou dizendo que o alvo é o alvo do evento em questão.
             let CorSelecionada = document.querySelector('.selected').style.backgroundColor; //aqui estou "pegando" o background-color da div que está com a classe selected
             selecionaPixel.style.backgroundColor = CorSelecionada; 
         });
@@ -56,10 +56,14 @@ colocaCor ()
 function resetaCor (){
 
     let reseta = document.querySelector('#resetar');
+    let pixel = document.querySelectorAll('.pixel');
+
 
     reseta.addEventListener('click', function() {
-        document.querySelectorAll('.pixel').style.backgroundColor = 'white';
+        for (let index = 0; index < pixel.length; index += 1) {    
+        pixel[index].style.backgroundColor = 'white';    
+        }
     });
- } 
+}
 
  resetaCor ()
