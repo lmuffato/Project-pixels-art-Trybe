e69fe72f53d3248a51d16a63f54e8b8511d5fac7
Guilherme Dugaich de Oliveira <guilherme.dugaich@gmail.com>
2021-03-10 13:11:57 -0300

    Requisito 10 - utilizando o botao VQV para gerar um novo board com input do usuario

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-//Declarando variáveis para os elementos que estão no HTML
+// Declarando variáveis para os elementos que estão no HTML
 const colorPalette = document.getElementById('color-palette');
 const pixelBoard = document.getElementById('pixel-board');
+const VQVBtn = document.getElementById('generate-board');
 const clearBtn = document.getElementById('clear-board');
 let colors = ['black', 'green', 'blue', 'red'];
 
 function createColorPalette() {
-  for (color of colors) {
-    let newColorInPalette = document.createElement('div');
+  for (let color of colors) {
+    const newColorInPalette = document.createElement('div');
     newColorInPalette.className = 'color';
     newColorInPalette.id = color;
     newColorInPalette.style.backgroundColor = color;
@@ -40,11 +41,18 @@ function clearPixels() {
   }
 }
 
+function paintPixel(event) {
+  // Pinta o pixel clicado com a cor atualmente selcionada
+  const selectedColor = document.querySelector('.selected').style.backgroundColor;
+  const selectedPixel = event.target;
+  selectedPixel.style.backgroundColor = selectedColor;
+}
+
 // Adicionando event handler no botao Limpar
 clearBtn.addEventListener('click', clearPixels);
 
 function createPixelBoard(n) {
-  // Criando os pixels
+  // Cria o pixel board com tamanho nxn
   for (let boardRow = 1; boardRow <= n; boardRow += 1) {
     const newRow = document.createElement('div');
     newRow.className = 'pixel-row';
@@ -57,23 +65,29 @@ function createPixelBoard(n) {
       newPixel.style.height = '40px';
       newPixel.style.border = '1px solid black';
       newPixel.style.display = 'inline-block';
+      // Adicionando event listener no Pixel
+      newPixel.addEventListener('click', paintPixel);
       newRow.appendChild(newPixel);
     }
   }
 }
 
+function deletePixelBoard() {
+  // Exclui o pixel board por inteiro
+  pixelBoard.innerHTML = '';
+}
+
+function regeneratePixelBoard() {
+  // Cria o pixel board novamente com input do usuário
+  const userInput = document.getElementById('board-size').value;
+  if (userInput === '') {
+    alert('Board Inválido!');
+  } else {
+    deletePixelBoard();
+    createPixelBoard(userInput);
+  }
+}
+
+VQVBtn.addEventListener('click', regeneratePixelBoard);
+
 createPixelBoard(5);
-
-function addColorToPixel(event) {
-  // Pinta o pixel clicado com a cor atualmente selcionada
-  let selectedColor = document.querySelector('.selected').style.backgroundColor;
-  // selectedColor = window.getComputedStyle(selectedColor).getPropertyValue('background-color');
-  const selectedPixel = event.target;
-  selectedPixel.style.backgroundColor = selectedColor;
-}
-
-// Adicionando event handler nos pixels
-const allPixels = document.querySelectorAll('.pixel');
-for (const pixel of allPixels) {
-  pixel.addEventListener('click', addColorToPixel);
-}
