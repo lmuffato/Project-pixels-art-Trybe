121bb44b33e420e8a8f9e4fb8b0412788023bce7
Cla Oliveira <clarice.coliveira@yahoo.com.br>
2021-03-11 16:26:09 -0300

    Requisito 10 - Define tamanho do quadro de pixels

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ const colorOne = document.getElementsByClassName('color-one')[0];
 const colorTwo = document.getElementsByClassName('color-two')[0];
 const colorThree = document.getElementsByClassName('color-three')[0];
 const colorFour = document.getElementsByClassName('color-four')[0];
-const pixel = document.getElementsByClassName('pixel');
+let pixel = document.getElementsByClassName('pixel');
 const btnLimpar = document.getElementById('clear-board');
+const pixelBoard = document.getElementById('pixel-board');
+const btnVQV = document.getElementById('generate-board');
+const inputBoardSize = document.getElementById('board-size');
 
 colorOne.addEventListener('click', () => {
   colorOne.classList.add('selected');
@@ -36,18 +39,45 @@ colorFour.addEventListener('click', () => {
   colorFour.classList.add('selected');
 });
 
-for (let index = 0; index < pixel.length; index += 1) {
-  pixel[index].addEventListener('click', (event) => {
-    const paletteSelectedColor = document.getElementsByClassName('selected')[0];
-    const backgroundColor = window.getComputedStyle(paletteSelectedColor, null)
-      .getPropertyValue('background-color');
-    const pixelColor = event.target;
-    pixelColor.style.backgroundColor = backgroundColor;
-  });
+function setPixels() {
+  for (let index = 0; index < pixel.length; index += 1) {
+    pixel[index].addEventListener('click', (event) => {
+      const paletteSelectedColor = document.getElementsByClassName('selected')[0];
+      const backgroundColor = window.getComputedStyle(paletteSelectedColor, null)
+        .getPropertyValue('background-color');
+      const pixelColor = event.target;
+      pixelColor.style.backgroundColor = backgroundColor;
+    });
+  }
+  pixelBoard.style.width = `${Number(inputBoardSize.value * 40 + 50).toString()}px`;
 }
 
+setPixels();
+
 btnLimpar.addEventListener('click', () => {
+  pixel = document.getElementsByClassName('pixel');
   for (let index = 0; index < pixel.length; index += 1) {
     pixel[index].style.backgroundColor = 'white';
+  }
+});
+
+btnVQV.addEventListener('click', () => {
+  if (Number(inputBoardSize.value) === 0) {
+    alert('Board inválido!');
+  } else {
+    pixelBoard.innerHTML = '';
+    const boardSize = Number(inputBoardSize.value);
+    for (let index = 0; index < boardSize; index += 1) {
+      const tableRow = document.createElement('div');
+      tableRow.classList.add('tr');
+      pixelBoard.appendChild(tableRow);
+      for (let indexCell = 0; indexCell < boardSize; indexCell += 1) {
+        const pixelElement = document.createElement('div');
+        pixelElement.classList.add('pixel');
+        tableRow.appendChild(pixelElement);
+      }
+    }
+    pixel = document.getElementsByClassName('pixel');
+    setPixels();
   }
 });
