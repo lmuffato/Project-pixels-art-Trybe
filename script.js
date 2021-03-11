5ada91b7f9c52f40c37bfca96657977274dc4ebd
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-03-11 14:50:28 -0300

    requisito 8, teste

--- script.js
+++ script.js
@@ -1,33 +1,25 @@
 function selectedColor() {
-    const selected1 = document.querySelector('#color-palette');
+  const selected1 = document.querySelector('#color-palette');
 
-    selected1.addEventListener('click',function(event) {
-        limpaSelected();
-        event.target.classList.add('selected') 
-    })
+  selected1.addEventListener('click' , function x (event) {
+  limpaSelected();
+  event.target.classList.add('selected') 
+})
 }
 
 function limpaSelected() {
-    const colorSelected = document.querySelector('.selected');
-    colorSelected.classList.remove('selected');
+const colorSelected = document.querySelector('.selected');
+colorSelected.classList.remove('selected');
 }
 
 selectedColor();
 
 
 function box25 (){
-    const boxGrande = document.querySelector('#pixel-board');
-    boxGrande.addEventListener('click',function(event){
-        event.target.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
-    })
-    }
-
-box25();
-
-function salveColor () {
-    let corDeFundo = document.querySelector('.selected').style.backgroundColor;
-    corDeFundo.addEventListener('click',function(){
-        corDeFundo.style.backgroundColor.remove;
-    } )
+const boxGrande = document.querySelector('#pixel-board');
+boxGrande.addEventListener('click' , function xx (event){
+    event.target.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
+})
 }
-salveColor();
+
+box25 ();
