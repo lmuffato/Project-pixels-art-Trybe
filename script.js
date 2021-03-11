5d2201a525834a443c3cd4420db1f3f6a5ed3bee
carlos25xrm <carlos25xrm@gmail.com>
2021-03-11 18:19:12 -0300

    Refazendo(2) o Requisito 7

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
+let corAtual = '';
+
 function selecionaPreto() {
-    document.getElementById('cor-preto').classList.add('selected');
-    document.getElementById('cor-laranja').classList.remove('selected');
-    document.getElementById('cor-azul').classList.remove('selected');
-    document.getElementById('cor-marrom').classList.remove('selected');
+  document.getElementById('cor-preto').classList.add('selected');
+  document.getElementById('cor-laranja').classList.remove('selected');
+  document.getElementById('cor-azul').classList.remove('selected');
+  document.getElementById('cor-marrom').classList.remove('selected');
+  corAtual = 'black';
 }
 
 function selecionaLaranja() {
-    document.getElementById('cor-laranja').classList.add('selected');
-    document.getElementById('cor-preto').classList.remove('selected');
-    document.getElementById('cor-azul').classList.remove('selected');
-    document.getElementById('cor-marrom').classList.remove('selected');
+  document.getElementById('cor-laranja').classList.add('selected');
+  document.getElementById('cor-preto').classList.remove('selected');
+  document.getElementById('cor-azul').classList.remove('selected');
+  document.getElementById('cor-marrom').classList.remove('selected');
+  corAtual = 'orange';
 }
 
 function selecionaAzul() {
-    document.getElementById('cor-azul').classList.add('selected');
-    document.getElementById('cor-preto').classList.remove('selected');
-    document.getElementById('cor-laranja').classList.remove('selected');
-    document.getElementById('cor-marrom').classList.remove('selected');
+  document.getElementById('cor-azul').classList.add('selected');
+  document.getElementById('cor-preto').classList.remove('selected');
+  document.getElementById('cor-laranja').classList.remove('selected');
+  document.getElementById('cor-marrom').classList.remove('selected');
+  corAtual = 'blue';
 }
 
 function selecionaMarrom() {
-    document.getElementById('cor-marrom').classList.add('selected');
-    document.getElementById('cor-preto').classList.remove('selected');
-    document.getElementById('cor-laranja').classList.remove('selected');
-    document.getElementById('cor-azul').classList.remove('selected');
+  document.getElementById('cor-marrom').classList.add('selected');
+  document.getElementById('cor-preto').classList.remove('selected');
+  document.getElementById('cor-laranja').classList.remove('selected');
+  document.getElementById('cor-azul').classList.remove('selected');
+  corAtual = 'rgb(102, 66, 40)';
 }
