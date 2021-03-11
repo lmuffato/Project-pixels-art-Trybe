34b56b7afef14c47938ea7332fbe45b2e0cd1f92
PollyanaOliveira <pollyana.deoliveir@gmail.com>
2021-03-11 19:36:04 -0400

    requisito 8

--- script.js
+++ script.js
@@ -55,11 +55,31 @@ function addSelect() {
       getSelect[index].classList.remove('selected');
     }
     this.classList.add('selected'); // ou event.target.classList.add('selected);
-  }  
+  }
+}
+
+// REQUISITO 8 - Pintar o pixel com a cor selecionada //
+
+function pixelSelected() {
+  const colorPixel = document.querySelectorAll('.pixel');
+  for (let index = 0; index < colorPixel.length; index += 1) {
+    colorPixel[index].addEventListener('click', printPixel)
+    }
+  function printPixel() {
+    const lookPixel = document.querySelectorAll('td');
+    for (let index2 = 0; index2 < lookPixel.length; index2 += 1) {
+      if (lookPixel[index2].className === 'color selected') {
+        for (let index3 = 0; index3 < colorPixel.length; index3 += 1) {
+          this.style.backgroundColor = lookPixel[index2].style.backgroundColor;
+        }
+      }
+    }
+  }
 }
 
 window.onload = function () {
   tableColorPalette();
   createTable();
   addSelect();
+  pixelSelected();
 };
