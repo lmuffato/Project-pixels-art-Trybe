53b55abae6d9f92454fffa8cfe1e533c004509cb
Wendell Costa <wendellcosta9141@gmail.com>
2021-03-11 16:26:38 -0300

    [Esqueci de linkar o CSS :(]

---
+++ script.js
@@ -0,0 +1,5 @@
+// Define a cor de background das paletas
+document.querySelector('.black').style.backgroundColor = 'black';
+document.querySelector('.blue').style.backgroundColor = 'blue';
+document.querySelector('.red').style.backgroundColor = 'green';
+document.querySelector('.yellow').style.backgroundColor = 'red';
