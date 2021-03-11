348ee4eed0876e4e8600ea2697e4970a69bf272a
Renata Andrade <reapminas@gmail.com>
2021-03-11 17:58:18 -0300

    Requisito 3 - Adicione a cor preta como a primeira cor da paleta de cores

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Acessando cada coluna de uma tabela: https://www.w3schools.com/jsref/prop_html_style.asp
 var columPalete = document.getElementsByTagName("td")[0];
-columPalete.style.backgroundColor = "blue";
+columPalete.style.backgroundColor = "black";
 
 var columPalete = document.getElementsByTagName("td")[1];
-columPalete.style.backgroundColor = "red";
+columPalete.style.backgroundColor = "blue";
 
 var columPalete = document.getElementsByTagName("td")[2];
 columPalete.style.backgroundColor = "green";
