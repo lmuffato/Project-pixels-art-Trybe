3d8e10cac7f34d089d268985a55d006e6bad216d
camiloLelis <lafirmafuerte@gmail.com>
2021-03-11 17:20:08 -0300

    projeto-project-pixels-ar- requisito 7 -fiz com olhada em codigo de outros alunos que postaram no slack - nao fixeio os conceitos de forma satisfatoria

--- script.js
+++ script.js
@@ -12,4 +12,25 @@
     
 
 
+/* const mudaCor= document.querySelector('.selected');
+function mudarACOr(e){
+    mudaCor.className = 'color';
+
+}
+
+//mudaCor.addeventListener('click', mudaCor);
+mudaCor.addEventListener('click',mudarACOr);
+ */
+
+let color = document.getElementsByClassName('color');
+
+
+color = addEventListener('click',select);
+
+function select(event){
+let colorr = document.getElementsByClassName('selected');
+colorr[0].classList.remove('selected')
+let alvo= event.target;
+alvo.classList.add('selected');
+}
 
