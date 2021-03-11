b8d90e547f18d029ea157a4d8587ecb2237da0ea
camiloLelis <lafirmafuerte@gmail.com>
2021-03-11 18:11:46 -0300

    projeto-project-pixels-ar- requisito 7 -tentativa2 -fiz com olhada em codigo de outros alunos que postaram no slack - nao fixeio os conceitos de forma satisfatoria

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 
-  /*   const alvo = document.getElementById('color1');
-    
-    function capturar(){
-        alvo.style.backgroundColor= 'red';
-        
-        console.log('testesssssss');
-    }
-    
-   alvo.addEventListener('click', capturar);
-         */
-    
+/*   const alvo = document.getElementById('color1');
+  
+  function capturar(){
+      alvo.style.backgroundColor= 'red';
+      
+      console.log('testesssssss');
+  }
+  
+ alvo.addEventListener('click', capturar);
+       */
+
 
 
 /* const mudaCor= document.querySelector('.selected');
@@ -22,15 +22,23 @@ function mudarACOr(e){
 mudaCor.addEventListener('click',mudarACOr);
  */
 
-let color = document.getElementsByClassName('color');
+// fiz com olhada em codigo de outros alunos que postaram no slack
+let color = document.getElementsByClassName('color'); 
+color = addEventListener('click', select);
 
-
-color = addEventListener('click',select);
-
-function select(event){
-let colorr = document.getElementsByClassName('selected');
-colorr[0].classList.remove('selected')
-let alvo= event.target;
-alvo.classList.add('selected');
+function select(event) {
+  let colorr = document.getElementsByClassName('selected');
+  colorr[0].classList.remove('selected')
+  let alvo = event.target;
+  alvo.classList.add('selected');
 }
+
+function pintar(e){
+  e.target.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;  
+  
+}  
+
+
+let colorir = document.querySelector('#pixel-board');
+colorir.addEventListener('click', pintar);
 
