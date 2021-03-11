2c2d5f5ceab7cba26a10ebcd14ac24ec7e064816
rcalvs <rafaelcalvette@gmail.com>
2021-03-11 17:14:15 -0300

    ja que nao vai 40, vamos ver se vai 36

--- script.js
+++ script.js
@@ -1,33 +1,30 @@
-//color[length] para alterar a cor nas paletas
+// color[length] para alterar a cor nas paletas
 
 window.onload = function bemVindo() {
-  console.log("teste testando");
+  console.log('teste testando');
+};
 
-}
 //lets //
-let Palette = document.getElementById('color-palette');
-let Color = document.querySelectorAll('.color');
-let Board = document.getElementById('pixel-board');
-let Clear = document.getElementById('clear-board');
-// let palleteColorRandom = ['black', randomColor(), randomColor(), randomColor()];
-let palleteColorBasic = ['black', 'red', 'green', 'blue'];
-
+const Palette = document.getElementById('color-palette');
+const Color = document.querySelectorAll('.color');
+const Board = document.getElementById('pixel-board');
+const Clear = document.getElementById('clear-board');
+const palleteColorBasic = ['black', 'red', 'green', 'blue'];
+const palleteColorRandom = ['black', randomColor(), randomColor(), randomColor()];
 
 // Gerador de Cores Automaticas by Viniccius13 //
 // https://dev.to/akhil_001/generating-random-color-with-single-line-of-js-code-fhj //
 
 function randomColor() {
-  var randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+  const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
   return randomColor;
-}
-
-
+};
 
 // Coloca as cores nas Divs //
-window.onload = function palleteColor(){
-  for (let index = 0; index < palleteColorBasic.length; index += 1)
-  Color[index].style.backgroundColor = palleteColorBasic[index];
-}
+window.onload = function palleteColor() {
+  for (let index = 0; index < palleteColorRandom.length; index += 1){
+    Color[index].style.backgroundColor = palleteColorRandom[index];
+};
 
 // Seleciona cor na palette //
 function setColor(event) {
@@ -38,14 +35,14 @@ function setColor(event) {
       elementTarget.classList.add('selected');
     });
   }
-}
+};
 
 // Salva cor //
 function getColor() {
   const elementSelected = document.querySelector('.selected');
   const color = window.getComputedStyle(elementSelected).getPropertyValue('background-color');
   return color;
-}
+};
 
 // Pinta pixel //
 function changePixels(event) {
@@ -53,7 +50,7 @@ function changePixels(event) {
   if (elementTarget.id !== 'pixel-board') {
     elementTarget.style.backgroundColor = getColor();
   }
-}
+};
 
 // Botão clear //
 // Teve um menino que bolou essa solução sem precisar de For nem nada,
@@ -61,14 +58,10 @@ function changePixels(event) {
 // me avise para creditá-lo da forma correta!
 function clearBoard() {
   window.location.reload();
-}
-
+};
 
 // Clicks //
 Palette.addEventListener('click', setColor);
 Board.addEventListener('click', changePixels);
 Clear.addEventListener('click', clearBoard);
-
-
-
-
+}
