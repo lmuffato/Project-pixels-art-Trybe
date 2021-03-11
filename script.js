8d2f08845c37ec1909f29d6bb202fc721de4f431
Gabriel Ferreira <glucas709@gmail.com>
2021-03-11 15:09:16 -0300

    stylelint

--- script.js
+++ script.js
@@ -61,8 +61,7 @@ let pixels = document.querySelectorAll('.pixel');
 function setColorPixel() {
   pixels.forEach((pixel, index) => {
     pixel.addEventListener('click', () => {
-      pixels[index].style.backgroundColor =
-        colorSelected[0].style.backgroundColor;
+      pixels[index].style.backgroundColor = colorSelected[0].style.backgroundColor;
     });
   });
 }
