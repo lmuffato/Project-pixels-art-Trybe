f24be03a15eee0584bd45c6a0658fd49591a755f
Juan Pablo <juan.tentiomaru9309@mail.com>
2021-03-11 17:14:53 -0300

    Requisito 7 correção 3

--- .history/script_20210311171337.js
+++ .history/script_20210311171337.js
@@ -3,7 +3,7 @@ let colorPalette = document.getElementById('color-palette');
 
 function corTroca() {
   for (let corIndex = 0; corIndex < color.length; corIndex += 1) {
-    color[corIndex].addEventListener('click', function () {
+    cor[corIndex].addEventListener('click', function () {
       colorPalette.querySelector('.selected').classList.remove('selected');
       this.classList.add('selected');
     });
