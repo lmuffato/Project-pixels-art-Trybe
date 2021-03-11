bb17ccaadf4f7f91d822530e2e7290d2cd0c504c
guilhermemd <guilherme.md@gmail.com>
2021-03-10 21:19:10 -0300

    requisito 04

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ const yellowBox = document.getElementsByClassName('color')[2];
 yellowBox.style.backgroundColor = 'yellow';
 const purpleBox = document.getElementsByClassName('color')[3];
 purpleBox.style.backgroundColor = 'purple';
+
+const tableID = document.getElementById('pixel-board');
+
+
+function TableMaker (){
+  for (let index = 0; index < 5; index += 1){
+    let trOfTable = document.createElement('tr');
+    tableID.appendChild(trOfTable)
+    for (let index = 0; index < 5; index += 1){
+      let pixelOnTable = document.createElement('th')
+      pixelOnTable.className = 'pixel'
+      pixelOnTable.style.backgroundColor = 'white'
+    }
+  }
+}
+
+TableMaker();
