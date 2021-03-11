9d0685ec0a66fbd4108bb5bafa43b48e4fa86006
Southier <patrick.southier@hotmail.com>
2021-03-11 01:24:34 -0300

    Incrementa códigos referentes aos requisitos 4 e 5. Escrevi a tabela toda no HTML pois enfrentei dificuldade para fazê-la pelo JS, pra adiantar o projeto preferi commitar dessa maneira pelo menos por agora.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-window.onload = function() {
+window.onload = function () {
   function setPalette(color1, color2, color3) {
-    let getPalette = document.getElementsByClassName('color');
+    const getPalette = document.getElementsByClassName('color');
     getPalette[0].style.backgroundColor = 'black';
     getPalette[1].style.backgroundColor = color1;
     getPalette[2].style.backgroundColor = color2;
@@ -8,4 +8,17 @@ window.onload = function() {
   }
 
   setPalette('red', 'green', 'blue');
-}
+
+  // function createCanvas() {
+  //   const getRow = document.getElementsByTagName('tr');
+  //   for (let index = 0; index < getRow.length; index += 1) {
+  //     let createColumn = document.createElement('td');
+  //     createColumn.className = 'pixel';
+  //     for (let columnIndex = 0; columnIndex <= getRow.length; columnIndex += 1) {
+  //       getRow[index].append(createColumn);
+  //     }
+  //   }
+  // }
+
+  // createCanvas();
+};
