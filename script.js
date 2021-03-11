22c6230ea4e1724e1c31990fd694a075e1a1a1c5
Eric <ericff07@gmail.com>
2021-03-11 16:05:46 -0300

    Corrije o problema no for e finaliza os passos 8 e 9

--- script.js
+++ script.js
@@ -18,12 +18,22 @@ for (let index = 0; index < colors.length; index += 1) {
 }
 
 // Clicar em um pixel o pinta com a cor selecionada
-let currentPixel = document.querySelectorAll('.pixel')
-for (let index2 = 0; index2 <= currentPixel.length; index2 += 1) {
+let currentPixel = document.getElementsByClassName('pixel')
+for (let index2 = 0; index2 < currentPixel.length; index2 += 1) {
     currentPixel[index2].addEventListener('click', function(e) {
     let element = document.querySelector('.selected')
-    selectedColor = window.getComputedStyle(element).getPropertyValue('background-color');
+    let selectedColor = window.getComputedStyle(element).getPropertyValue('background-color');
     //currentPixel[index2].innerHTML = selectedColor;
     e.target.style.backgroundColor = selectedColor;
 })
 }
+
+// Clicar no botão limpa todos os pixels
+let clear = document.querySelector('#clear-board');
+let resetPixel = document.querySelectorAll('.pixel')
+clear.addEventListener('click', function () {
+    for (let index3 = 0; index3 < resetPixel.length; index3 += 1) {
+        resetPixel[index3].style.backgroundColor = 'white'
+        resetPixel[index3].classList.add()
+    }
+})
