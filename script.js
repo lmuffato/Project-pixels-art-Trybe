bee6502c82a04dd696d172dc57d9cab54f783283
Ana Ventura <anaventura1811@gmail.com>
2021-03-11 18:19:21 -0300

    Muda as cores no stylesheet

--- script.js
+++ script.js
@@ -1,27 +1,46 @@
-// função para mudar o selected de lugar
+function colorizePalette() {
+  const colorOne = document.getElementById('black');
+  colorOne.style.backgroundColor = 'black';
+}
+colorizePalette();
 
-// uma função para servir de add event listener
-// uma função usando event.target
-// uma função para o botão modificar o tamanho do quadro de pixels
-// const clearPixelBoard = document.querySelector('.btn');
-// função para armazenar a cor selecionada
+// Teste do Requisito 8
+const colorSelected = document.querySelector('.selected');
 
-// função que 'armazena' a cor clicada
+function paintPixelBoard(e) {
+  e.target.style.backgroundColor = colorSelected;
+  console.log('picked color');
+}
+const paintBoard = document.getElementById('pixel-board');
+paintBoard.addEventListener('click', paintPixelBoard);
 
-// gera as cores aleatórias
+// Requisito 9
 
+// Requisito 12 - Parte 1 - gera as cores aleatórias --> target.addEventListener(event, function, useCapture)
 function randomColors() {
   const r = Math.random() * 255;
   const g = Math.random() * 255;
   const b = Math.random() * 255;
   return `rgb(${r}, ${g}, ${b})`;
 }
+// Referência usada para a função randomColors:
+// https://wallacemaxters.com.br/blog/2021/02/20/como-gerar-cores-aleatorias-no-javascript
 
-// distribuição de cores aleatórias
+// Requisito 12 - Parte 2 - distribuição de cores aleatórias
 function distributeRandomColors() {
   document.getElementById('green').style.backgroundColor = randomColors();
   document.getElementById('magenta').style.backgroundColor = randomColors();
   document.getElementById('blue').style.backgroundColor = randomColors();
 }
+// função que apaga o board para criar um novo
+function removeBoard() {
+  document.getElementById('board').remove();
+}
+const btn = document.getElementById('generate-board');
+btn.addEventListener('click', removeBoard);
 
-window.onload = distributeRandomColors;
+// função que cria novo board
+
+window.onload = function initialize() {
+  distributeRandomColors();
+};
