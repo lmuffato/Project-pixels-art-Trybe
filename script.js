5b8daa120b98a0706d5b544f48ed2629e9924bf2
Renata Andrade <reapminas@gmail.com>
2021-03-11 17:56:00 -0300

    Requisito 2 - Adicione à página uma paleta de quatro cores distintas

---
+++ script.js
@@ -0,0 +1,12 @@
+// Acessando cada coluna de uma tabela: https://www.w3schools.com/jsref/prop_html_style.asp
+var columPalete = document.getElementsByTagName("td")[0];
+columPalete.style.backgroundColor = "blue";
+
+var columPalete = document.getElementsByTagName("td")[1];
+columPalete.style.backgroundColor = "red";
+
+var columPalete = document.getElementsByTagName("td")[2];
+columPalete.style.backgroundColor = "green";
+
+var columPalete = document.getElementsByTagName("td")[3];
+columPalete.style.backgroundColor = "pink";
