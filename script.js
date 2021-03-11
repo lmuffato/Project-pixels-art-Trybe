19de6b2121eec8fc8df4952cbc05683154791849
ThalitaC <thalitaceci@gmail.com>
2021-03-11 16:24:40 -0300

    refatora pixel-board, corrige requisito 8

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
-//Requisito 7
+// Requisito 7 //desenvolvido com ajuda dos colegas Patrick Morais, Nilson Ribeiro e Rafael Cardoso
 function clearSelected() {
-  let colorSelected = document.querySelector('.selected');
+  const colorSelected = document.querySelector('.selected');
   colorSelected.classList.remove('selected');
-} 
-
-function selectColor() {
-  const changeColor = document.querySelector('#color-palette')
-  
-  changeColor.addEventListener('click', function(event) {
-    clearSelected();
-    event.target.classList.add('selected')
-  })
 }
 
-//Requisito 8
-function paintColor() {
-  const paintBox = document.querySelector('#pixel-board')
+function selectColor() {
+  const changeColor = document.querySelector('#color-palette');
   
-  paintBox.addEventListener('click', function(event) {
+  changeColor.addEventListener('click', function(event) {
+    clearSelected ();
+    event.target.classList.add('selected')
+  });
+};
+
+// Requisito 8 //desenvolvido com ajuda do colega Lucas Pedroso
+function paintColor() {
+  const paintBox = document.getElementById('pixel-board');
+  
+  paintBox.addEventListener('click', function (event) {
     event.target.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
-  })
+    const a = document.querySelector('.selected');
+    console.log(a.style.backgroundColor);
+  });
 }
 
 selectColor();
 paintColor();
-
