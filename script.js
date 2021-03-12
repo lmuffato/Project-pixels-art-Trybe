11406d1d87875fcf678aaf62dd37c65caef39619
Tiago Santos <tiago881@gmail.com>
2021-03-11 23:16:09 -0300

    7 - Clicar em uma das cores da paleta faz com que ela seja selecionada e utilizada para preencher os pixels no quadro.

--- script.js
+++ script.js
@@ -8,4 +8,18 @@ function quadrado() {
   }
 }
 
-window.addEventListener('load', quadrado);
+function paletteColor() {
+  const colorPalette = document.querySelectorAll('.color');
+  for (let index = 0; index < colorPalette.length; index += 1) {
+    colorPalette[index].addEventListener('click', (e) => {
+      for (let indice = 0; indice < colorPalette.length; indice += 1) {
+        colorPalette[indice].classList.remove('selected');
+      }
+      e.target.classList.add('selected');
+    });
+  }
+}
+
+paletteColor();
+
+window.addEventListener('load', quadrado, paletteColor);
