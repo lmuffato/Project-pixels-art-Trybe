25d1568c8f70bd3363ac587a3eeecef9c2acd63d
Ana Ventura <anaventura1811@gmail.com>
2021-03-11 23:56:23 -0300

    Tentativa resolução do requisito 7

--- script.js
+++ script.js
@@ -42,5 +42,64 @@ function clearPixelColor() {
 const clearBtn = document.getElementById('clear-board');
 clearBtn.addEventListener('click', clearPixelColor, false);
 
+const colorBlack = document.getElementsByClassName('color')[0];
+const colorTwo = document.getElementsByClassName('color')[1];
+const colorThree = document.getElementsByClassName('color')[2];
+const colorFour = document.getElementsByClassName('color')[3];
+
+function colorTwoSelected() {
+  if (colorTwo.classList.contains('selected')) {
+    colorBlack.classList.remove('selected');
+    colorThree.classList.remove('selected');
+    colorFour.classList.remove('selected');
+  }
+}
+function colorThreeSelected() {
+  if (colorThree.classList.contains('selected')) {
+    colorBlack.classList.remove('selected');
+    colorTwo.classList.remove('selected');
+    colorFour.classList.remove('selected');
+  }
+}
+
+function colorFourSelected() {
+  if (colorFour.classList.contains('selected')) {
+    colorBlack.classList.remove('selected');
+    colorTwo.classList.remove('selected');
+    colorThree.classList.remove('selected');
+  }
+}
+
+function colorBlackSelected() {
+  if (colorBlack.classList.contains('selected')) {
+    colorTwo.classList.remove('selected');
+    colorThree.classList.remove('selected');
+    colorFour.classList.remove('selected');
+  }
+}
+
+function colorTwoSelection() {
+  colorTwo.classList.add('selected');
+  colorTwoSelected();
+}
+colorTwo.addEventListener('click', colorTwoSelection);
+
+function colorThreeSelection() {
+  colorThree.classList.add('selected');
+  colorThreeSelected();
+}
+colorThree.addEventListener('click', colorThreeSelection);
+
+function colorFourSelection() {
+  colorFour.classList.add('selected');
+  colorFourSelected();
+}
+colorFour.addEventListener('click', colorFourSelection);
+
+function colorBlackSelection() {
+  colorBlack.classList.add('selected');
+  colorBlackSelected();
+}
+colorBlack.addEventListener('click', colorBlackSelection);
 // Referência usada para a função randomColors:
 // https://wallacemaxters.com.br/blog/2021/02/20/como-gerar-cores-aleatorias-no-javascript
