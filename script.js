6cf0cf151e96aff3ca5e23aeff94d77eff3d2498
diegoCorreia <diegomethal@gmail.com>
2021-03-11 21:05:25 -0300

    requisito 2

---
+++ script.js
@@ -0,0 +1,10 @@
+function panelColors(){
+for (let index = 0; index < colorList.lenght; index += 1){
+    colorList = document.getQuerySelectorAll('.color');
+    colorList[0].style.backgroundColor = 'blue';
+    colorList[1].style.backgroundColor = 'yellow';
+    colorList[2].style.backgroundColor = 'red';
+    colorList[3].style.backgroundColor = 'green';
+}
+}
+panelColors();
