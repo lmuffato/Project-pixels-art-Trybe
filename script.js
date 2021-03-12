52e146e5dd349f38c1edda81e902f6d42d8ec590
diegoCorreia <diegomethal@gmail.com>
2021-03-11 21:16:46 -0300

    requisito 2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function panelColors(){
+function panelColors(colorList){
 for (let index = 0; index < colorList.lenght; index += 1){
-    colorList = document.getQuerySelectorAll('.color');
+    colorList[index] = document.getQuerySelectorAll('.color');
     colorList[0].style.backgroundColor = 'blue';
     colorList[1].style.backgroundColor = 'yellow';
     colorList[2].style.backgroundColor = 'red';
