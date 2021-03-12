81793b92d8a8277485b0848962103112068574ad
Southier <patrick.southier@hotmail.com>
2021-03-12 02:07:31 -0300

    Increment codes from requirements 6 and 7.

--- script.js
+++ script.js
@@ -1,27 +1,58 @@
-window.onload = function () {
-  function setPalette(color1, color2, color3) {
-    const getPalette = document.getElementsByClassName('color');
-    getPalette[0].style.backgroundColor = 'black';
-    getPalette[1].style.backgroundColor = color1;
-    getPalette[2].style.backgroundColor = color2;
-    getPalette[3].style.backgroundColor = color3;
-  }
+function setPalette(color1, color2, color3) {
+  const getPalette = document.getElementsByClassName('color');
+  getPalette[0].style.backgroundColor = 'black';
+  getPalette[0].className = 'color selected'; 
+  getPalette[1].style.backgroundColor = color1;
+  getPalette[2].style.backgroundColor = color2;
+  getPalette[3].style.backgroundColor = color3;
+}
 
-  setPalette('red', 'green', 'blue');
+setPalette('red', 'green', 'blue');
 
-  function createCanvas(tableSize) {
-    const getTable = document.getElementById('pixel-board');
-    
-    for (let index = 0; index < tableSize; index += 1) {
-      let createRow = document.createElement('tr');
-      getTable.appendChild(createRow);
-      for (let columnIndex = 0; columnIndex < tableSize; columnIndex += 1) {
-        let createColumn = document.createElement('td');
-        createRow.appendChild(createColumn);
-        createColumn.className = 'pixel';
-      }
+function createCanvas(tableSize) {
+  const getTable = document.getElementById('pixel-board');
+  for (let index = 0; index < tableSize; index += 1) {
+    const createRow = document.createElement('tr');
+    getTable.appendChild(createRow);
+    for (let columnIndex = 0; columnIndex < tableSize; columnIndex += 1) {
+      const createColumn = document.createElement('td');
+      createRow.appendChild(createColumn);
+      createColumn.className = 'pixel';
     }
   }
+}
 
-  createCanvas(5);
-};
+createCanvas(5);
+
+//Código desta função foi feito usando a função feita pelo Patrick Morais como referência(https://github.com/tryber/sd-010-a-project-pixels-art/pull/12/files).
+function listenColorSelection() {
+  const getPalette = document.getElementById('color-palette');
+  getPalette.addEventListener('click', function(event) {
+    const getColors = document.getElementsByClassName('color');
+    for (let index = 0; index < getColors.length; index += 1) {
+      getColors[index].classList.remove('selected');
+      getPalette.classList.remove('selected');
+    }
+    event.target.classList.add('selected');
+  });
+}
+
+listenColorSelection();
+
+// function addTaskColorToDay() {
+//   let getDiv = document.querySelector('.my-tasks').lastChild;
+//   let selectedCheck = 'task selected';
+//   let taskColor = getDiv.style.backgroundColor;
+//   let originalDayColor = 'rgb(119,119,119)';
+//   let getDay = document.querySelectorAll('.day');
+  
+//   for (let index = 0; index < getDay.length; index += 1) {
+//     getDay[index].addEventListener('click', function() {
+//       if (getDay[index].style.color === taskColor && getDiv.className === selectedCheck) {
+//         getDay[index].style.color = originalDayColor;
+//       } else if (getDiv.className === selectedCheck) {
+//         getDay[index].style.color = taskColor;
+//       }
+//     })
+//   }
+// }
