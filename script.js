e4a2db7c1c954bcaa5025c1e111f2fe96c65e6a8
johnpligeiro <johnpligeiro@gmail.com>
2021-03-13 18:13:07 -0300

    Requisito 7.

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 window.onload = function() {
+  document.querySelector('#color-palette').addEventListener('click', classChange);
+  function classChange(event) {
+    const paletteColors = document.querySelectorAll('.color');
+    for (let index = 0; index < paletteColors.length; index += 1) {
+      paletteColors[index].classList.remove('selected');
+    }
+    event.target.classList.add('selected');
+  }
 };
