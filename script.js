62bbbdf950f40af59153ae6cb874d8a6bc2d9eb7
Maria Lisboa <marialisboa1515@gmail.com>
2021-03-13 18:54:58 -0300

    desafio 4 e 5

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+//verifica se ao abrir a página a cor inicial dos quadros deve ser branca.
+function corInicial (){
+    for (index = 0; index<=25; index += 1) {
+        let corWhite = corInicial[index = 0].style.backgroundColor ;
+    }
+    return corInicial;
+}
+
 // Definindo a cor preta como cor inicial
 
+
+
 // Botão que limpa as cores do quadro
-function clearBoard () {
-  for (index=0; index<=25; index+=1){
 
-  }
 
-}
