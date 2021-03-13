c6639cb4072542578e4d80519bf297b2680c84c4
diegoCorreia <diegomethal@gmail.com>
2021-03-13 16:00:53 -0300

    correçao requisito 4 e requisito 5

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function panelColors() {
   const colorList = document.querySelectorAll('.color');
-  colorList[0].style.backgroundColor = 'black';
+  colorList[0].style.backgroundColor = 'blue';
   colorList[1].style.backgroundColor = 'yellow';
   colorList[2].style.backgroundColor = 'red';
   colorList[3].style.backgroundColor = 'green';
