6f5434cfa3760bc3dd26209f9d07cb070da1b51b
diegoCorreia <diegomethal@gmail.com>
2021-03-13 15:52:05 -0300

    requisito 4

--- script.js
+++ script.js
@@ -7,16 +7,16 @@ function panelColors() {
 }
 panelColors();
 
-let tableBoard = document.querySelector('#pixel-board');
+const tableBoard = document.querySelector('#pixel-board');
   function pixelBoard(){
     for (let index = 0; index < 5; index +=1){
-      let tableRow = document.createElement('tr');
+      const tableRow = document.createElement('tr');
       tableBoard.appendChild('tableRow');
-        for (let index = 0; index < 5; index +=1){
-          let tableData = document.createElement('td');
+        for (index = 0; index < 5; index +=1){
+          const tableData = document.createElement('td');
           tableRow.appendChild(tableData);
           tableData.classList.add('pixel');
     }
   }  
 }
-tableBoard();
+pixelBoard();
