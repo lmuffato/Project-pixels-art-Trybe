53244c033e86d90038650ca5d5f862372df2acfd
Nathalia Zebral <natzebral@gmail.com>
2021-03-13 12:37:15 -0300

    alteracoes de acordo com o eslint

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 const paletteColors = document.getElementById('color-palette');
 
 function changeClassColors(event) {
- const colorsDiv = document.getElementsByClassName('color');
- for (let index = 0; index < colorsDiv.length; index += 1) {
- colorsDiv[index].classList.remove('selected');
- }
- event.target.classList.add('selected');
+  const colorsDiv = document.getElementsByClassName('color');
+  for (let index = 0; index < colorsDiv.length; index += 1) {
+    colorsDiv[index].classList.remove('selected');
+  }
+  event.target.classList.add('selected') = newColorSelected;
 }
 
-paletteColors.addEventListener("click", changeClassColors);
+paletteColors.addEventListener('click', changeClassColors);
 
 const pixelBoard = document.getElementById('pixel-board');
 function ChangeBackgroundColor(event) {
- document.getElementsByClassName('pixel');
- const selectedElement = document.getElementsByClassName('selected')[0];
- const colorToUse = selectedElement.style.backgroundColor;
- event.target.style.backgroundColor = colorToUse;
+  document.getElementsByClassName('pixel');
+  const selectedElement = document.getElementsByClassName('selected')[0];
+  const colorToUse = selectedElement.style.backgroundColor;
+  event.target.style.backgroundColor = colorToUse;
 }
 
 pixelBoard.addEventListener('click', ChangeBackgroundColor);
@@ -23,9 +23,9 @@ pixelBoard.addEventListener('click', ChangeBackgroundColor);
 const button = document.querySelector('#clear-board');
 
 function clearAll() {
- const pixels = document.querySelectorAll('td');
- for (let index = 0; index < pixels.length; index += 1) {
- pixels[index].style.backgroundColor = 'white';
- }
+  const pixels = document.querySelectorAll('td');
+  for (let index = 0; index < pixels.length; index += 1) {
+    pixels[index].style.backgroundColor = 'white';
+  }
 }
 button.addEventListener('click', clearAll);
