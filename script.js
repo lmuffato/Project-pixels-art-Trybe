226f7e7a4ea345e4e8864f1d767c9b8a8598c3f4
Caio Oliveira <parafrenteeadiante@gmail.com>
2021-03-12 21:13:34 -0300

    corrigindo identacao

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let dimension = 5;
 
-//Cria o tabuleiro dinamicamente. Em board recupera-se o elemento que representa o tabuleiro,
-//o primeiro for cria uma div que representa uma linha, atribui a classe nela e aplica a div criada em board. 
-//Em seguida o outro for recupera a div recém criada e cria outras divs dentro dela que representarao os pixels.
-//Assim o ciclo se repete: Cria uma div linha e dentro dela cria as divs pixels de acordo com o valor dimensao
+// Cria o tabuleiro dinamicamente. Em board recupera-se o elemento que representa o tabuleiro,
+// o primeiro for cria uma div que representa uma linha, atribui a classe nela e aplica a div criada em board. 
+// Em seguida o outro for recupera a div recém criada e cria outras divs dentro dela que representarao os pixels.
+// Assim o ciclo se repete: Cria uma div linha e dentro dela cria as divs pixels de acordo com o valor dimensao
 function makeBoard(dim) {
   const board = document.getElementById('pixel-board');
   let createdLine;
@@ -23,7 +23,7 @@ function makeBoard(dim) {
 }
 
 //  Função que é ativada quando se clica numa cor da paleta, ela recupera o elemento que contem
-//a classe selected, remove essa classe dele e aplica no elemento que ativou o listener
+// a classe selected, remove essa classe dele e aplica no elemento que ativou o listener
 function changeSelected(eventOrigin) {
   const selectedNow = document.querySelector('.selected');
   selectedNow.classList.remove('selected');
@@ -40,15 +40,15 @@ function addPaletteListener() {
   }
 }
 
-//Esta funcao recupera a propriedade backgroundColor do elemento da paleta com a classe selected,
-//e entao recupera o elemento pixel que foi clicado e atribui a ele esta propriedade.
+// Esta funcao recupera a propriedade backgroundColor do elemento da paleta com a classe selected,
+// e entao recupera o elemento pixel que foi clicado e atribui a ele esta propriedade.
 function applyColor(eventOrigin) {
   const selectedColor = document.querySelector('.selected').style.backgroundColor;
   const clickedPixel = eventOrigin.target;
   clickedPixel.style.backgroundColor = selectedColor;
 }
 
-//Funcao que adiciona um escutador em cada elemento pixel
+// Funcao que adiciona um escutador em cada elemento pixel
 function addPixelListener() {
   const pixels = document.getElementsByClassName('pixel');
   for (let index = 0; index < pixels.length; index += 1) {
@@ -56,7 +56,7 @@ function addPixelListener() {
   }
 }
 
-//Funcao que 'limpa' o tabuleiro atribuindo a propriedade backgroundColor o valor 'white'
+// Funcao que 'limpa' o tabuleiro atribuindo a propriedade backgroundColor o valor 'white'
 function cleanBoard() {
   const pixels = document.getElementsByClassName('pixel');
   for (let index = 0; index < pixels.length; index += 1) {
@@ -64,19 +64,19 @@ function cleanBoard() {
   }
 }
 
-//Adiciona um escutador no botão limpar
+// Adiciona um escutador no botão limpar
 function addButtomListener() {
   const buttom = document.getElementById('clear-board');
   buttom.addEventListener('click', cleanBoard);
 }
 
 function changeDim() {
-  dimension = document.FormDimension.dim.value; //recupera o valor do input que vai de 5 a 50
-  if (dimension == ''){
-    alert("Board inválido!");
+  dimension = document.FormDimension.dim.value; // Recupera o valor do input que vai de 5 a 50
+  if (dimension === '') {
+    alert('Board inválido!');
   }
   else {
-    document.getElementById('pixel-board').innerHTML = ''; //apaga o conteudo do tabuleiro atual
+    document.getElementById('pixel-board').innerHTML = ''; // Apaga o conteudo do tabuleiro atual
     makeBoard(dimension);
   }
 }
