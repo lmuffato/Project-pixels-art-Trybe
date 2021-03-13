12585489a140b0a54fc28ef85e67d29c6b4832a0
walvesb65 <willian.alvesb65@gmail.com>
2021-03-13 16:08:22 -0300

    requisito 6

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-function paleta(){
-  for (let index = 1; index <= 4; index += 1){
-  const tagDiv = document.createElement('div');
-  const tagPai = document.querySelector('#color-palette');
+function paleta() {
+  for (let index = 1; index <= 4; index += 1) {
+    const tagDiv = document.createElement('div');
+    const tagPai = document.querySelector('#color-palette');
     tagPai.appendChild(tagDiv);
     tagDiv.classList.add('color')
   }
 }
 paleta();
 
-function coresDaTabela(){
+function coresDaTabela() {
   const cores = document.getElementsByClassName('color');
   cores[0].style.backgroundColor = 'black'
   cores[1].style.backgroundColor = 'blue'
@@ -17,12 +17,12 @@ function coresDaTabela(){
 }
 coresDaTabela();
 
-function quadroDePixels(){
-  for(let index = 1 ; index <= 5; index += 1){
+function quadroDePixels() {
+  for (let index = 1; index <= 5; index += 1) {
     const pixelsPai = document.createElement('div');
     const tagPai = document.getElementById('pixel-board');
     tagPai.appendChild(pixelsPai);
-    for(let index = 1 ; index <= 5; index += 1){
+    for (let index = 1; index <= 5; index += 1) {
       const pixels = document.createElement('div');
       pixelsPai.appendChild(pixels);
       pixels.classList.add('pixel')
@@ -30,3 +30,9 @@ function quadroDePixels(){
   }
 }
 quadroDePixels();
+
+function corBlack(){
+  const corBlack1 = document.getElementsByClassName('color')[0].classList.add('selected');
+}
+corBlack();
+
