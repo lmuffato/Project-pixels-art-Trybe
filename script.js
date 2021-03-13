3db2c5aa9fd584e0574361d123b0e8fba7f3ba9c
Nathalia Zebral <natzebral@gmail.com>
2021-03-13 12:01:09 -0300

    requisito 9 feito JS

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const paletteColors = document.getElementById('color-palette');
 
-function changeClassColors(event){
-	const colorsDiv = document.getElementsByClassName('color');
-	for (let index = 0; index < colorsDiv.length; index +=1 ) {
+function changeClassColors(event) {
+ const colorsDiv = document.getElementsByClassName('color');
+	for(let index = 0; index < colorsDiv.length; index += 1 ) {
 		colorsDiv[index].classList.remove('selected');
 	}
 	const colorClass = event.target.classList.add('selected');
@@ -10,9 +10,8 @@ function changeClassColors(event){
 	paletteColors.addEventListener("click", changeClassColors);
 
 
+
 const pixelBoard = document.getElementById('pixel-board');
-
-
 function ChangeBackgroundColor(event){
 	const pixel = document.getElementsByClassName('pixel');
 	const selectedElement = document.getElementsByClassName('selected')[0];
@@ -21,3 +20,13 @@ function ChangeBackgroundColor(event){
 }
 
 pixelBoard.addEventListener("click", ChangeBackgroundColor);
+
+const button = document.querySelector('#clear-board');
+
+function clearAll() {
+ const pixels = document.querySelectorAll('td');
+ for(let index = 0; index < pixels.length; index += 1) {
+	pixels[index].style.backgroundColor = "white";
+}
+}
+button.addEventListener("click", clearAll);
