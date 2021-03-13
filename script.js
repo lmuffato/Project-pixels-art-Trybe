f1bed4d23c410268b04720202a24a13fffb8042d
Luiz <luizfvaccarif@gmail.com>
2021-03-13 10:19:14 +0000

    Requirement 4

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const colorPaletteSection = document.createElement('section');
 colorPaletteSection.id = 'color-palette' 
 document.body.appendChild(colorPaletteSection);
 
-for (let index = 1; index <= 4; index++) {
+for (let index = 1; index <= 4; index +=1) {
     const colorPaletteDivs = document.createElement('div');
     colorPaletteDivs.className = ('color');
     colorPaletteDivs.style.display = 'inline-block';
@@ -14,7 +14,7 @@ for (let index = 1; index <= 4; index++) {
     colorPaletteDivs.style.height = '40px'
     colorPaletteSection.appendChild(colorPaletteDivs);
     if (index === 1) {
-        colorPaletteDivs.style.backgroundColor = 'black'
+        colorPaletteDivs.style.backgroundColor = 'black' // Requirement 3
     } if (index === 2) {
         colorPaletteDivs.style.backgroundColor = 'red' 
     } if (index === 3) {
@@ -23,3 +23,28 @@ for (let index = 1; index <= 4; index++) {
         colorPaletteDivs.style.backgroundColor = 'orange'
     }
   }
+  // Requirement 4 
+  const createTable = document.createElement('table');
+  document.body.appendChild(createTable);
+  createTable.id = 'pixel-board';
+
+for (let index = 1; index <= 25; index += 1) {
+  const divSquare = document.createElement('div');
+  divSquare.style.display = 'inline-block';
+  divSquare.style.width = '40px';
+  divSquare.style.height = '40px';
+  divSquare.style.border = '1px solid black';
+  divSquare.style.marginTop = '0px';
+  divSquare.className = 'pixel';
+  divSquare.style.background = 'white';
+  createTable.appendChild(divSquare);
+  if (index === 5) {
+    divSquare.style.marginRight = '800px'
+  } if (index === 10) {
+    divSquare.style.marginRight = '800px'
+  } if (index === 15) {
+    divSquare.style.marginRight = '800px'
+  } if (index === 20) {
+    divSquare.style.marginRight = '800px'
+  }
+}
