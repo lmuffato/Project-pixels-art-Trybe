1171889fa87b7bfbe55ec1c18a361a086389faa5
walvesb65 <willian.alvesb65@gmail.com>
2021-03-13 19:27:43 -0300

    ajustnado requisito 8

--- script.js
+++ script.js
@@ -51,16 +51,6 @@ function click() {
 }
 click();
 
-/* 8 - Clicar em um pixel dentro do quadro após selecionar uma cor na paleta 
-faz com que o pixel seja preenchido com a cor selecionada. 
-
-O que será verificado:
-
-Verifica se ao carregar a página deve ser possível pintar os pixels de preto
-Verifica se após selecionar uma outra cor na paleta, é possível pintar os pixels com essa cor
-Verifica se somente o pixel que foi clicado foi preenchido com a cor selecionada, sem influenciar 
-na cor dos demais pixels. */
-
 function pint() {
   const selectedColor = document.querySelector('#pixel-board') /* recupere o pai onde vai acontecer o evento  */
   selectedColor.addEventListener('click', (e) => { /* diz que o meu evento vai ser um click */
