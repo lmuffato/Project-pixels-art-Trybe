1bff349190089f25c1e417a33c68255c413a9389
walvesb65 <willian.alvesb65@gmail.com>
2021-03-13 15:49:37 -0300

    requisito 3 e 4

--- script.js
+++ script.js
@@ -10,9 +10,24 @@ paleta();
 
 function coresDaTabela(){
   const cores = document.getElementsByClassName('color');
-  cores[0].style.backgroundColor = 'red'
+  cores[0].style.backgroundColor = 'black'
   cores[1].style.backgroundColor = 'blue'
   cores[2].style.backgroundColor = 'pink'
   cores[3].style.backgroundColor = 'green'
 }
 coresDaTabela();
+
+function quadroDePixels(){
+  for(let index = 1 ; index <= 5; index += 1){
+    const pixelsPai = document.createElement('div');
+    const tagPai = document.getElementById('pixel-board');
+    tagPai.appendChild(pixelsPai);
+    for(let index = 1 ; index <= 5; index += 1){
+      const pixels = document.createElement('div');
+      pixelsPai.appendChild(pixels);
+      pixels.classList.add('pixel')
+    }
+  }
+}
+
+quadroDePixels();
