b9121d71613bcc0ce1eca4248c5c2b2a72c3de9d
Debora Passos <deborinhapassos@gmail.com>
2021-03-13 17:32:19 -0300

    requisito 8 a 10

--- script.js
+++ script.js
@@ -1,24 +1,46 @@
 //Requisito 7
 const colorClick = document.getElementById('color-palette');
 
-  function changeColor(event) {
-    const colorClass = document.getElementsByClassName('color');
-    console.log(colorClass[0].classList);
-    for (index = 0; index < colorClass.length; index += 1) {
-      colorClass[index].classList.remove('selected');
+  function changeClass(event) {
+
+    const nameClass = document.getElementsByClassName('color');
+    for (index = 0; index < nameClass.length; index += 1) {
+      nameClass[index].classList.remove('selected');
     }
     //console.log(event);
+
     const addClass = event.target.classList.add('selected');
   }
-  colorClick.addEventListener('click', changeColor);
+  
+  colorClick.addEventListener('click', changeClass);
+
+// requisito 8
+  const change = document.getElementById('pixel-board');
+  
+  function changeColor (event) {
+    const nameClass = document.getElementsByClassName('color');
+
+    for (index = 0; index < nameClass.length; index += 1) {
+
+      if (nameClass[index].classList.contains('selected')) {
+        var obj = window.getComputedStyle(nameClass[index])
+        event.target.style.backgroundColor = obj.backgroundColor;
+      }
+    }
+  }
+
+  change.addEventListener('click', changeColor);
 
 
 // requisito 10 
 const clearPixel = document.getElementById('clear-board')
-const pixelWhite = document.querySelectorAll(".pixel");
+const pixelWhite = document.getElementsByClassName('pixel');
 
 function clear () {
-  pixelWhite.style.backgroudColor = white;
+  for (index = 0; index < pixelWhite.length; index += 1){
+    pixelWhite[index].style.backgroundColor = "white";
+  }
 }
+
 clearPixel.addEventListener('click', clear);
 
