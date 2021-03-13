9196f9cc235525e4b174d46fcf9359778e502e29
guilhermemd <guilherme.md@gmail.com>
2021-03-12 22:59:04 -0300

    concertando requisito 06

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function boxColor() {
   for (let colorIndex = 0; colorIndex < colorsArr.length; colorIndex += 1) {
     selectColorClass[colorIndex].style.backgroundColor = colorsArr[colorIndex];
   }
-  selectColorClass[0].className = ' selected';
+  selectColorClass[0].className += ' selected';
 };
 
 function creatingPixels() {
@@ -35,6 +35,7 @@ function creatingPixels() {
     }
   }
 };
+
 
 // Chamando todas as funções no final para ficar mais clean.
 window.onload = function() {
