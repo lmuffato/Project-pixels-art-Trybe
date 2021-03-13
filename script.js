1bf5f7ba4b11b7863be8370a97b67a7eecba667c
Mateus Alencar <mtauws@gmail.com>
2021-03-13 15:55:44 -0300

    arrow functions

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /* eslint func-names: ["error", "never"] */
 
-function jqueryHover() {
+const jqueryHover = () => {
   $('.pixel').hover(function () {
     $(this).css('width', '43px');
     $(this).css('height', '43px');
@@ -8,9 +8,20 @@ function jqueryHover() {
     $(this).css('width', '40px');
     $(this).css('height', '40px');
   });
-}
+};
+
+const stylePixel = (pixel) => {
+  pixel.className = 'pixel';
+  pixel.style.width = '40px';
+  pixel.style.height = '40px';
+  pixel.style.backgroundColor = 'white';
+  pixel.style.display = 'inline-block';
+  pixel.style.border = 'solid black 1px';
+  pixel.style.transition = '0.2s';
+};
+
 /* Source: https://github.com/tryber/sd-010-a-project-pixels-art/pull/4/files */
-function createPixels(n) {
+const createPixels = (n) => {
   for (let index = 0; index < n; index += 1) {
     const createDiv = document.createElement('div');
     createDiv.className = 'line';
@@ -18,68 +29,62 @@ function createPixels(n) {
     document.querySelector('#pixel-board').appendChild(createDiv);
     for (let jindex = 0; jindex < n; jindex += 1) {
       const pixel = document.createElement('div');
-      pixel.className = 'pixel';
-      pixel.style.width = '40px';
-      pixel.style.height = '40px';
-      pixel.style.backgroundColor = 'white';
-      pixel.style.display = 'inline-block';
-      pixel.style.border = 'solid black 1px';
-      pixel.style.transition = '0.2s';
+      stylePixel(pixel);
       jqueryHover();
       document.querySelectorAll('.line')[index].appendChild(pixel);
     }
   }
-}
-createPixels(5);
+};
 
-function firstColorSelected() {
+const firstColorSelected = () => {
   const color = document.querySelector('.color');
   color.classList.add('selected');
-}
+};
 
 /**
  * Deu uma olhada no código do luanlsr e achei bem legal a forma como ele fez para remover a classe selected
  * resolvi implementa-lá no meu código. Github: https://github.com/tryber/sd-010-a-project-pixels-art/pull/15/files
  */
+// Desafio 7
 const elementOne = document.getElementsByClassName('color')[0];
 const elementTwo = document.getElementsByClassName('color')[1];
 const elementThree = document.getElementsByClassName('color')[2];
 const elementFour = document.getElementsByClassName('color')[3];
 
-function clickElementOne() {
+const clickElementOne = () => {
   elementOne.classList.add('selected');
   elementTwo.classList.remove('selected');
   elementThree.classList.remove('selected');
   elementFour.classList.remove('selected');
-}
+};
 
-function clickElementTwo() {
+const clickElementTwo = () => {
   elementOne.classList.remove('selected');
   elementTwo.classList.add('selected');
   elementThree.classList.remove('selected');
   elementFour.classList.remove('selected');
-}
+};
 
-function clickElementThree() {
+const clickElementThree = () => {
   elementOne.classList.remove('selected');
   elementTwo.classList.remove('selected');
   elementThree.classList.add('selected');
   elementFour.classList.remove('selected');
-}
+};
 
-function clickElementFour() {
+const clickElementFour = () => {
   elementOne.classList.remove('selected');
   elementTwo.classList.remove('selected');
   elementThree.classList.remove('selected');
   elementFour.classList.add('selected');
-}
+};
 
-function selectColor() {
+const selectColor = () => {
   elementOne.addEventListener('click', clickElementOne);
   elementTwo.addEventListener('click', clickElementTwo);
   elementThree.addEventListener('click', clickElementThree);
   elementFour.addEventListener('click', clickElementFour);
-}
+};
 
 // Desafio 8
 const colorOne = 'black';
@@ -87,7 +92,7 @@ const colorTwo = 'red';
 const colorThree = 'blue';
 const colorFour = 'green';
 
-function palletClicked() {
+const palletClicked = () => {
   if (elementOne.classList.contains('selected')) {
     return colorOne;
   }
@@ -103,9 +108,9 @@ function palletClicked() {
   if (elementFour.classList.contains('selected')) {
     return colorFour;
   }
-}
+};
 
-function paintPixel() {
+const paintPixel = () => {
   const pixel = document.querySelectorAll('.pixel');
 
   for (let index = 0; index < pixel.length; index += 1) {
@@ -113,10 +118,10 @@ function paintPixel() {
       pixel[index].style.backgroundColor = palletClicked();
     });
   }
-}
+};
 
 // Desafio 9
-function clearBoard() {
+const clearBoard = () => {
   const clearButton = document.querySelector('#clear-board');
   const pixelBoard = document.querySelectorAll('.pixel');
 
@@ -125,8 +130,9 @@ function clearBoard() {
       pixelBoard[index].style.backgroundColor = 'white';
     });
   }
-}
+};
 
+createPixels(5);
 window.onload = () => {
   paintPixel();
   firstColorSelected();
