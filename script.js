4fab078d980046caaecd965169bcfc2426cd324f
walvesb65 <willian.alvesb65@gmail.com>
2021-03-13 18:30:32 -0300

    ajustando requsito 7

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ function paleta() {
     const tagDiv = document.createElement('div');
     const tagPai = document.querySelector('#color-palette');
     tagPai.appendChild(tagDiv);
-    tagDiv.classList.add('color')
+    tagDiv.classList.add('color');
   }
 }
 paleta();
 
 function coresDaTabela() {
   const cores = document.getElementsByClassName('color');
-  cores[0].style.backgroundColor = 'black'
-  cores[1].style.backgroundColor = 'blue'
-  cores[2].style.backgroundColor = 'pink'
-  cores[3].style.backgroundColor = 'green'
+  cores[0].style.backgroundColor = 'black';
+  cores[1].style.backgroundColor = 'blue';
+  cores[2].style.backgroundColor = 'pink';
+  cores[3].style.backgroundColor = 'green';
 }
 coresDaTabela();
 
@@ -25,29 +25,31 @@ function quadroDePixels() {
     for (let index = 1; index <= 5; index += 1) {
       const pixels = document.createElement('div');
       pixelsPai.appendChild(pixels);
-      pixels.classList.add('pixel')
+      pixels.classList.add('pixel');
     }
   }
 }
 quadroDePixels();
 
-function corBlack(){
+function corBlack() {
   const corBlack1 = document.getElementsByClassName('color')[0].classList.add('selected');
 }
 corBlack();
 /* Neste requisito recebi ajuda do amigo Lucas Godoi turma 10 tribo A, mas obtive total entendimento da questão */
-function click(){
-  const recuperar = document.getElementById('color-palette') /* Estou pegando minha classe pai */
-  recuperar.addEventListener('click',(e) =>{  /* Passando um evento de clic para minha classe pai com a propriedades de evento! */
-    const target1 = e.target  /* Estou atribuindo a propriedade target do meu evento click ao meu (parametro (e))*/
-    const recuperaColor = document.getElementsByClassName('color') /* Estou recuperando a minha classe 'color' */
-    for(let index = 0; index < recuperaColor.length; index += 1){ /* Pegando todos os indexda minha classe 'color' */
-      recuperaColor[index].classList.remove('selected')/* Remove de todos meus indexa classe 'selected' */
+function click() {
+  const recuperar = document.getElementById('color-palette'); /* Estou pegando minha classe pai */
+  recuperar.addEventListener('click', (e) => {  /* Passando um evento de clic para minha classe pai com a propriedades de evento! */
+    const target1 = e.target;  /* Estou atribuindo a propriedade target do meu evento click ao meu (parametro (e))*/
+    if (target1.classList.contains('color')) { /* SE - localName da minha propriedade target do meu evento for igual a 'div'*/
+      const recuperaColor = document.querySelectorAll('.color'); /* Estou recuperando a minha classe 'color' */
+      for (let index = 0; index < recuperaColor.length; index += 1) { /* Pegando todos os indexda minha classe 'color' */
+        recuperaColor[index].classList.remove('selected');/* Remove de todos meus indexa classe 'selected' */
+      }
+      target1.classList.add('selected'); /* então add a classe selected */
     }
-    if(target1.localName === 'div'){ /* SE - localName da minha propriedade target do meu evento for igual a 'div'*/
-    target1.classList.add('selected') /* então add a classe selected */
-  }
-  })
+  });
 }
 click();
+
+
 
