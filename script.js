4d8ac82ac34cd2cabd4524c105e3e9611e275ee9
root <root@DESKTOP-74CRLKE.localdomain>
2021-03-13 00:05:48 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+let paletaDeCores = document.getElementsByClassName('color');
 
+function clique(event) {
+  let corSelecionada = event.target;
+  let SelectAnterior = document.querySelector('.selected');
+  SelectAnterior.classList.remove('selected');
+  corSelecionada.classList.add('selected');
+}
+
+for (let index = 0; index < paletaDeCores.length; index += 1) {
+  paletaDeCores[index].addEventListener('click', clique);
+}
