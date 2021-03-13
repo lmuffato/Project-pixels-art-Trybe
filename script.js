0145463df6be94acc5ba32bc3e4924fafe138d52
root <root@DESKTOP-74CRLKE.localdomain>
2021-03-13 11:07:16 -0300

    requisito 7: 2 tentativa

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-let paletaDeCores = document.getElementsByClassName('color');
+function escolherCor() {
+  let paletaDeCores = document.getElementsByClassName('color');
 
-function clique(event) {
-  let corSelecionada = event.target;
-  let SelectAnterior = document.querySelector('.selected');
-  SelectAnterior.classList.remove('selected');
-  corSelecionada.classList.add('selected');
-}
+  function clicar(event) {
+    let corSelecionada = event.target;
+    let UltimoSelect = document.querySelector('.selected');
+    UltimoSelect.classList.remove('selected');
+    corSelecionada.classList.add('selected');
+  }
 
-for (let index = 0; index < paletaDeCores.length; index += 1) {
-  paletaDeCores[index].addEventListener('click', clique);
+  for (let index = 0; index < paletaDeCores.length; index += 1) {
+    paletaDeCores[index].addEventListener('click', clicar);
+
+  } 
+
 }
