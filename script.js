1ba28d39ef165c18be1979b648c98932590ebff5
Nathalia Zebral <natzebral@gmail.com>
2021-03-13 12:19:50 -0300

    correcoes do codigo de acordo com o eslint e style lint. retirada das consts coloClass linha 8 e pixel linha 16

--- script.js
+++ script.js
@@ -2,31 +2,30 @@ const paletteColors = document.getElementById('color-palette');
 
 function changeClassColors(event) {
  const colorsDiv = document.getElementsByClassName('color');
-	for(let index = 0; index < colorsDiv.length; index += 1 ) {
-		colorsDiv[index].classList.remove('selected');
-	}
-	const colorClass = event.target.classList.add('selected');
+ for (let index = 0; index < colorsDiv.length; index += 1) {
+ colorsDiv[index].classList.remove('selected');
+ }
+ event.target.classList.add('selected');
 }
-	paletteColors.addEventListener("click", changeClassColors);
 
-
+paletteColors.addEventListener("click", changeClassColors);
 
 const pixelBoard = document.getElementById('pixel-board');
-function ChangeBackgroundColor(event){
-	const pixel = document.getElementsByClassName('pixel');
-	const selectedElement = document.getElementsByClassName('selected')[0];
-	const colorToUse = selectedElement.style.backgroundColor;
-	event.target.style.backgroundColor = colorToUse;
+function ChangeBackgroundColor(event) {
+ document.getElementsByClassName('pixel');
+ const selectedElement = document.getElementsByClassName('selected')[0];
+ const colorToUse = selectedElement.style.backgroundColor;
+ event.target.style.backgroundColor = colorToUse;
 }
 
-pixelBoard.addEventListener("click", ChangeBackgroundColor);
+pixelBoard.addEventListener('click', ChangeBackgroundColor);
 
 const button = document.querySelector('#clear-board');
 
 function clearAll() {
  const pixels = document.querySelectorAll('td');
- for(let index = 0; index < pixels.length; index += 1) {
-	pixels[index].style.backgroundColor = "white";
+ for (let index = 0; index < pixels.length; index += 1) {
+ pixels[index].style.backgroundColor = 'white';
+ }
 }
-}
-button.addEventListener("click", clearAll);
+button.addEventListener('click', clearAll);
