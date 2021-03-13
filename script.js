d8f503b2c08068c8e596e0c0741f6662eaf8efc1
walvesb65 <willian.alvesb65@gmail.com>
2021-03-13 17:26:47 -0300

    requisito 7

--- script.js
+++ script.js
@@ -35,4 +35,19 @@ function corBlack(){
   const corBlack1 = document.getElementsByClassName('color')[0].classList.add('selected');
 }
 corBlack();
+/* Neste requisito recebi ajuda do amigo Lucas Godoi turma 10 tribo A, mas obtive total entendimento da questão */
+function click(){
+  const recuperar = document.getElementById('color-palette') /* Estou pegando minha classe pai */
+  recuperar.addEventListener('click',(e) =>{  /* Passando um evento de clic para minha classe pai com a propriedades de evento! */
+    const target1 = e.target  /* Estou atribuindo a propriedade target do meu evento click ao meu (parametro (e))*/
+    const recuperaColor = document.getElementsByClassName('color') /* Estou recuperando a minha classe 'color' */
+    for(let index = 0; index < recuperaColor.length; index += 1){ /* Pegando todos os indexda minha classe 'color' */
+      recuperaColor[index].classList.remove('selected')/* Remove de todos meus indexa classe 'selected' */
+    }
+    if(target1.localName === 'div'){ /* SE - localName da minha propriedade target do meu evento for igual a 'div'*/
+    target1.classList.add('selected') /* então add a classe selected */
+  }
+  })
+}
+click();
 
