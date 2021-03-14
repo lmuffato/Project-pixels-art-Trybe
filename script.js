5f8c7102ee329310583a6e20a12db7f65ac2019c
Maria Lisboa <marialisboa1515@gmail.com>
2021-03-14 16:15:58 -0300

    corrigindo linst

--- script.js
+++ script.js
@@ -4,28 +4,27 @@ let elementoTd = document.getElementsByClassName('pixel');
 window.onload = function () {
   document.getElementsByClassName('color')[0].classList.add('select');
   color = "black";
-  for (let index = 0; index < elementoTd.length; index ++) {
-  elementoTd[index].onclick = function(event){ addColor(event)};      
-}
+  for (let index = 0; index < elementoTd.length; index++) {
+  elementoTd[index].onclick = function (event) { addColor(event)};
+  }
 }
 
-function addColor (elemento){
+function addColor (elemento) {
   elemento.target.classList.add(color);
 }
 
-function limpar () {
+function limpar() {
   for (let index = 0; index < elementoTd.length; index++) {
-
-  elementoTd[index].classList.remove('black');
-  elementoTd[index].classList.remove('pink');
-  elementoTd[index].classList.remove('yellow');
-  elementoTd[index].classList.remove('grey');        
-}
+    elementoTd[index].classList.remove('black');
+    elementoTd[index].classList.remove('pink');
+    elementoTd[index].classList.remove('yellow');
+    elementoTd[index].classList.remove('grey');        
+  }
 }
 
 let colorSelect = document.getElementsByClassName('color');
-  for (let index = 0; index < colorSelect.length; index ++ ) {
-  colorSelect[index].onclick = function(event){ selectElementoCor(event)};   
+  for (let index = 0; index < colorSelect.length; index++) {
+colorSelect[index].onclick = function (event) { selectElementoCor(event)};   
 }
 let classes;
   function selectElementoCor (evento) {
