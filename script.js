fa67553b70c6a423fe1055fa6dc34b5cdf489f48
diegoCorreia <diegomethal@gmail.com>
2021-03-14 14:24:57 -0300

    correçao lint

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const colorList = document.querySelectorAll('.color');
-    for (let index = 0; index < 4; index += 1){
-        colorList[0].style.backgroundColor = 'blue';
-        colorList[1].style.backgroundColor = 'yellow';
-        colorList[2].style.backgroundColor = 'red';
-        colorList[3].style.backgroundColor = 'green';
-    }
+for (let index = 0; index < 4; index += 1) {
+  colorList[0].style.backgroundColor = 'blue';
+  colorList[1].style.backgroundColor = 'yellow';
+  colorList[2].style.backgroundColor = 'red';
+  colorList[3].style.backgroundColor = 'green';
+}
 colorList[0].style.backgroundColor = 'black';
 colorList[0].classList.add('selected');
 
-/*const tableBoard = document.querySelector('#pixel-board');
+/* const tableBoard = document.querySelector('#pixel-board');
   function pixelBoard(line, column){
     for (let line = 0; line < 5; line +=1){
       const tableRow = document.createElement('tr');
@@ -18,6 +18,6 @@ colorList[0].classList.add('selected');
           tableRow.appendChild(tableData);
           tableData.classList.add('pixel');
     }
-  }  
+  }
 }
-pixelBoard(5,5);*/
+pixelBoard(5,5); */
