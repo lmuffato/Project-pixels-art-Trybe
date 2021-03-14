b0111b72df8b17eae352fda327d75f8671eeffd0
Maria Lisboa <marialisboa1515@gmail.com>
2021-03-13 21:44:28 -0300

    desafios 5-8

--- script.js
+++ script.js
@@ -1,13 +1,51 @@
 //verifica se ao abrir a página a cor inicial dos quadros deve ser branca.
-function corInicial (){
-    for (index = 0; index<=25; index += 1) {
-        let corWhite = corInicial[index = 0].style.backgroundColor ;
-    }
-    return corInicial;
+let color;
+
+let elementoTd =document.getElementsByClassName('pixel');
+window.onload = function(){
+    color = "black";
+    for (let index = 0; index < elementoTd.length; index++) {
+
+        elementoTd[index].onclick = function(event){ addColor(event)};
+        
+   }
+   
+
+}
+function addColor(elemento){
+    elemento.target.classList.add(color);
+}
+
+function limpar(){
+    for (let index = 0; index < elementoTd.length; index++) {
+
+        elementoTd[index].classList.remove('black');
+        elementoTd[index].classList.remove('pink');
+        elementoTd[index].classList.remove('yellow');
+        elementoTd[index].classList.remove('grey');
+        
+   }
 }
 
 // Definindo a cor preta como cor inicial
+let colorSelect = document.getElementsByClassName('color');
+for (let index = 0; index < colorSelect.length; index++) {
 
+    colorSelect[index].onclick = function(event){ selectElementoCor(event)};
+    
+}
+let classes;
+function selectElementoCor(evento){
+    for (let index = 0; index < colorSelect.length; index++) {
+
+        let classes = colorSelect[index].classList;
+       
+            classes.remove("select");  
+        
+    }
+    evento.target.classList.add("select");
+   color = evento.target.id;
+}
 
 
 // Botão que limpa as cores do quadro
