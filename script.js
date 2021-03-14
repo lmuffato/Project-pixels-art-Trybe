5d8cec36ad9a64faebecacc8b6af66519757e3d3
Renata Andrade <reapminas@gmail.com>
2021-03-14 16:50:25 -0300

    Criando funçao para o requisito 2

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-// Acessando cada coluna de uma tabela: https://www.w3schools.com/jsref/prop_html_style.asp
-var columPalete = document.getElementsByTagName("td")[0];
-columPalete.style.backgroundColor = "black";
+function colorsPalete(colors) {
+  for (let index = 0; index < colors.length; index += 1) {
+    // Acessando cada coluna de uma tabela: https://www.w3schools.com/jsref/prop_html_style.asp
+    document.getElementsByTagName('td')[index].style.backgroundColor = colors[index];
+  }
+}
 
-var columPalete = document.getElementsByTagName("td")[1];
-columPalete.style.backgroundColor = "blue";
-
-var columPalete = document.getElementsByTagName("td")[2];
-columPalete.style.backgroundColor = "green";
-
-var columPalete = document.getElementsByTagName("td")[3];
-columPalete.style.backgroundColor = "pink";
+const colors = ['black', 'blue', 'green', 'pink'];
+colorsPalete(colors);
