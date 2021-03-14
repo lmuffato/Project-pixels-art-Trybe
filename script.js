885d99cc16d1dfd8fddb2f88728a68f2d6bbb534
johnpligeiro <johnpligeiro@gmail.com>
2021-03-13 21:57:11 -0300

    Requisito 8.

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-window.onload = function() {
-  document.querySelector('#color-palette').addEventListener('click', classChange);
-  function classChange(event) {
-    const paletteColors = document.querySelectorAll('.color');
-    for (let index = 0; index < paletteColors.length; index += 1) {
-      paletteColors[index].classList.remove('selected');
-    }
-    event.target.classList.add('selected');
+function classChange(event) {
+  const paletteColors = document.querySelectorAll('.color');
+  for (let index = 0; index < paletteColors.length; index += 1) {
+    paletteColors[index].classList.remove('selected');
   }
-};
+  event.target.classList.add('selected');
+}
+
+document.querySelector('#color-palette').addEventListener('click', classChange);
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle
+
+function selectColor() {
+  const getColorElement = document.querySelector('.selected');
+  const selColor = window.getComputedStyle(getColorElement).getPropertyValue('background-color');
+  return selColor;
+}
+
+function pixelsColor() {
+  const pixels = document.getElementsByClassName('pixel');
+  Array.from(pixels).forEach((pixel) => {
+    pixel.addEventListener('click', (event) => {
+      const pixelItem = event.target;
+      pixelItem.style.backgroundColor = selectColor();
+    });
+  });
+}
+pixelsColor();
