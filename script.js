4bf0a23ccf65c7ab73623dc84cc5d832044ef6a5
¨WesleyFernandoAlmeida¨ <wesley.fer.almeida98@gmail.com>
2021-03-13 21:06:23 -0300

    alteração dentro da função paintPixel

--- script.js
+++ script.js
@@ -1,38 +1,37 @@
 function removeSelected() {
-    const element = document.querySelector('.selected');
-    element.classList.remove('selected')
+  const element = document.querySelector('.selected');
+  element.classList.remove('selected');
 }
 
 function addSelected(event) {
-    event.target.classList.add('selected')
+  event.target.classList.add('selected');
 }
 
 function handleClick(event) {
-
-    if (event.target.id !== 'color-palette') {
-        removeSelected()
-        addSelected(event)
-    }
+  if (event.target.id !== 'color-palette') {
+    removeSelected();
+    addSelected(event);
+  }
 }
 
 function listenToColorPalette() {
-    const colorPalette = document.querySelector('#color-palette');
-    colorPalette.addEventListener('click', handleClick)
+  const colorPalette = document.querySelector('#color-palette');
+  colorPalette.addEventListener('click', handleClick);
 }
 
 listenToColorPalette();
 
 function paintPixel(event) {
-    const elementWithSelected = document.querySelector('.selected');
-    const selectColor = elementWithSelected.id;
-    
-    event.target.style.backgroundColor = selectColor;
+  const elementWithSelected = document.querySelector('.selected');
+  const selectColor = elementWithSelected.id;
+  const clickedPixel = event.target;
+
+  clickedPixel.style.backgroundColor = selectColor;
 }
 
 function listenToColorPixel() {
-    const pixelBoard = document.querySelector('#pixel-board');
-    pixelBoard.addEventListener('click', paintPixel)
-
+  const pixelBoard = document.querySelector('#pixel-board');
+  pixelBoard.addEventListener('click', paintPixel);
 }
 
 listenToColorPixel();
