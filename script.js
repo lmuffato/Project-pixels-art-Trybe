4724cf1e30241cce236bce3e3aa971c3d48e2bcc
root <root@DESKTOP-74CRLKE.localdomain>
2021-03-14 01:47:26 -0300

    requisito 8: tive que usar if e else por problemas tecnicos

--- script.js
+++ script.js
@@ -10,7 +10,24 @@ function escolherCor() {
 
   for (let index = 0; index < paletaDeCores.length; index += 1) {
     paletaDeCores[index].addEventListener('click', clicar);
+  }  
+}
 
-  } 
+let pixels = document.getElementsByClassName('pixel');
+for (let index = 0; index < pixels.length; index += 1) {
+  pixels[index].addEventListener('click', mudarCor);
+}
+function mudarCor(event) {
+  let armazena = event.target;
+  let select = document.querySelector('.selected');
 
+  if (select === document.getElementById('cor1')) {
+    armazena.style.backgroundColor = 'black';
+  } else if (select === document.getElementById('cor2')) {
+    armazena.style.backgroundColor = 'rgb(184, 17, 17)';
+  } else if (select === document.getElementById('cor3')) {
+    armazena.style.backgroundColor = 'rgb(28, 53, 129)';
+  } else if (select === document.getElementById('cor4')) {
+    armazena.style.backgroundColor = 'rgb(61, 61, 61)';
+  }
 }
