365c3f051d2e2c63d0492d1ce4c9c67f5a200ea3
Maria Baeta <m.martinsdg@gmail.com>
2021-03-13 21:11:54 -0300

    Atende ao requisito 7

--- script.js
+++ script.js
@@ -36,8 +36,32 @@ function createElementsTable(number) {
   // Dar um retorno pra minha função ok
   return pixelBoard;
 }
-// Chamar minha funçao
+
+// Requisito 7
+//  7. Recuperar meu elemento com a class color (selected)
+function getColor(event) {
+  const color = document.querySelectorAll('.color');
+  // Remover a class selected do elemento atual e
+  for (let index = 0; index < color.length; index += 1) {
+    if (color[index].className === 'color selected') {
+      color[index].classList.remove('selected');
+    }
+  }
+  // Adicionar a class selected ao clicar em outro elemento;
+  if (event.target.className === 'color') {
+    event.target.classList.add('selected');
+  }
+}
+
+function managerEvents() {
+//  7. Recuperar o elemento  pai da class selected
+  const colorPalette = document.querySelector('#color-palette');
+  // 7. Adiciona um evento de click (Adicionar a class selected ao clicar em outro elemento);
+  colorPalette.addEventListener('click', getColor);
+}
+// Chamar minhas funções:
 window.onload = function start() {
   setColors();
   createElementsTable(5);
+  managerEvents();
 };
