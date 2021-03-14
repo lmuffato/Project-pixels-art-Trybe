c39a96fdbfebd666e8e3c8552866848038acb5da
Maria Lisboa <marialisboa1515@gmail.com>
2021-03-14 16:59:22 -0300

    adicionando classe select

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 let color;
 let elementoTd = document.getElementsByClassName('pixel');
 window.onload = function () {
-  document.getElementsByClassName('color')[0].classList.add('select');
   color = "black";
   for (let index = 0; index < elementoTd.length; index++) {
   elementoTd[index].onclick = function (event) { addColor(event)};
@@ -26,12 +25,13 @@ let colorSelect = document.getElementsByClassName('color');
   for (let index = 0; index < colorSelect.length; index++) {
 colorSelect[index].onclick = function (event) { selectElementoCor(event)};   
 }
+
 let classes;
   function selectElementoCor (evento) {
   for (let index = 0; index < colorSelect.length; index ++ ) {
   let classes = colorSelect[index].classList;
   classes.remove("select");      
-}
+  }
   evento.target.classList.add("select");
   color = evento.target.id;
 }
