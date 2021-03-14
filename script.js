bbe6943185289421d4d0f75842acbbe74512d4b9
Tiago Santos <tiago881@gmail.com>
2021-03-14 18:14:45 -0300

    8 - Clicar em um pixel dentro do quadro após selecionar uma cor na paleta, faz com que o pixel seja preenchido com a cor selecionada

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ function quadrado() {
   }
 }
 
+quadrado();
+
 function paletteColor() {
   const colorPalette = document.querySelectorAll('.color');
   for (let index = 0; index < colorPalette.length; index += 1) {
@@ -21,5 +23,15 @@ function paletteColor() {
 }
 
 paletteColor();
-
-window.addEventListener('load', quadrado, paletteColor);
+// 8 - Clicar em um pixel dentro do quadro após selecionar uma cor na paleta, faz com que o pixel seja preenchido com a cor selecionada.
+function pixelColor() {
+  const pixel = document.querySelectorAll('.pixel');
+  // console.log(pixel);
+  for (let index = 0; index < pixel.length; index += 1) {
+    pixel[index].addEventListener('click', (e) => {
+      const color = document.querySelector('.selected');
+      e.target.style.backgroundColor = color.style.backgroundColor;
+    });
+  }
+}
+pixelColor();
