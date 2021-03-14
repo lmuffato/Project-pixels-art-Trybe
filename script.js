7a1b00bd2134156f80f86f48c4a5507e401d55f2
Maria Lisboa <marialisboa1515@gmail.com>
2021-03-14 16:06:14 -0300

    corrigindo erros linst

--- script.js
+++ script.js
@@ -1,54 +1,38 @@
-//verifica se ao abrir a página a cor inicial dos quadros deve ser branca.
+
 let color;
-
-let elementoTd =document.getElementsByClassName('pixel');
-window.onload = function(){
-    document.getElementsByClassName('color')[0].classList.add('select');
-    color = "black";
-    for (let index = 0; index < elementoTd.length; index++) {
-
-        elementoTd[index].onclick = function(event){ addColor(event)};
-        
-   }
-   
-
+let elementoTd = document.getElementsByClassName('pixel');
+window.onload = function () {
+  document.getElementsByClassName('color')[0].classList.add('select');
+  color = "black";
+  for (let index = 0; index < elementoTd.length; index ++) {
+  elementoTd[index].onclick = function(event){ addColor(event)};      
 }
-function addColor(elemento){
-    elemento.target.classList.add(color);
 }
 
-function limpar(){
-    for (let index = 0; index < elementoTd.length; index++) {
-
-        elementoTd[index].classList.remove('black');
-        elementoTd[index].classList.remove('pink');
-        elementoTd[index].classList.remove('yellow');
-        elementoTd[index].classList.remove('grey');
-        
-   }
+function addColor (elemento){
+  elemento.target.classList.add(color);
 }
 
-// Definindo a cor preta como cor inicial
+function limpar () {
+  for (let index = 0; index < elementoTd.length; index++) {
+
+  elementoTd[index].classList.remove('black');
+  elementoTd[index].classList.remove('pink');
+  elementoTd[index].classList.remove('yellow');
+  elementoTd[index].classList.remove('grey');        
+}
+}
+
 let colorSelect = document.getElementsByClassName('color');
-for (let index = 0; index < colorSelect.length; index++) {
-
-    colorSelect[index].onclick = function(event){ selectElementoCor(event)};
-    
+  for (let index = 0; index < colorSelect.length; index ++ ) {
+  colorSelect[index].onclick = function(event){ selectElementoCor(event)};   
 }
 let classes;
-function selectElementoCor(evento){
-    for (let index = 0; index < colorSelect.length; index++) {
-
-        let classes = colorSelect[index].classList;
-       
-            classes.remove("select");  
-        
-    }
-    evento.target.classList.add("select");
-   color = evento.target.id;
+  function selectElementoCor (evento) {
+  for (let index = 0; index < colorSelect.length; index ++ ) {
+  let classes = colorSelect[index].classList;
+  classes.remove("select");      
 }
-
-
-// Botão que limpa as cores do quadro
-
-
+  evento.target.classList.add("select");
+  color = evento.target.id;
+}
