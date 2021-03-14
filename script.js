1830b801df2bb542658a166395bfd8fab9c352ca
¨Moises <moisescoimbra.digitalpro@gmail.com>
2021-03-14 13:22:44 -0300

    Requisitos de 7 a 9

---
+++ script.js
@@ -0,0 +1,37 @@
+const addClassSelected = document.querySelector('.color');
+addClassSelected.className = 'color selected';
+
+const changeNameClass = document.getElementById('color-palette');
+
+function colorSelected(event) {
+  document.getElementById('black').className = 'color';
+  document.getElementById('yellow').className = 'color';
+  document.getElementById('blue').className = 'color';
+  document.getElementById('green').className = 'color';
+  document.getElementById('red').className = 'color';
+
+  const targetEvent = event.target.id;
+  document.getElementById(targetEvent).className = 'color selected';
+}
+
+changeNameClass.addEventListener('click', colorSelected);
+
+const variable = document.getElementById('pixel-board');
+
+function colorize(colorPixel) {
+  const colorizePixel = document.querySelector('.selected').id;
+  colorPixel.target.style.backgroundColor = colorizePixel;
+}
+
+variable.addEventListener('click', colorize);
+
+const clearBoard = document.querySelector('#clear-board');
+
+function boardCleanAll() {
+  const pixelFrame = document.querySelectorAll('.pixel');
+  for (let index = 0; index < pixelFrame.length; index += 1) {
+    pixelFrame[index].style.backgroundColor = 'white';
+  }
+}
+
+clearBoard.addEventListener('click', boardCleanAll);
