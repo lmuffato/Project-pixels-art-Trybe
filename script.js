780f8c96bea62c83f58c5432ad439f63eeb49c68
Renata Andrade <reapminas@gmail.com>
2021-03-14 19:30:53 -0300

    Requisito 7 - Clicar em uma das cores da paleta faz com que ela seja selecionada e utilizada para preencher os pixels no quadro

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ colorsPalete(colors);
 
 // Adicionando uma nova classe no elemento https://www.w3schools.com/howto/howto_js_add_class.asp
 document.getElementsByClassName('color')[0].classList.add('selected');
+
+let selected = document.getElementsByClassName('color');
+// Adicionando o evento para cada elemento da lista https://stackoverflow.com/questions/19655189/javascript-click-event-listener-on-class
+for (let index = 0; index < selected.length; index += 1) {
+  selected[index].addEventListener('click', receiveClick);
+}
+
+function receiveClick (event) {
+  // Removendo a classe de seleção do elemento anterior https://www.w3schools.com/howto/howto_js_remove_class.asp
+  document.getElementsByClassName('selected')[0].classList.remove('selected');
+  event.target.classList.add('selected');
+}
