470e25a36cf5673fc02017bc6643646e04ba8248
diogomarin <diogoamarin@gmail.com>
2021-03-14 04:30:46 -0300

    Meeting requirements 6 and 7

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function randomColor() {
   }
   return color;
 }
-console.log(randomColor());
+// console.log(randomColor());
 
 firstColor.style.backgroundColor = 'black';
 secondColor.style.backgroundColor = randomColor();
@@ -19,3 +19,41 @@ thirdColor.style.backgroundColor = randomColor();
 fourthColor.style.backgroundColor = randomColor();
 
 sessionStorage.setItem('color', 'black');
+
+//Função que ao clicar em uma das cores da paleta é adiconado uma nova classe (selected) para cor clicada.
+
+function addColorSelected() {
+  const selected = document.getElementsByClassName('color selected');
+  const colors = document.querySelectorAll('.color');
+
+  for (let i = 0; i < colors.length; i += 1) {
+  colors[i].addEventListener('click', function(event) {
+    if (selected.length === 0) {
+      event.target.className = 'color selected'
+    } else {
+      event.target.className = 'color'
+    }
+  });
+  } 
+};
+
+addColorSelected();
+
+function addColorToPixel() {
+  const selected = document.getElementsByClassName('color selected');
+  const colors = document.querySelectorAll('.color');
+  const pixels = document.querySelector('#pixel-board');
+  const colorPalette = colors[2].style.backgroundColor;
+
+  pixels.addEventListener('click', function(event) {
+    let eventTargetColor = event.target.style.backgroundColor;
+    if (selected.length > 0 && eventTargetColor !== colorPalette) {
+      let pixelColor = selected[0].style.backgroundColor;
+      event.target.style.backgroundColor = pixelColor;
+    } else if (eventTargetColor === colorPalette && selected.length !== 0) {
+      event.target.style.color = 'white';
+    }
+  });
+};
+
+addColorToPixel();
