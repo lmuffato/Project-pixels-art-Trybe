4937d7b75df219d4aa0c1ab3152edae6210463dc
GetulioGutemberg <getulio.dev@gmail.com>
2021-03-15 02:28:06 -0300

    resolve requisito 7

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function selectColor() {
+    const colors = document.querySelectorAll('.color');
+    for(let index1 = 0; index1 < colors.length; index += 1) {
+        colors[index1].addEventListener('click', (e) => {
+            for(let index2 = 0; index2 < colors.length; index2 += 1) {
+                colors[index2].classList.remove('selected');
+            }
+            e.target.classList.add('selected');
+        });
+    }
+}
 
+selectColor();
