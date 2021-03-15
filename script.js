6968298c37e59747419e8c974b502e4a3f1d1fa2
biazidioti <bzferreira.bio@gmail.com>
2021-03-15 13:27:34 -0300

    Add funtion that changes the class selected.

--- script.js
+++ script.js
@@ -13,20 +13,24 @@ addPixel();
 // criar uma função com condição para add class selected uma cor por vez. Evento. 
 // window.onload = funtion () {
 
-// }
+// criar elemento clique
+let colorPalette = document.getElementsByClassName('color');
 
-function selectedColor() {
-  let colorPalette = document.getElementById('color-palette');
-  colorPalette.addEventListener('click', function ('click', select) {
-    let colorElement = select.target;
-    let colors = document.querySelectorAll('.color');
-    for (let index = 0; index < colors.length; index += 1) {
-      colors[index].classList.remove('selected');
-    }
-    if (colorElement.localName === 'div') {
-      colorElement.classList.add('selected');
-    }
-  });
+// Criar for para adicionar a cada elementos
+for (let index = 0; index < colorPalette.length; index +=1){
+colorPalette[index].addEventListener('click', selectedColor);
 }
 
-selectedColor();
+// evento da fução clique
+function selectedColor(evento) {
+  
+   // REmove a classe select     
+    let colors = document.querySelectorAll('.selected');
+    colors[0].classList.remove('selected');
+
+    // Adiciona a classe selected no elemento
+    let colorElement = evento.target;
+    colorElement.classList.add('selected');
+}
+
+// selectedColor();
