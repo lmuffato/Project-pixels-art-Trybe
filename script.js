fbe7fcb1bf4e9cf05b069cae1d740681fff93a33
Alex Smith <alexsmith7192@gmail.com>
2021-03-15 19:34:26 -0300

    6 - refatorando class selected inicial

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.onload = function main() {
   const paletteColors = document.querySelectorAll('.color');
   const colors = ['black', 'red', 'blue', 'green'];
 
-  document.querySelector('.color').className += ' selected';
+  paletteColors[0].className += ' selected';
 
   for (let indexPalette = 0; indexPalette < paletteColors.length; indexPalette += 1) {
     paletteColors[indexPalette].style.backgroundColor = colors[indexPalette];
