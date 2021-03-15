f24cbc3bbb8e3dbeb65138ca54e439c7420efecf
Victor Ricco <victor.ricco001@gmail.com>
2021-03-15 16:59:36 -0300

    clear button working

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ window.onload = function(){
 
   let colorInfo = document.querySelector('#color-palette');
 	let frame = document.querySelector('#pixel-board');
+	let button = document.querySelector('#clear-board');
+
+	button.addEventListener('click', function(event){
+		let pixels = document.querySelectorAll('.pixel');
+		for(index = 0; index < pixels.length; index += 1){
+			pixels[index].style.backgroundColor = 'white';
+		}
+	});
 
 	colorInfo.addEventListener('click', function(event){
 		let colorName = event.target.id;
@@ -26,7 +34,7 @@ window.onload = function(){
 			targetPixel.style.backgroundColor = `${colorSelected}`
 		}
 	});
-
+	
 	function createTable(height, weight){
 		let table = document.querySelector('#pixel-board');
 		for(let index = 0; index < height; index += 1){
