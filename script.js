6aecca845051ca7b836943305f1e575c926cafa5
Junior Portela <portelajr14@gmail.com>
2021-03-14 21:02:15 -0300

    Requisito 2 finzalizado

--- script.js
+++ script.js
@@ -2,17 +2,32 @@ function createPalette () {
   let paletteItems = ['red', 'blue', 'yellow', 'green'];
   let getPaleta = document.getElementById("color-palette");
   
-  for (let index = 0; index < paletteItems.length; index += 0) {
+  for (let index = 0; index < paletteItems.length; index += 1) {
 
     let createPalette = document.createElement("div");
     createPalette.className = 'color';
     
     createPalette.style.backgroundColor = paletteItems[index];
+    createPalette.style.display = 'inline-block';
+    createPalette.style.alignItems = 'space-between';
 
     getPaleta.appendChild(createPalette);
    
   }
-}
-
+};
 
 createPalette();
+
+
+//criar evento do clique
+
+// armazenar em sessionstorage ? a cor do clique
+
+// evento: clique.
+
+// clique p
+
+// taget/type pra idetificar quem chamou o evento?
+
+
+// 'pintar' com valor armazenado em local storage
