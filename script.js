3d1baa7542600b4f9f89c26846f5e8a8263e8556
Jorge Meyrelles Jr <jotaengpuc@gmail.com>
2021-03-14 21:19:28 -0300

    exercicio 1

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const tit = document.querySelector('#title');
+function creatingTitle () {
+  const tit = document.querySelector('#title');
 const txt = tit.innerHTML = 'Paleta de Cores';
 const bodymain = document.querySelector('body');
 bodymain.appendChild(tit);
+}
+creatingTitle();
+
