b93e222afe5ed305edaf848d854be92767a7d218
Jorge Meyrelles Jr <jotaengpuc@gmail.com>
2021-03-14 23:30:50 -0300

    exercicio 6

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function creatingPalleteColors (qtd) {
     const createDiv = document.createElement('div');
     // cor aleatoria: https://qastack.com.br/programming/1484506/random-color-generator
     const color = '#' + Math.floor (Math.random () * 16777215) .toString (16);
-    if (i == 0) {
+    if (i === 0) {
       createDiv.style.backgroundColor = 'Black';
       createDiv.className = 'selected';
       sectionMain.appendChild(createDiv);
@@ -27,7 +27,7 @@ function pixelBoard (vertical, horizontal) {
   const sectionMain = document.querySelector('#pixel-board');
   for (let i = 0; i < vertical; i += 1) {
     const createSection = document.createElement('section');
-    for (let j = 0; j < horizontal; j +=1) {
+    for (let j = 0; j < horizontal; j += 1) {
       const createDiv = document.createElement('div');
       createDiv.className = 'pixel';
       createSection.appendChild(createDiv);
