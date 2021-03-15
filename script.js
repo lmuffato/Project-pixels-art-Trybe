e4ef649d420506775c61f616a75e2856400b4983
Jorge Meyrelles Jr <jotaengpuc@gmail.com>
2021-03-14 22:11:44 -0300

    exercicio 2

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function creatingTitle () {
   const tit = document.querySelector('#title');
-const txt = tit.innerHTML = 'Paleta de Cores';
-const bodymain = document.querySelector('body');
-bodymain.appendChild(tit);
+  const txt = tit.innerHTML = 'Paleta de Cores';
 }
 creatingTitle();
+
+function creatingPalleteColors (qtd) {
+  for (let i = 0; i < qtd; i += 1) {
+    const sectionMain = document.querySelector('section');
+    const createDiv = document.createElement('div');
+    createDiv.className = 'color';
+    // cor aleatoria: https://qastack.com.br/programming/1484506/random-color-generator
+    const color = '#' + Math.floor (Math.random () * 16777215) .toString (16);
+    createDiv.style.backgroundColor = color;
+    sectionMain.appendChild(createDiv);
+  }
+}
+creatingPalleteColors(4);
 
