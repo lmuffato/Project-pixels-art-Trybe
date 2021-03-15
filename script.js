22dd788a2ee7dc1eee2e5af0f1198cca9114ee12
Jorge Meyrelles Jr <jotaengpuc@gmail.com>
2021-03-14 23:20:33 -0300

    exercicio 6

--- script.js
+++ script.js
@@ -8,14 +8,15 @@ function creatingPalleteColors (qtd) {
   for (let i = 0; i < qtd; i += 1) {
     const sectionMain = document.querySelector('section');
     const createDiv = document.createElement('div');
-    createDiv.className = 'color';
     // cor aleatoria: https://qastack.com.br/programming/1484506/random-color-generator
     const color = '#' + Math.floor (Math.random () * 16777215) .toString (16);
     if (i == 0) {
       createDiv.style.backgroundColor = 'Black';
+      createDiv.className = 'selected';
       sectionMain.appendChild(createDiv);
     } else {
       createDiv.style.backgroundColor = color;
+      createDiv.className = 'color';
       sectionMain.appendChild(createDiv);
     }
   }
