a9170883034a5d311b24885e1abed7b6994e30ac
biazidioti <bzferreira.bio@gmail.com>
2021-03-15 10:34:43 -0300

    Add clear board button.

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-function addPixel() {
-    for (let index = 0; index < 25; index +=1){
-        let pixelParent = document.getElementById('pixel-board');
-        let divPixel = document.createElement('div');
-        divPixel.className = "pixel"; // divPixel.classList.add('pixel') 
-        pixelParent.appendChild(divPixel);
-    }
+// ************** Cria função que adiciona a lcass pixel em cada elemento 'pixel' da board-grid **************** 
+function addPixel() { 
+  for (let index = 0; index < 25; index += 1) {
+      let pixelParent = document.getElementById('pixel-board');
+      let divPixel = document.createElement('div');
+      divPixel.className = 'pixel'; // divPixel.classList.add('pixel') 
+      pixelParent.appendChild(divPixel);
+  }
 }
 
 addPixel();
+
+// criar uma função com condição para add class selected uma cor por vez. Evento. 
+window.onload = funtion () {
+
+}
+
+function selectedColor() {
+  let click = document.getElementById('color');
+  let
+
+  button.addEventListener(1, 2);
+}
