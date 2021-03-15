f10614a6c78b978ce4f1d5fe1eb30bbb1ae651ad
André Aguilar <andreaguilar.ti@gmail.com>
2021-03-15 11:31:02 -0300

    requisitos obrigatorios

--- script.js
+++ script.js
@@ -1,18 +1,44 @@
-function cores () {
-    document.getElementsByClassName("color")[0].style.backgroundColor = 'black';
-    document.getElementsByClassName("color")[1].style.backgroundColor = 'red';
-    document.getElementsByClassName("color")[2].style.backgroundColor = 'blue';
-    document.getElementsByClassName("color")[3].style.backgroundColor = 'green';
+//Código feito com ajuda do código do Luiz Putinatti
+
+// criando o click
+let corClicada = document.getElementsByClassName('color');
+
+//adicionando o click
+for (let index = 0; index < corClicada.length; index = index + 1) {
+    corClicada[index].addEventListener('click', removeSelecao);
 }
-cores ()
 
-// ou 
-//function colorsPaddles() {
-//    const adicionandoCores = document.getElementsByClassName('color');
-//    adicionandoCores[0].style.backgroundColor = 'red';
-//    adicionandoCores[1].style.backgroundColor = 'blue';
-//    adicionandoCores[2].style.backgroundColor = 'purple';
-//    adicionandoCores[3].style.backgroundColor = 'yellow';
-//}
-//colorsPaddles();
+//removendo a selected de todas as classes
+function removeSelecao (evente) {
+    let classeRemovida = document.getElementsByClassName('selected');
+    classeRemovida[0].classList.remove('selected');
 
+    let eventeTarget = evente.target;
+    eventeTarget.classList.add('selected');
+}
+
+//incluindo click nos pixels
+let pixelSelecionado = document.getElementsByClassName('pixel');
+
+for(let index = 0; index < pixelSelecionado.length; index = index + 1) {
+    pixelSelecionado[index].addEventListener('click', corDoPixel);
+}
+
+//clicando no pixel
+function corDoPixel (event) {
+    let eventTarget = event.target;
+    eventTarget.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
+}
+
+//limpando quasro de pixels
+let clearPixels = document.getElementById('clear-board');
+
+clearPixels.addEventListener('click', clearBoard);
+
+function clearBoard () {
+    let blockPixels = document.getElementsByClassName('pixel');
+    for (let index = 0; index< blockPixels.length; index += 1){
+        blockPixels[index].style.backgroundColor = 'white';
+    }
+
+} 
