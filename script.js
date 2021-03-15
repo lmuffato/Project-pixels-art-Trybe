53b99c992705cdce0c36c5dbf15ae8cde0ef619e
Alex Smith <alexsmith7192@gmail.com>
2021-03-15 18:53:56 -0300

    6 - define cor preta no inicio da paleta e a seleciona ao carregar a página

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ window.onload = function main() {
   const paletteColors = document.querySelectorAll('.color');
   const colors = ['black', 'red', 'blue', 'green'];
 
+  document.querySelector('.color').className += ' selected';
+
   for (let indexPalette = 0; indexPalette < paletteColors.length; indexPalette += 1) {
     paletteColors[indexPalette].style.backgroundColor = colors[indexPalette];
   }
