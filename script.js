6002cfc9fa107b92243db00880f361caa469cc08
renancosta21 <renancosta21@gmail.com>
2021-03-16 18:52:33 -0300

    checkando os requisitos 1-7

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+/* const colorsList = document.querySelectorAll('.color');
+
+colorsList[0].style.backgroundColor = 'black';
+colorsList[1].style.backgroundColor = 'red';
+colorsList[2].style.backgroundColor = 'green';
+colorsList[3].style.backgroundColor = 'blue';
+
+function pixelBoard(size) {
+  for (let index = 0; index < size; index += 1) {
+    for (let index2 = 0; index2 < size; index2 += 1) {
+      const pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      document.querySelector('#pixel-board').appendChild(pixel);
+    }
+  }
+}
+
+document.querySelector('#color-palette').addEventListener('click', (selected) => {
+  const selectedColor = document.querySelector('.selected');
+  selectedColor.classList.remove('selected');
+  selected.target.classList.add('selected');
+});
+
+document.querySelector('#pixel-board').addEventListener('click', (fill) => {
+  const color = document.querySelector('.selected').style.backgroundColor;
+  const targeted = fill.target;
+  targeted.style.backgroundColor = color;
+});
+
+document.querySelector('#clear-board').addEventListener('click', () => {
+  const pixel = document.querySelectorAll('.pixel');
+  for (let index = 0; index < pixel.length; index += 1) {
+    pixel[index].style.backgroundColor = 'white';
+  }
+});
+
+pixelBoard(5);
+
+AQUI ACABA O JS DE 128
+*/
+
 const colorsList = [];
 
 function preSelectBlack() {
@@ -13,10 +54,14 @@ for (let index = 0; index < 4; index += 1) {
   document.querySelector('#color-palette').appendChild(colorsList[index]);
 }
 
-document.getElementsByClassName('color')[0].style.backgroundColor = 'black';
-document.getElementsByClassName('color')[1].style.backgroundColor = 'green';
-document.getElementsByClassName('color')[2].style.backgroundColor = 'red';
-document.getElementsByClassName('color')[3].style.backgroundColor = 'blue';
+const firstColor = document.getElementsByClassName('color')[0];
+const secondColor = document.getElementsByClassName('color')[1];
+const thirdColor = document.getElementsByClassName('color')[2];
+const fourthColor = document.getElementsByClassName('color')[3];
+firstColor.style.backgroundColor = 'black';
+secondColor.style.backgroundColor = 'green';
+thirdColor.style.backgroundColor = 'red';
+fourthColor.style.backgroundColor = 'blue';
 
 const pixelBoard = [];
 for (let index = 0; index < 25; index += 1) {
@@ -26,12 +71,12 @@ for (let index = 0; index < 25; index += 1) {
   document.querySelector('#pixel-board').appendChild(pixelBoard[index]);
 }
 
-const clickFirstColor = document.getElementsByClassName('color')[0];
-const clickSecondColor = document.getElementsByClassName('color')[1];
-const clickThirdColor = document.getElementsByClassName('color')[2];
-const clickFourthColor = document.getElementsByClassName('color')[3];
+const clickFirstColor = firstColor;
+const clickSecondColor = secondColor;
+const clickThirdColor = thirdColor;
+const clickFourthColor = fourthColor;
 
-function firstColor() {
+function pickFirstColor() {
   clickFirstColor.classList.remove('selected');
   clickSecondColor.classList.remove('selected');
   clickThirdColor.classList.remove('selected');
@@ -39,7 +84,7 @@ function firstColor() {
   clickFirstColor.classList.add('selected');
 }
 
-function secondColor() {
+function pickSecondColor() {
   clickFirstColor.classList.remove('selected');
   clickSecondColor.classList.remove('selected');
   clickThirdColor.classList.remove('selected');
@@ -47,7 +92,7 @@ function secondColor() {
   clickSecondColor.classList.add('selected');
 }
 
-function thirdColor() {
+function pickThirdColor() {
   clickFirstColor.classList.remove('selected');
   clickSecondColor.classList.remove('selected');
   clickThirdColor.classList.remove('selected');
@@ -55,7 +100,7 @@ function thirdColor() {
   clickThirdColor.classList.add('selected');
 }
 
-function fourthColor() {
+function pickFourthColor() {
   clickFirstColor.classList.remove('selected');
   clickSecondColor.classList.remove('selected');
   clickThirdColor.classList.remove('selected');
@@ -63,7 +108,33 @@ function fourthColor() {
   clickFourthColor.classList.add('selected');
 }
 
-clickFirstColor.addEventListener('click', firstColor);
-clickSecondColor.addEventListener('click', secondColor);
-clickThirdColor.addEventListener('click', thirdColor);
-clickFourthColor.addEventListener('click', fourthColor);
+clickFirstColor.addEventListener('click', pickFirstColor);
+clickSecondColor.addEventListener('click', pickSecondColor);
+clickThirdColor.addEventListener('click', pickThirdColor);
+clickFourthColor.addEventListener('click', pickFourthColor);
+
+const pixels = [];
+
+/* function grabColor() {
+  const bGColor = document.querySelector('.selected');
+  if (bGColor.style.backgroundColor === firstColor.style.backgroundColor) {
+    console.log(bGColor);
+  } else if (bGColor.style.backgroundColor === secondColor.style.backgroundColor) {
+    console.log(bGColor);
+  } else if (bGColor.style.backgroundColor === thirdColor.style.backgroundColor) {
+    console.log(bGColor);
+  } else if (bGColor.style.backgroundColor === fourthColor.style.backgroundColor) {
+    console.log(bGColor);
+  }
+}
+*/
+
+function grabColor() {
+  console.log('teste');
+}
+
+for (let index = 0; index < pixelBoard.length; index += 1) {
+  pixels.push('');
+  pixels[index] = document.getElementsByClassName('pixel')[index];
+  pixels[index].addEventListener('click', grabColor);
+}
