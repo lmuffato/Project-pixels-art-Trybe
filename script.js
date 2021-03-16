d642cc7215d28451164521351ba3af00302fafba
Junior Portela <portelajr14@gmail.com>
2021-03-16 11:26:54 -0300

    Requisitos 3,4,5 executados

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function createPalette() {
-  const paletteItems = ['red', 'blue', 'yellow', 'green'];
+  const paletteItems = ['black', 'blue', 'yellow', 'green'];
   const getPalette = document.getElementById('color-palette');
 
   for (let index = 0; index < paletteItems.length; index += 1) {
@@ -14,6 +14,40 @@ function createPalette() {
 }
 
 createPalette();
+
+function createTr() {
+  const getBoard = document.getElementById('pixel-board');
+  for(let index = 0; index < 5; index += 1){
+    const divTr = document.createElement('div');
+    divTr.className = ('linha'+ index);
+    divTr.className += ' div-tr';
+    getBoard.appendChild(divTr); 
+  }
+
+  createPixel();
+   
+}
+
+createTr();
+
+function createPixel() {
+  // const getLines = document.getElementsByClassName('div-tr');
+  const getLines = document.getElementById('pixel-board').childNodes;
+  for (let index = 0; index < getLines.length; index += 1) {
+
+    let getTr = getLines[index];
+    for (let i = 0; i < getLines.length; i += 1) {
+
+      let createCell = document.createElement('div');
+      createCell.className = 'pixel';
+
+      getTr.appendChild(createCell);
+
+    }
+  }
+
+}
+
 
 //criar evento do clique
 
