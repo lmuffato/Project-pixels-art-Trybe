98c64f24b2f1ff630ee094b130243dd86aa82f1c
Djaniza Vasques <djanizav@gmail.com>
2021-03-16 04:50:12 -0300

    Adiciona requisito 6

---
+++ script.js
@@ -0,0 +1,9 @@
+
+  let picColor = document.querySelectorAll('.color');
+  picColor.addEventListener('click', function()) {
+    for( let index = 1; index < picColor.length; index += 1){
+      if(picColor[index].contains('selected')){
+        
+      }
+    }
+  }
