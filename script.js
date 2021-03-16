38f9d7b2a242d1536a9b916b8e3a79a971c54631
Wendell Costa <wendellcosta9141@gmail.com>
2021-03-16 15:10:40 -0300

    [Requisito 8 - Concluido]

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Define a cor de background das paletas
 document.querySelector('.black').style.backgroundColor = 'black';
 document.querySelector('.blue').style.backgroundColor = 'blue';
-document.querySelector('.red').style.backgroundColor = 'green';
-document.querySelector('.yellow').style.backgroundColor = 'red';
+document.querySelector('.red').style.backgroundColor = 'red';
+document.querySelector('.green').style.backgroundColor = 'green';
 
-// Define a cor 'selected' ao clicar na paleta de cores
+// Define a cor 'selected' ao clicar na paleta de cores, remove a antiga cor 'selected'
 
 function colorSelection() {
   const listElements = document.getElementsByClassName('color');
@@ -20,4 +20,25 @@ function colorSelection() {
   }
 }
 
+// Pinta um pixel utilizando a cor 'selected'
+function colorDraw() {
+  const pixelElements = document.getElementsByClassName('pixel');
+
+  for (let i = 0; i < pixelElements.length; i += 1) {
+    pixelElements[i].addEventListener('click', () => {
+      const selected = document.getElementsByClassName('selected')[0];
+      if (selected.classList.contains('black')) {
+        pixelElements[i].style.backgroundColor = 'black';
+      } else if (selected.classList.contains('red')) {
+        pixelElements[i].style.backgroundColor = 'red';
+      } else if (selected.classList.contains('green')) {
+        pixelElements[i].style.backgroundColor = 'green';
+      } else if (selected.classList.contains('blue')) {
+        pixelElements[i].style.backgroundColor = 'blue';
+      }
+    });
+  }
+}
+
+colorDraw();
 colorSelection();
