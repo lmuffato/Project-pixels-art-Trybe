b808c3f60c4107f2db5acf7da6c4615221dfd442
renancosta21 <renancosta21@gmail.com>
2021-03-16 19:32:03 -0300

    requisito 8

--- script.js
+++ script.js
@@ -1,44 +1,3 @@
-/* const colorsList = document.querySelectorAll('.color');
-
-colorsList[0].style.backgroundColor = 'black';
-colorsList[1].style.backgroundColor = 'red';
-colorsList[2].style.backgroundColor = 'green';
-colorsList[3].style.backgroundColor = 'blue';
-
-function pixelBoard(size) {
-  for (let index = 0; index < size; index += 1) {
-    for (let index2 = 0; index2 < size; index2 += 1) {
-      const pixel = document.createElement('div');
-      pixel.className = 'pixel';
-      document.querySelector('#pixel-board').appendChild(pixel);
-    }
-  }
-}
-
-document.querySelector('#color-palette').addEventListener('click', (selected) => {
-  const selectedColor = document.querySelector('.selected');
-  selectedColor.classList.remove('selected');
-  selected.target.classList.add('selected');
-});
-
-document.querySelector('#pixel-board').addEventListener('click', (fill) => {
-  const color = document.querySelector('.selected').style.backgroundColor;
-  const targeted = fill.target;
-  targeted.style.backgroundColor = color;
-});
-
-document.querySelector('#clear-board').addEventListener('click', () => {
-  const pixel = document.querySelectorAll('.pixel');
-  for (let index = 0; index < pixel.length; index += 1) {
-    pixel[index].style.backgroundColor = 'white';
-  }
-});
-
-pixelBoard(5);
-
-AQUI ACABA O JS DE 128
-*/
-
 const colorsList = [];
 
 function preSelectBlack() {
@@ -115,22 +74,10 @@ clickFourthColor.addEventListener('click', pickFourthColor);
 
 const pixels = [];
 
-/* function grabColor() {
-  const bGColor = document.querySelector('.selected');
-  if (bGColor.style.backgroundColor === firstColor.style.backgroundColor) {
-    console.log(bGColor);
-  } else if (bGColor.style.backgroundColor === secondColor.style.backgroundColor) {
-    console.log(bGColor);
-  } else if (bGColor.style.backgroundColor === thirdColor.style.backgroundColor) {
-    console.log(bGColor);
-  } else if (bGColor.style.backgroundColor === fourthColor.style.backgroundColor) {
-    console.log(bGColor);
-  }
-}
-*/
-
-function grabColor() {
-  console.log('teste');
+function grabColor(fill) {
+  const pickedColor = document.querySelector('.selected').style.backgroundColor;
+  const pixelBg = fill.target;
+  pixelBg.style.backgroundColor = pickedColor;
 }
 
 for (let index = 0; index < pixelBoard.length; index += 1) {
