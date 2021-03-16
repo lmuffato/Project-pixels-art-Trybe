40405528cd90059a74d1ed8413e0f55d09c51087
Elisa <elisafranca.carvalho@gmail.com>
2021-03-16 19:05:31 -0300

    Requisito 10: Faz o quadro de pixels ter seu tamanho definido pela pessoa usuária

--- script.js
+++ script.js
@@ -1,23 +1,8 @@
-const pixels = document.getElementsByClassName('pixel');
-
-function createBoard(lines, columns) {
-  const pixelBoard = document.querySelector('#pixel-board');
-
-  for (let index = 0; index < lines; index += 1) {
-    const numberOfLines = document.createElement('tr');
-    pixelBoard.appendChild(numberOfLines);
-    for (let indexColum = 0; indexColum < columns; indexColum += 1) {
-      const numberOfColumns = document.createElement('td');
-      numberOfLines.appendChild(numberOfColumns);
-      numberOfColumns.className = 'pixel';
-    }
-  }
-}
-
 function initiateColor() {
   // Para este requisito consultei os seguintes links:
   // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/from
   // https://www.alura.com.br/artigos/javascript-quando-devo-usar-foreach-e-map?gclid=Cj0KCQiA1pyCBhCtARIsAHaY_5cscwZQUJTOMnmG6B8naM2D5eFCmquk0s-168iGWrcyGy1gb6YSgEMaAmwpEALw_wcB
+  const pixels = document.getElementsByClassName('pixel');
   Array.from(pixels).forEach((element) => {
     const pixel = element;
     pixel.style.backgroundColor = 'white';
@@ -25,6 +10,7 @@ function initiateColor() {
 }
 
 function updatePixelSize() {
+  const pixels = document.getElementsByClassName('pixel');
   Array.from(pixels).forEach((element) => {
     const pixel = element;
     pixel.style.width = '40px';
@@ -34,9 +20,69 @@ function updatePixelSize() {
 }
 
 function updatePixelBorder() {
+  const pixels = document.getElementsByClassName('pixel');
   Array.from(pixels).forEach((element) => {
     const pixel = element;
     pixel.style.border = 'black solid 1px';
+  });
+}
+
+function getDivColor() {
+  // Para essa função contei com ajuda dos colegas: Felipe Muller e Andre Jacques via thread.
+  // Também com a leitura do seguinte link:
+  // https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle
+  const divSelected = document.querySelector('.selected');
+  const divBackground = window.getComputedStyle(divSelected).getPropertyValue('background-color');
+  return divBackground;
+}
+
+function colorPixels() {
+  const pixels = document.getElementsByClassName('pixel');
+  Array.from(pixels).forEach((pixel) => {
+    pixel.addEventListener('click', (event) => {
+      const element = event.target;
+      element.style.backgroundColor = getDivColor();
+    });
+  });
+}
+
+// Para criar o quadro via js contei com a ajuda do Luan Ramalho que dedicou seu tempo em uma call, explicando sua lógica.
+function createBoard(lines, columns, destiny) {
+  console.log(lines);
+  console.log(columns);
+  console.log(destiny);
+  for (let index = 0; index < lines; index += 1) {
+    const numberOfLines = document.createElement('tr');
+    destiny.appendChild(numberOfLines);
+    for (let indexColum = 0; indexColum < columns; indexColum += 1) {
+      const numberOfColumns = document.createElement('td');
+      numberOfLines.appendChild(numberOfColumns);
+      numberOfColumns.className = 'pixel';
+    }
+  }
+  updatePixelSize();
+  updatePixelBorder();
+  colorPixels();
+  initiateColor();
+}
+
+function generateBoard() {
+  const pixelBoard = document.querySelector('#pixel-board');
+  const boardChooseSize = document.getElementById('generate-board');
+  boardChooseSize.addEventListener('click', () => {
+    const numberChoose = document.getElementById('board-size');
+    let defineLinesAndColumns = Number(numberChoose.value);
+    pixelBoard.innerHTML = '';
+    if (defineLinesAndColumns <= 0 || defineLinesAndColumns === '') {
+      alert('Board inválido!');
+    }
+    if (defineLinesAndColumns < 5) {
+      defineLinesAndColumns = 5;
+    }
+    if (defineLinesAndColumns > 50) {
+      defineLinesAndColumns = 50;
+    }
+    createBoard(defineLinesAndColumns, defineLinesAndColumns, pixelBoard);
   });
 }
 
@@ -62,36 +108,17 @@ function changePlaceClassSelected() {
   });
 }
 
-function getDivColor() {
-  // Para essa função contei com ajuda dos colegas: Felipe Muller e Andre Jacques via thread.
-  // Também com a leitura do seguinte link:
-  // https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle
-  const divSelected = document.querySelector('.selected');
-  const divBackground = window.getComputedStyle(divSelected).getPropertyValue('background-color');
-  return divBackground;
-}
-
-function colorPixels() {
-  Array.from(pixels).forEach((pixel) => {
-    pixel.addEventListener('click', (event) => {
-      const element = event.target;
-      element.style.backgroundColor = getDivColor();
-    });
-  });
-}
-
 function clickButton() {
   const button = document.getElementById('clear-board');
   button.addEventListener('click', initiateColor);
 }
 
 window.onload = function startSession() {
-  createBoard(5, 5);
-  initiateColor();
-  updatePixelSize();
-  updatePixelBorder();
+  const pixelBoard = document.querySelector('#pixel-board');
+
+  createBoard(5, 5, pixelBoard);
+  generateBoard();
   createClassSelected();
   changePlaceClassSelected();
-  colorPixels();
   clickButton();
 };
