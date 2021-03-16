1d31d8dc22894b7aa57939f20b6514079865a65c
Djaniza Vasques <djanizav@gmail.com>
2021-03-16 07:04:15 -0300

    Atualiza script.js

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
+window.onload = fillPalette
+ 
+const colorPalette = document.getElementById('color-palette');
+const pixelBoard = document.getElementById('pixel-board');
+const color = document.querySelectorAll('.color');
+const pixels = document.querySelectorAll('.pixel');
 
-  let picColor = document.querySelectorAll('.color');
-  picColor.addEventListener('click', function()) {
-    for( let index = 1; index < picColor.length; index += 1){
-      if(picColor[index].contains('selected')){
-        
-      }
-    }
+function randomColor(){
+  let hexadecimais = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+  let cor = '#';
+  for (let index = 0; index < 6; index += 1 ) {
+     cor += hexadecimais[Math.floor(Math.random() * 16)];
   }
+   return cor;
+}
+// A idéia das cores randomicas foi baseado no trabalho do [RCalvs], mas a função é quase igual a de um usuário 
+// do github chamado Girol, de quase 4 anos atrás. A função dele tinha uma string e não um array.
+
+const colors = ['black', randomColor(), randomColor(), randomColor()];
+
+function fillPalette (){
+  for ( let index = 0; index < colorPalette.length; index += 1) {
+    colorPalette[index].style.backgroundColor = colors[index];
+  }
+}
