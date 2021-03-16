32d036c9ddfe85e3ff84eb224a20324f375e3815
Elisa <elisafranca.carvalho@gmail.com>
2021-03-16 09:41:20 -0300

    Refatora codigo: cria quadro via javascript

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+const pixels = document.getElementsByClassName('pixel');
+
+function createBoard(lines, columns) {
+  const pixelBoard = document.querySelector('#pixel-board');
+
+  for (let index = 0; index < lines; index += 1) {
+    const numberOfLines = document.createElement('tr');
+    pixelBoard.appendChild(numberOfLines);
+    for (let indexColum = 0; indexColum < columns; indexColum += 1) {
+      const numberOfColumns = document.createElement('td');
+      numberOfLines.appendChild(numberOfColumns);
+      numberOfColumns.className = 'pixel';
+    }
+  }
+}
+
 function initiateColor() {
-  const pixels = document.getElementsByClassName('pixel');
   // Para este requisito consultei os seguintes links:
   // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/from
   // https://www.alura.com.br/artigos/javascript-quando-devo-usar-foreach-e-map?gclid=Cj0KCQiA1pyCBhCtARIsAHaY_5cscwZQUJTOMnmG6B8naM2D5eFCmquk0s-168iGWrcyGy1gb6YSgEMaAmwpEALw_wcB
@@ -10,7 +25,6 @@ function initiateColor() {
 }
 
 function updatePixelSize() {
-  const pixels = document.getElementsByClassName('pixel');
   Array.from(pixels).forEach((element) => {
     const pixel = element;
     pixel.style.width = '40px';
@@ -20,7 +34,6 @@ function updatePixelSize() {
 }
 
 function updatePixelBorder() {
-  const pixels = document.getElementsByClassName('pixel');
   Array.from(pixels).forEach((element) => {
     const pixel = element;
     pixel.style.border = 'black solid 1px';
@@ -59,8 +72,6 @@ function getDivColor() {
 }
 
 function colorPixels() {
-  const pixels = document.getElementsByClassName('pixel');
-
   Array.from(pixels).forEach((pixel) => {
     pixel.addEventListener('click', (event) => {
       const element = event.target;
@@ -75,6 +86,7 @@ function clickButton() {
 }
 
 window.onload = function startSession() {
+  createBoard(5, 5);
   initiateColor();
   updatePixelSize();
   updatePixelBorder();
