65cfd7a117369809361733eb15875fc6b2d0d7f0
Priscila <prihscapin@gmail.com>
2021-03-16 18:42:31 -0300

    Requisitos 8 e 9 fixed - again

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function criaPixels (numberOfPixels) {
    for (let index = 0; index < numberOfPixels * numberOfPixels; index +=1) {
    const fazDiv = document.createElement('div');
-   fazDiv.className = 'pixel tr';
+   fazDiv.className = 'pixel';
    const divMae = document.getElementById('pixel-board');
   divMae.append(fazDiv);
 }
@@ -12,7 +12,7 @@ criaPixels(5)
 // Requisito 7 - Seleciona cor
 function selecionaCor() {
    const cores = document.getElementsByClassName('color');
-
+   
    for (let index = 0; index < cores.length; index += 1) {
    cores[index].addEventListener('click',() => {
       for (let i = 0; i < cores.length; i += 1) {
@@ -24,40 +24,51 @@ function selecionaCor() {
 }
  selecionaCor();
 
+
+
+
 // Requisito 8 - Pinta pixels
+selecionaCor ()
+const cor =  document.querySelector('.selected');
+const pixel = document.querySelectorAll('.pixel');
 
-function pintaPixels() {
-   const pixel = document.querySelector('.pixel tr');
-   for(i = 0; i <= pixel.length; i += 1); {
-      pixel[i].addEventListener('click', (event) => {
-      const clickedPixel = event.target;
-      const cor = documento.querySelector('.selected').style.backgroundColor;
-      console.log(color)
-      clickedPixel.style.backgroundColor = color;
+  function pintaPixels(event) {
+  
+  // percorre quadro e adiciona o clique:
+
+ for (let index = 0; index < pixel.length; index += 1) {
+    
+     pixel[index].addEventListener('click', (event)=> {
+        //if(cor.style.backgroundColor = 'green') {
+         pixel[index].style.backgroundColor = cor.style.backgroundColor;
+     // } else if (cor.style.backgroundColor = 'green'){
+     //    pixel[index].style.backgroundColor = 'green'
+     // } else if (cor.style.backgroundColor = 'brown'){
+     //   pixel[index].style.backgroundColor = 'brown'
+     // } else if (cor.style.backgroundColor = 'black'){
+     //   pixel[index].style.backgroundColor = 'black'
       })
-   }   
+      }
+   //} 
+ }
+  
+selecionaCor ();
+pintaPixels();
 
-}
 
 
-
-//function setColor() {
- //  const pixelTable = document.querySelector('#pixel-board');
-  // pixelTable.addEventListener('click', (event) => {
-   //  const selectPixel = event.target;
-   //  const color = document.querySelector('.selected').style.backgroundColor;
-     // console.log(color)
-     //selectPixel.style.backgroundColor = color;
 
 // Requisito 9 - Limpa board
 function limpaQuadro () {
 const botao = document.getElementById('clear-board');
-const pixels = document.getElementsByClassName('pixel tr');
 
    botao.addEventListener('click', () => {
-      for (let index = 0; index < pixels.length; i += 1) {
-      pixels[index].style.backgroundColor = 'white';
+      for (let index = 0; index < pixel.length; index += 1) {
+         pixel[index].style.backgroundColor = 'white';
+
       }
 
    })
 }
+
+limpaQuadro()
