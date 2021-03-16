22773334313ac377add50779f84941c1943a4147
diegoCorreia <diegomethal@gmail.com>
2021-03-15 22:07:43 -0300

    requisitos corrigidos

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 const colorList = document.querySelectorAll('.color');
 function colorPanel() {
-for (let index = 0; index < 4; index += 1) {
-  colorList[0].style.backgroundColor = 'black';
-  colorList[0].classList.add('selected');
-  colorList[0].className ='selected';
-  colorList[1].style.backgroundColor = 'yellow';
-  colorList[1].classList.add('selected');
-  colorList[1].className ='selected';
-  colorList[2].style.backgroundColor = 'red';
-  colorList[2].classList.add('selected');
-  colorList[2].className ='selected';
-  colorList[3].style.backgroundColor = 'green';
-  colorList[3].classList.add('selected');
-  colorList[3].className ='selected';
-}
+  for (let index = 0; index < 4; index += 1) {
+    colorList[0].style.backgroundColor = 'black';
+    colorList[0].classList.add('selected');
+    colorList[0].className = 'selected';
+    colorList[1].style.backgroundColor = 'yellow';
+    colorList[1].classList.add('selected');
+    colorList[1].className = 'selected';
+    colorList[2].style.backgroundColor = 'red';
+    colorList[2].classList.add('selected');
+    colorList[2].className = 'selected';
+    colorList[3].style.backgroundColor = 'green';
+    colorList[3].classList.add('selected');
+    colorList[3].className = 'selected';
+  }
 }
 colorPanel();
 
 function generateTableBoard() {
-const tableBoard = document.getElementById('pixel-board');
+  const tableBoard = document.getElementById('pixel-board');
   for (let index = 0; index <= 4; index += 1) {
-    const row = document.createElement("tr");
+    const row = document.createElement('tr');
   for (let data = 0; data <= 4; data += 1) {
-    const cell = document.createElement("td");
-    cell.className = 'pixel';
-  row.appendChild(cell);
-}
-tableBoard.appendChild(row);
-}
+    const cell = document.createElement('td');
+      cell.className = 'pixel';
+      row.appendChild(cell);
+    }
+    tableBoard.appendChild(row);
+  }
 }
 generateTableBoard();
 
@@ -39,16 +39,14 @@ function getPanelColors() {
   });
 }
 getPanelColors();
-  
 function setPanelColors() {
   const tbl = document.getElementById('pixel-board');
-  tbl.addEventListener('click' , (event) => {
-  const color = document.querySelector('.selected').style.backgroundColor;
-  event.target.style.backgroundColor = color;
+  tbl.addEventListener('click', (event) => {
+    const color = document.querySelector('.selected').style.backgroundColor;
+    event.target.style.backgroundColor = color;
   }); 
 }
 setPanelColors();
-
 window.onload = () => {
   colorPanel();
   genereteTableBoard();
