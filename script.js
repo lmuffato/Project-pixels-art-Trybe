58891865caef25c7656b39cc32a2df21098d93b8
diegoCorreia <diegomethal@gmail.com>
2021-03-15 22:31:24 -0300

    requisito 4 e 5

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 const colorList = document.querySelectorAll('.color');
 function colorPanel(){
-for (let index = 0; index < 4; index += 1) {
-  colorList[0].style.backgroundColor = 'black';
-  colorList[1].style.backgroundColor = 'yellow';
-  colorList[2].style.backgroundColor = 'red';
-  colorList[3].style.backgroundColor = 'green';
-}
+  for (let index = 0; index < 4; index += 1) {
+    colorList[0].style.backgroundColor = 'black';
+    colorList[1].style.backgroundColor = 'yellow';
+    colorList[2].style.backgroundColor = 'red';
+    colorList[3].style.backgroundColor = 'green';
+  }
 }
 colorPanel();
+function generateTableBoard() {
+  const tableBoard = document.getElementById('pixel-board');
+    for (let index = 0; index <= 4; index += 1) {
+    const row = document.createElement("tr");
+    for (let data = 0; data <= 4; data += 1) {
+    const cell = document.createElement("td");
+    cell.className = 'pixel';
+    row.appendChild(cell);
+  }
+  tableBoard.appendChild(row);
+}
+}
+generateTableBoard();
+  
 
