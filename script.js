0b0a031828a6cb7fbc009800cc74dd44e82a8426
pmaiamateus <pmaiamateus@gmail.com>
2021-03-17 16:33:04 -0300

    conserta os pixels

---
+++ script.js
@@ -0,0 +1,19 @@
+function createColorPalette(qnt) {
+  for (let index = 0; index < qnt; index += 1) {
+    const square = document.createElement('div');
+    const Palette = document.getElementById('color-palette');
+    Palette.appendChild(square);
+    square.className = 'color';
+  }
+}
+
+createPalettes(4);
+
+function paletteColor() {
+    const palleteColor = document.getElementsByClassName('color');
+    palleteColor[0].style.backgroundColor = 'black';
+    palleteColor[1].style.backgroundColor = 'orange';
+    palleteColor[2].style.backgroundColor = 'purple';
+    palleteColor[3].style.backgroundColor = 'green';
+}
+paletteColor();
