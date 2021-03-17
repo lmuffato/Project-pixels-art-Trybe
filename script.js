f585a5fb06be78160081355081a04c08eaeeb7d8
Lucas <lucasml@live.com>
2021-03-16 21:27:08 -0300

    requisito 2.1- alterando parametros para melhor dinamismo

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-function createPalettes() {
-  for (index = 0; index < 4; index+=1) {
+function createPalettes(num) {
+  for (index = 0; index < num; index+=1) {
     const palettes = document.createElement('div');
     const allPalettes = document.getElementById('color-palette');
     allPalettes.appendChild(palettes);
     palettes.className = 'color';
   }
 }
-createPalettes();
+createPalettes(20);
+
+// function paletteColor(){
+
+// }
