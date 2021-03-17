7e6999c95622e4be724dd39be38c39e22a38d21b
pmaiamateus <pmaiamateus@gmail.com>
2021-03-17 19:08:02 -0300

    conserta passo 7

--- script.js
+++ script.js
@@ -4,14 +4,31 @@ for (let index = 1; index <= 25; index += 1) {
   pixelboard.appendChild(pixel);
   pixel.className = 'pixel';
 }
-//colocando a cor preta como selecionada
+// colocando a cor preta como selecionada
 document.getElementById('color-palette').firstElementChild
-.classList.add('selected');
-
+  .classList.add('selected');
+let currentColor = colorSelected ();
 
 // selecionando cores
-function colorSelected () {
-    let color = window.getComputedStyle(document.querySelector('.selected'),null).getPropertyValue("background-color");
-    return color;
+let selectedPixel = document.querySelectorAll('.pixel');
+const color = document.querySelectorAll('.color');
+
+for (let index = 0; index < 4; index += 1) {
+  color[index].addEventListener('click', function () {
+    document.querySelector('.selected').classList.remove('selected');
+    color[index].classList.add('selected');
+    currentColor = colorSelected ();
+  })
 }
-let currentColor = colorSelected ();
+
+function colorSelected () {
+  const color = window.getComputedStyle(document.querySelector( 
+      '.selected'),null).getPropertyValue("background-color");
+  return color;
+}
+
+for (let index = 0; index < 25; index += 1) {
+    selectedPixel[index].addEventListener('click', function () {
+      selectedPixel[index].style.backgroundColor = currentColor;
+    })
+}
