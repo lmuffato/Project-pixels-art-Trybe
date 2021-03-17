bd3db91880b617114a9a717fc0356b8fc4aea130
Lucas <lucasml@live.com>
2021-03-16 21:24:54 -0300

    requisito 2.1

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-a
+function createPalettes() {
+  for (index = 0; index < 4; index+=1) {
+    const palettes = document.createElement('div');
+    const allPalettes = document.getElementById('color-palette');
+    allPalettes.appendChild(palettes);
+    palettes.className = 'color';
+  }
+}
+createPalettes();
