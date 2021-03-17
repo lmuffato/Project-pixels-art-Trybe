97cbf588cc6d9b61123df94b5b842f3a71a786e2
rcalvs <rafaelcalvette@gmail.com>
2021-03-17 16:11:10 -0300

    Pixel Art finalizado

--- script.js
+++ script.js
@@ -1,67 +1,67 @@
-// color[length] para alterar a cor nas paletas
+// Dica pro futuro: color[length] para alterar a cor nas paletas //
 
 window.onload = function bemVindo() {
-  console.log('teste testando');
+  console.log('Bem vindo a Pixel Art!');
 };
 
-//lets //
+// Variáveis //
 const Palette = document.getElementById('color-palette');
 const Color = document.querySelectorAll('.color');
 const Board = document.getElementById('pixel-board');
 const Clear = document.getElementById('clear-board');
-const palleteColorBasic = ['black', 'red', 'green', 'blue'];
-const palleteColorRandom = ['black', randomColor(), randomColor(), randomColor()];
+// const palleteColorBasic = ['black', 'red', 'green', 'blue'];
 
 // Gerador de Cores Automaticas by Viniccius13 //
 // https://dev.to/akhil_001/generating-random-color-with-single-line-of-js-code-fhj //
 
+const palleteColorRandom = ['black', randomColor(), randomColor(), randomColor()];
 function randomColor() {
   const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
   return randomColor;
-};
+}
 
 // Coloca as cores nas Divs //
 window.onload = function palleteColor() {
-  for (let index = 0; index < palleteColorRandom.length; index += 1){
+  for (let index = 0; index < palleteColorRandom.length; index += 1) {
     Color[index].style.backgroundColor = palleteColorRandom[index];
-};
-
-// Seleciona cor na palette //
-function setColor(event) {
-  const elementTarget = event.target;
-  if (elementTarget.id !== 'color-palette') {
-    Color.forEach((element) => {
-      element.classList.remove('selected');
-      elementTarget.classList.add('selected');
-    });
   }
-};
 
-// Salva cor //
-function getColor() {
-  const elementSelected = document.querySelector('.selected');
-  const color = window.getComputedStyle(elementSelected).getPropertyValue('background-color');
-  return color;
-};
-
-// Pinta pixel //
-function changePixels(event) {
-  const elementTarget = event.target;
-  if (elementTarget.id !== 'pixel-board') {
-    elementTarget.style.backgroundColor = getColor();
+  // Seleciona cor na palette //
+  function setColor(event) {
+    const elementTarget = event.target;
+    if (elementTarget.id !== 'color-palette') {
+      Color.forEach((element) => {
+        element.classList.remove('selected');
+        elementTarget.classList.add('selected');
+      });
+    }
   }
-};
 
-// Botão clear //
-// Teve um menino que bolou essa solução sem precisar de For nem nada,
-// procurei nos Code-Review e não achei ele, então se alguem saber quem é
-// me avise para creditá-lo da forma correta!
-function clearBoard() {
-  window.location.reload();
-};
+  // Salva cor //
+  function getColor() {
+    const elementSelected = document.querySelector('.selected');
+    const color = window.getComputedStyle(elementSelected).getPropertyValue('background-color');
+    return color;
+  }
 
-// Clicks //
-Palette.addEventListener('click', setColor);
-Board.addEventListener('click', changePixels);
-Clear.addEventListener('click', clearBoard);
-}
+  // Pinta pixel //
+  function changePixels(event) {
+    const elementTarget = event.target;
+    if (elementTarget.id !== 'pixel-board') {
+      elementTarget.style.backgroundColor = getColor();
+    }
+  }
+
+  // Botão clear //
+  // Teve um menino que bolou essa solução sem precisar de For nem nada,
+  // procurei nos Code-Review e não achei ele, então se alguem saber quem é
+  // me avise para creditá-lo da forma correta!
+  function clearBoard() {
+    window.location.reload();
+  }
+
+  // Clicks //
+  Palette.addEventListener('click', setColor);
+  Board.addEventListener('click', changePixels);
+  Clear.addEventListener('click', clearBoard);
+};
