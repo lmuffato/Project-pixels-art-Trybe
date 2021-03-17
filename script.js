911e445e574830301f5053b4de7c5454a5d20677
pmaiamateus <pmaiamateus@gmail.com>
2021-03-17 17:06:14 -0300

    tenta consertar o script

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 function createColorPalette(qnt) {
   for (let index = 0; index < qnt; index += 1) {
     const square = document.createElement('div');
-    const Palette = document.getElementById('color-palette');
-    Palette.appendChild(square);
+    const palette = document.getElementById('color-palette');
     square.className = 'color';
+    palette.appendChild(square);
   }
 }
 
 createColorPalette(4);
 
 function paletteColor() {
-    const palleteColor = document.getElementsByClassName('color');
-    palleteColor[0].style.backgroundColor = 'black';
-    palleteColor[1].style.backgroundColor = 'orange';
-    palleteColor[2].style.backgroundColor = 'purple';
-    palleteColor[3].style.backgroundColor = 'green';
+  const palleteColor = document.getElementsByClassName('color');
+  palleteColor[0].style.backgroundColor = 'black';
+  palleteColor[1].style.backgroundColor = 'orange';
+  palleteColor[2].style.backgroundColor = 'purple';
+  palleteColor[3].style.backgroundColor = 'green';
 }
 paletteColor();
