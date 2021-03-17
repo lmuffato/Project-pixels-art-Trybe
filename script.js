4dd4baa17cbf45468770334f534a11269a618118
pmaiamateus <pmaiamateus@gmail.com>
2021-03-17 19:15:31 -0300

    adiciona passo 8

--- script.js
+++ script.js
@@ -7,19 +7,11 @@ for (let index = 1; index <= 25; index += 1) {
 // colocando a cor preta como selecionada
 document.getElementById('color-palette').firstElementChild
   .classList.add('selected');
-let currentColor = colorSelected ();
+
 
 // selecionando cores
 let selectedPixel = document.querySelectorAll('.pixel');
 const color = document.querySelectorAll('.color');
-
-for (let index = 0; index < 4; index += 1) {
-  color[index].addEventListener('click', function () {
-    document.querySelector('.selected').classList.remove('selected');
-    color[index].classList.add('selected');
-    currentColor = colorSelected ();
-  })
-}
 
 function colorSelected () {
   const color = window.getComputedStyle(document.querySelector( 
@@ -27,8 +19,18 @@ function colorSelected () {
   return color;
 }
 
+for (let index = 0; index < 4; index += 1) {
+  color[index].addEventListener('click', function () {
+    document.querySelector('.selected').classList.remove('selected');
+    color[index].classList.add('selected');
+    currentColor = colorSelected();
+  })
+}
+
+let currentColor = colorSelected();
+
 for (let index = 0; index < 25; index += 1) {
-    selectedPixel[index].addEventListener('click', function () {
-      selectedPixel[index].style.backgroundColor = currentColor;
-    })
+  selectedPixel[index].addEventListener('click', function () {
+    selectedPixel[index].style.backgroundColor = currentColor;
+  })
 }
