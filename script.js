ae94854409a2086dc1e66a338f77506220e7d01f
Lucas <lucasml@live.com>
2021-03-16 22:06:26 -0300

    criterios 4 e 5

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function createPalettes(num) {
-  for (index = 0; index < num; index+=1) {
+  for (let index = 0; index < num; index += 1) {
     const palettes = document.createElement('div');
     const allPalettes = document.getElementById('color-palette');
     allPalettes.appendChild(palettes);
@@ -8,13 +8,26 @@ function createPalettes(num) {
 }
 createPalettes(4);
 
-function paletteColor(){
-
-    const pColor = document.getElementsByClassName('color');
-    pColor[0].style.backgroundColor = 'black';
-    pColor[1].style.backgroundColor = 'green';
-    pColor[2].style.backgroundColor = 'yellow';
-    pColor[3].style.backgroundColor = 'blue';
-
+function paletteColor() {
+  const pColor = document.getElementsByClassName('color');
+  pColor[0].style.backgroundColor = 'black';
+  pColor[1].style.backgroundColor = 'green';
+  pColor[2].style.backgroundColor = 'yellow';
+  pColor[3].style.backgroundColor = 'blue';
 }
 paletteColor();
+
+function createPixels() {
+  const pixelBox = document.getElementById('pixel-board');
+  for (let index = 0; index < 5; index += 1) {
+    const pixelC = document.createElement('div');
+    pixelBox.appendChild(pixelC);
+    for (let indexx = 0; indexx < 5; indexx += 1) {
+      const pixelL = document.createElement('div');
+      pixelC.appendChild(pixelL);
+      pixelL.className = 'pixel';
+    }
+  }
+}
+
+createPixels();
