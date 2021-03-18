95560f68a128d4d5cfcfe4f5109c154a21c1c7b6
Henrique Lima <hnamix@gmail.com>
2021-03-18 07:35:16 -0400

    requisito 07

--- script.js
+++ script.js
@@ -48,10 +48,8 @@ function selectColor_4() {
 color_4.addEventListener('click', selectColor_4);
 */
 let selected = document.getElementsByClassName('color');
-for (let index = 0; index < selected.length; index += 1) {
-  if (selected[index].includes('selected')){
-    selected[index].classList.remove("selected")
-  }
+if (selected[0].includes('selected')){
+  selected[0].classList.remove("selected")
 }
 
 const color_1 = document.getElementsByClassName('color')[0];
@@ -59,36 +57,20 @@ const color_2 = document.getElementsByClassName('color')[1];
 const color_3 = document.getElementsByClassName('color')[2];
 const color_4 = document.getElementsByClassName('color')[3];
 
-color_1.addEventListener('click',chooseColor);
-color_2.addEventListener('click',chooseColor);
-color_3.addEventListener('click',chooseColor);
-color_4.addEventListener('click',chooseColor);
-
-
-
-function chooseColor(e){
+function selectColor(e){
     if(e.target.id == "color_1"){
-        color_1.classList.add("selected")
-        color_2.classList.remove("selected")
-        color_3.classList.remove("selected")
-        color_4.classList.remove("selected")
+      color_1.className = 'color selected';
     }
     else if(e.target.id == "color_2"){
-        color_2.classList.add("selected")
-        color_1.classList.remove("selected")
-        color_3.classList.remove("selected")
-        color_4.classList.remove("selected")
+      color_2.className = 'color selected';
     }else if(e.target.id == "color_3"){
-        red.classList.add("selected")
-        color_1.classList.remove("selected")
-        color_2.classList.remove("selected")
-        color_4.classList.remove("selected")
+      color_3.className = 'color selected';
     }else if(e.target.id == "color_4"){
-        color_4.classList.add("selected")
-        color_1.classList.remove("selected")
-        color_2.classList.remove("selected")
-        color_3.classList.remove("selected")
+      color_4.className = 'color selected';
     }
-
-
 }
+
+color_1.addEventListener('click', selectColor);
+color_2.addEventListener('click', selectColor);
+color_3.addEventListener('click', selectColor);
+color_4.addEventListener('click', selectColor);
