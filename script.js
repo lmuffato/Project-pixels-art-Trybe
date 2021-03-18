db07a1d5c744b9f014ffd123c9e73a4d06e8372c
LuanAlexandre <luanrb19@gmail.com>
2021-03-18 02:18:53 -0400

    Dia 5.5 - projeto-pixels-art: requisito 7

--- script.js
+++ script.js
@@ -16,3 +16,28 @@ window.onload = function pixelsArt() {
 
   document.getElementById('black').classList.add('selected');
 };
+
+const colors = document.getElementsByClassName('color');
+
+function changeColor(e) {
+  for (let index = 0; index < colors.length; index += 1) {
+    const element = colors[index];
+    const event = e.target;
+
+    if (event === element) {
+      event.classList.add('selected');
+    } else {
+      element.classList.remove('selected');
+    }
+  }
+}
+
+function selectColor() {
+  for (let index = 0; index < colors.length; index += 1) {
+    const element = colors[index];
+
+    element.addEventListener('click', changeColor);
+  }
+}
+
+selectColor();
