a689496b23bbb9e666929771c9f06bfda0c0383f
Henrique Lima <hnamix@gmail.com>
2021-03-18 03:26:47 -0400

    requisito 07

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let selected = document.getElementsByClassName('color');
+/*let selected = document.getElementsByClassName('color');
 
 const color_1 = document.getElementsByClassName('color')[0];
 const color_2 = document.getElementsByClassName('color')[1];
@@ -46,3 +46,18 @@ function selectColor_4() {
 }
 
 color_4.addEventListener('click', selectColor_4);
+*/
+
+document.getElementsByClassName('color')[0].classList.add('selected');
+
+let selected = document.getElementsByClassName('color');
+// Adicionando o evento para cada elemento da lista https://stackoverflow.com/questions/19655189/javascript-click-event-listener-on-class
+for (let index = 0; index < selected.length; index += 1) {
+  selected[index].addEventListener('click', receiveClick);
+}
+
+function receiveClick (event) {
+  // Removendo a classe de seleção do elemento anterior https://www.w3schools.com/howto/howto_js_remove_class.asp
+  document.getElementsByClassName('selected')[0].classList.remove('selected');
+  event.target.classList.add('selected');
+}
