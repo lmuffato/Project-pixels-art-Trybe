70e85c62d9f809ffae93337862c787ba8eb9f1e5
Alex Smith <alexsmith7192@gmail.com>
2021-03-19 10:13:12 -0300

    7 - Seleciona uma cor quando clica na paleta de cores

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-window.onload = function main() {
-  const paletteColors = document.querySelectorAll('.color');
-  const colors = ['black', 'red', 'blue', 'green'];
+const paletteColors = document.querySelectorAll('.color');
+const colors = ['black', 'red', 'blue', 'green'];
 
-  paletteColors[0].className += ' selected';
+paletteColors[0].className += ' selected';
 
-  for (let indexPalette = 0; indexPalette < paletteColors.length; indexPalette += 1) {
-    paletteColors[indexPalette].style.backgroundColor = colors[indexPalette];
+for (let indexPalette = 0; indexPalette < paletteColors.length; indexPalette += 1) {
+  paletteColors[indexPalette].style.backgroundColor = colors[indexPalette];
+}
+
+function removeSelected() {
+  const selected = document.querySelector('.selected');
+  selected.classList.remove('selected');
+}
+
+function selectColor() {
+  for (let index = 0; index < paletteColors.length; index += 1) {
+    paletteColors[index].addEventListener('click', function(){
+      removeSelected()
+      paletteColors[index].className = 'color selected';
+      const colorSelected = paletteColors[index].style.backgroundColor;
+      return colorSelected;
+    });
   }
-};
+}
+
+selectColor();
