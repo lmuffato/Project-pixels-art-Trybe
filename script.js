f183acbb78c202fb2ebbfa6084049481c6fc9107
Alex Smith <alexsmith7192@gmail.com>
2021-03-19 11:38:03 -0300

    8 - Pinta os pixels com a cor escolhida

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const paletteColors = document.querySelectorAll('.color');
 const colors = ['black', 'red', 'blue', 'green'];
+const pixels = document.querySelectorAll('.pixel');
 
 paletteColors[0].className += ' selected';
 
 for (let indexPalette = 0; indexPalette < paletteColors.length; indexPalette += 1) {
   paletteColors[indexPalette].style.backgroundColor = colors[indexPalette];
 }
+
+let colorSelected = document.querySelector('.selected').style.backgroundColor;
 
 function removeSelected() {
   const selected = document.querySelector('.selected');
@@ -15,12 +18,20 @@ function removeSelected() {
 function selectColor() {
   for (let index = 0; index < paletteColors.length; index += 1) {
     paletteColors[index].addEventListener('click', function(){
-      removeSelected()
+      removeSelected();
       paletteColors[index].className = 'color selected';
-      const colorSelected = paletteColors[index].style.backgroundColor;
-      return colorSelected;
+      colorSelected = paletteColors[index].style.backgroundColor;
+    });
+  }
+}
+
+function paintPixel() {
+  for (let indexPixel = 0; indexPixel < pixels.length; indexPixel += 1) {
+    pixels[indexPixel].addEventListener('click', function(){
+      pixels[indexPixel].style.backgroundColor = colorSelected;
     });
   }
 }
 
 selectColor();
+paintPixel();
