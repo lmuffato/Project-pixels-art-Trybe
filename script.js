9ad50ea86ddc9011c270ad04e14595fca508c019
Henrique Lima <hnamix@gmail.com>
2021-03-18 22:24:49 -0400

    requisito 07

--- script.js
+++ script.js
@@ -1,98 +1,16 @@
-/*let selected = document.getElementsByClassName('color');
+function addSelect(){
+Const addSelect = document.querySelectorAll('.color');
 
-const color_1 = document.getElementsByClassName('color')[0];
-const color_2 = document.getElementsByClassName('color')[1];
-const color_3 = document.getElementsByClassName('color')[2];
-const color_4 = document.getElementsByClassName('color')[3];
-
-//for (let index = 0; index < selected.length; index += 1) {
-  //if (selected[index].className === 'selected') {
-    selected[0].classList.remove('selected');
-  //}
-//}
-
-function selectColor_1() {
-  color_1.className = 'color selected';
-  color_2.className = 'color';
-  color_3.className = 'color';
-  color_4.className = 'color';
+for (let index = 0; index < addSelect.length; index += 1){
+    addSelect[index].addEventListener('click', removeSelect);
+}
 }
 
-color_1.addEventListener('click', selectColor_1);
-
-function selectColor_2() {
-  color_1.className = 'color';
-  color_2.className = 'color selected';
-  color_3.className = 'color';
-  color_4.className = 'color';
+function removeSelect () {
+  for (let index = 0; index < addSelect.length; index += 1){
+    addSelect[index].classList.remove('select');
+}
+this.classList.add('select');
 }
 
-color_2.addEventListener('click', selectColor_2);
-
-function selectColor_3() {
-  color_1.className = 'color';
-  color_2.className = 'color';
-  color_3.className = 'color selected';
-  color_4.className = 'color';
-}
-
-color_3.addEventListener('click', selectColor_3);
-
-function selectColor_4() {
-  color_1.className = 'color';
-  color_2.className = 'color';
-  color_3.className = 'color';
-  color_4.className = 'color selected';
-}
-
-color_4.addEventListener('click', selectColor_4);
-*/
-
-let selected = document.getElementsByClassName('color');
-
-if (selected[0].classList('selected')) {
-  selected[0].classList.remove("selected");
-}
-
-const color_1 = document.getElementsByClassName('color')[0];
-const color_2 = document.getElementsByClassName('color')[1];
-const color_3 = document.getElementsByClassName('color')[2];
-const color_4 = document.getElementsByClassName('color')[3];
-
-function selectColor(e){
-  verificaSelected();
-  
-      if(e.target.id === "color_1"){
-      color_1.classList = 'color selected';
-    }
-    else if(e.target.id === "color_2"){
-      color_2.classList = 'color selected';
-    }else if(e.target.id === "color_3"){
-      color_3.classList = 'color selected';
-    }else if(e.target.id === "color_4"){
-      color_4.classList = 'color selected';
-    }
-}
-
-function verificaSelected() {
-  let cont = 0;
-  for (let index = 0; index < selected.length; index++){
-  if (selected[index].classList('selected')) {
-    cont += 1;
-   }
-}
-if ( connt > 1) {
-  removeSelected();
-}
-function removeSelected() {
-  for (let index = 0; index < selected.length; index++){
-  if (selected[index].classList('selected')) {
-    selected[index].classList.remove("selected");
-  }
-}
-
-}
-color_1.addEventListener('click', selectColor);
-color_2.addEventListener('click', selectColor);
-color_3.addEventListener('click', selectColor);
-color_4.addEventListener('click', selectColor);
+addSelect();
