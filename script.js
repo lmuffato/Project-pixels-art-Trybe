1f9e2e846d686f00c7f559287ea3a6d3fe906c2d
Henrique Lima <hnamix@gmail.com>
2021-03-20 11:34:39 -0400

    refatoraçao

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let cor = document.getElementsByClassName('color');
-const black = document.querySelectorAll('.class')[0];
-const red = document.querySelectorAll('.class')[1];
-const blue = document.querySelectorAll('.class')[2];
-const green = document.querySelectorAll('.class')[3];
+const black = document.querySelectorAll('.color')[0];
+const red = document.querySelectorAll('.color')[1];
+const blue = document.querySelectorAll('.color')[2];
+const green = document.querySelectorAll('.color')[3];
 
 black.style.backgroundColor = 'black';
 red.style.backgroundColor = 'red';
