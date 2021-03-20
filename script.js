4b5b64c375878b5479c7cc98e9a26e1a31aa3bc9
Henrique Lima <hnamix@gmail.com>
2021-03-20 14:26:46 -0400

    requisito 09

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const black = document.querySelectorAll('.color')[0];
 black.className += ' selected';
+window.onload = black;
 const red = document.querySelectorAll('.color')[1];
 const blue = document.querySelectorAll('.color')[2];
 const green = document.querySelectorAll('.color')[3];
@@ -9,8 +10,8 @@ red.style.backgroundColor = 'red';
 blue.style.backgroundColor = 'darkblue';
 green.style.backgroundColor = 'darkgreen';
 
+// Requisito 07
 function trocaCor(evento) {
-  console.log(evento);
   const elementos = document.querySelectorAll('.color');
   for (let index = 0; index < elementos.length; index += 1) {
     if (elementos[index].classList === 'selected');
@@ -22,3 +23,11 @@ function trocaCor(evento) {
 const select = document.getElementById('color-palette');
 select.addEventListener('click', trocaCor);
 
+// Requisito 08
+
+const caixas = document.querySelectorAll('#pixel');
+caixas.addEventListener('click', pintar);
+
+function pintar(evento) {
+  evento.target.style.backgroundColor = select.classList('.selected').style.backgroundColor;
+}
