f513352edbf3bfe574790463e670c76560de3970
Henrique Lima <hnamix@gmail.com>
2021-03-20 11:06:58 -0400

    requisitos refeitos

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 let cor = document.getElementsByClassName('color');
-cor[0].style.backgroundColor = 'black';
+const black = document.querySelectorAll('.class')[0];
+const red = document.querySelectorAll('.class')[1];
+const blue = document.querySelectorAll('.class')[2];
+const green = document.querySelectorAll('.class')[3];
+
+black.style.backgroundColor = 'black';
+red.style.backgroundColor = 'red';
+blue.style.backgroundColor = 'darkblue';
+green.style.backgroundColor = 'darkgreen';
