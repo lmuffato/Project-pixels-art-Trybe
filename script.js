4930f55d1e8d830c80ba7a4f3a4793ebece3a521
Henrique Lima <hnamix@gmail.com>
2021-03-20 12:41:50 -0400

    Requisito 07

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ black.style.backgroundColor = 'black';
 red.style.backgroundColor = 'red';
 blue.style.backgroundColor = 'darkblue';
 green.style.backgroundColor = 'darkgreen';
+
+function trocaCor(evento) {
+  console.log(evento);
+  const elementos = document.querySelectorAll('.color');
+  for (let index = 0; index < elementos.length; index += 1) {
+    if (elementos[index].classList === 'selected');
+    elementos[index].classList.remove('selected');
+  }
+  evento.target.classList.add('selected');
+}
+
+const select = document.getElementById('color-palette');
+select.addEventListener('click', trocaCor);
+
