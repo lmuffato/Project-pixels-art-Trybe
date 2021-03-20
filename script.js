83e6ae6aa1d9842aa87c6233df4c08847eb4027e
Henrique Lima <hnamix@gmail.com>
2021-03-20 10:48:25 -0400

    Requisito 01

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 // Requisito 01
-const bodY = document.querySelector('body');
+/*const bodY = document.querySelector('body');
 const h1 = document.createElement('h1');
 const headeR = document.createElement('header')
 bodY.appendChild(headeR);
 headeR.appendChild(h1);
 h1.innerText = 'Paleta de Cores';
 h1.id = 'title';
+
+// Requisito 02
+
 
 /*const black = document.querySelectorAll('.class')[0];
 black.className += ' selected';
