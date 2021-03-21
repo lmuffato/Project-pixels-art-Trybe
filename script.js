e390713555a3537ef335720c341e0061e4c063d9
Giovanni <giovannitadeu08@gmail.com>
2021-03-21 15:55:34 -0300

    Requisito bonus de cores aleatorias

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ for (let colorPos = 0; colorPos < 4; colorPos += 1) { // Gera os blocos de cores
   colorPalette.appendChild(createBoxColors);
 }
 
-for (let linePos = 0; linePos < 5; linePos += 1) { //  Gera a tabela
+for (let linePos = 0; linePos < 5; linePos += 1) { //  Gera a tabela.
   const linha = document.createElement('div');
   linha.className = 'line';
   pixelBoard.appendChild(linha);
@@ -24,27 +24,44 @@ for (let linePos = 0; linePos < 5; linePos += 1) { //  Gera a tabela
   }
 }
 
-window.onload = function colorSelector() { //  Seleciona a cor preta como padrão ao carregar a pagina
-  colorSelected[0].className += ' selected';
+function colorGenerator (){ // Gera cores aleatorias.
+  let r = Math.random() * 255;
+  let g = Math.random() * 255;
+  let b = Math.random() * 255;
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+const black = document.querySelectorAll('.color')[0];
+const red = document.querySelectorAll('.color')[1];
+const blue = document.querySelectorAll('.color')[2];
+const green = document.querySelectorAll('.color')[3];
+
+black.style.backgroundColor = 'black';
+red.style.backgroundColor = colorGenerator();
+blue.style.backgroundColor = colorGenerator();
+green.style.backgroundColor = colorGenerator();
+
+window.onload = function colorSelector() { //  Seleciona a cor preta como padrão ao carregar a pagina.
+  black.className += ' selected';
 };
 
-function changeSelected(select) { //  Seleciona o pixel que vai ser pintado
+function changeSelected(select) { //  Seleciona o pixel que vai ser pintado.
   const selected = document.querySelector('.selected');
   selected.classList.remove('selected');
   select.className = select.className + ' selected';
 }
 
-colorPalette.addEventListener('click', function changeSelectedColor(event) { //  Troca a cor selecionada para pintar
+colorPalette.addEventListener('click', function changeSelectedColor(event) { //  Troca a cor selecionada para pintar.
   const index = event.target;
   changeSelected(index);
 });
 
-pixelBoard.addEventListener('click', function changePixelToSelectedColor(event) { //  Troca a cor do pixel para a cor selecionada
+pixelBoard.addEventListener('click', function changePixelToSelectedColor(event) { //  Troca a cor do pixel para a cor selecionada.
   const index = event.target;
   index.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
 });
 
-buttonElement.addEventListener('click', function pixelsCleaner() { // Limpador de Tela
+buttonElement.addEventListener('click', function pixelsCleaner() { // Limpador de Tela.
   for (let screenCleaner = 0; screenCleaner < pixelSelected.length; screenCleaner += 1) {
     pixelSelected[screenCleaner].style.background = 'white';
   }
