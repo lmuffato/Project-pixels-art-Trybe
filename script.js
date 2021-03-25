150229f395fd9b4c82b185f53b392070eb0ee742
Eicke Yumoto <eickeyumoto@gmail.com>
2021-03-25 10:55:40 -0300

    ramdom colors

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-window.onload = document.querySelector('.color').classList.add('selected');
-
+const palette = document.querySelectorAll('.color');
 const black = document.getElementsByClassName('color')[0];
 const blue = document.getElementsByClassName('color')[1];
 const red = document.getElementsByClassName('color')[2];
 const green = document.getElementsByClassName('color')[3];
+const createPixel = document.createElement('div');
+const selected = 'color selected';
+const pixelBoard = document.querySelector('#pixel-board');
+
+function ramdomColor() {
+  for (let index = 0; index < palette.length; index += 1) {
+    const color1 = Math.floor(Math.random() * 255);
+    const color2 = Math.floor(Math.random() * 255);
+    const color3 = Math.floor(Math.random() * 255);
+    const randomColors = `rgb(${color1},${color2},${color3})`;
+    palette[index].style.backgroundColor = randomColors;
+    document.querySelector('.color').classList.add('selected');
+    document.querySelector('.color').style.backgroundColor = 'black';
+  }
+}
+
+ramdomColor();
+
+function generatePixels(userSize) {
+  for (let index = 0; index < userSize.length; index += 1) {
+    pixelBoard.appendChild(createPixel);
+  }
+}
+
+generatePixels(5);
 
 function blackSelected() {
-  black.className = 'color selected';
+  black.className = selected;
   blue.className = 'color';
   red.className = 'color';
   green.className = 'color';
@@ -16,7 +40,7 @@ black.addEventListener('click', blackSelected);
 
 function blueSelected() {
   black.className = 'color';
-  blue.className = 'color selected';
+  blue.className = selected;
   red.className = 'color';
   green.className = 'color';
 }
@@ -26,7 +50,7 @@ blue.addEventListener('click', blueSelected);
 function redSelected() {
   black.className = 'color';
   blue.className = 'color';
-  red.className = 'color selected';
+  red.className = selected;
   green.className = 'color';
 }
 red.addEventListener('click', redSelected);
@@ -35,7 +59,7 @@ function greenSelected() {
   black.className = 'color';
   blue.className = 'color';
   red.className = 'color';
-  green.className = 'color selected';
+  green.className = selected;
 }
 
 green.addEventListener('click', greenSelected);
