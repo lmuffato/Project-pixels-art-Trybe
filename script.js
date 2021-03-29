224c38a625b50f2f7000a211e37199596cdf910b
Rodrigo Grande <rogrande0496@gmail.com>
2021-03-28 21:33:33 -0300

    completa requisito 7

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const board = document.getElementById('pixel-board');
-const startingColor = document.querySelector('.color');
+const colors = document.getElementsByClassName('color');
+const color1 = document.querySelectorAll('.color')[0];
 
 function pixelBoard(size) {
   for (let i = 1; i <= size; i += 1) {
@@ -14,6 +15,18 @@ function pixelBoard(size) {
   }
 }
 
-startingColor.classList.add('selected');
+function colorSelection() {
+  color1.className = 'color selected';
 
+  for (let i = 0; i < colors.length; i += 1) {
+    colors[i].addEventListener('click', (e) => {
+      for (let j = 0; j < colors.length; j += 1) {
+        colors[j].className = 'color';
+      }
+      e.target.className = 'color selected';
+    });
+  }
+}
+
+colorSelection();
 pixelBoard(5);
