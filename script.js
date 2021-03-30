435cb77a99cb8444e0760fccdbcc3ce5cfb93c0f
Akirakiraa <andretaki@2gmail.com>
2021-03-30 02:31:59 -0300

    selected colors

--- script.js
+++ script.js
@@ -1,51 +1,89 @@
-const colors = ['black','blue','red','yellow'];
-const pixels = document.querySelectorAll('.pixel');
-const clearBtn = document.getElementById('clear-board');
-const pixelPalette = document.getElementById('color-palette')
-const pixelBoard = document.getElementById('pixel-board');
-const colorSel = document.querySelector('.selected')
-const paint = document.querySelector('.selected')
-const allPixels =document.querySelectorAll('.pixel')
+const paleta = document.querySelector('#color-palette');
+paleta.addEventListener('click', function (event) {
+  if (event.target.className !== 'color selected') {
+    const paletaSelecionada = document.querySelector('.selected');
+    event.target.className = 'color selected';
+    paletaSelecionada.className = 'color';
+  }
+});
+
+let paletas = document.querySelectorAll('.color');
+let pixels = document.querySelector('#pixel-board');
+pixels.addEventListener('click', function (event) {
+  for (let i = 0; i < paletas.length; i++) {
+    if (paletas[i].className === 'color selected') {
+      event.target.style.backgroundColor = window.getComputedStyle(paletas[i], null).getPropertyValue('background-color')
+    }
+  }
+})
+
+let pixelsList = document.querySelectorAll('.pixel');
+let botaoLimpeza = document.querySelector('#clear-board');
+botaoLimpeza.addEventListener('click', function () {
+  for (let i = 0; i < pixelsList.length; i++) {
+    pixelsList[i].style.backgroundColor = 'white'
+  }
+})
 
 
-
-//Chamando a criação das paletas
-window.onload = createPalette(colors),createBoard();
-//Criacao da paleta
-function createPalette(colors){
-    for (let pos = 0; pos < 4;pos++){
-        const pPixel = document.createElement('div');
-        pPixel.className = 'color';
-        if(colors[pos] === 'black'){
-            pPixel.className = 'color selected';
+let botaoVQV = document.querySelector("#generate-board")
+botaoVQV.addEventListener('click', function() {
+  
+  let quant = document.querySelector("#board-size");
+  let receptora = document.querySelector("body > div:nth-child(5)")
+  receptora.id = 'receptora'
+  let newPixels = document.createElement('div')
+  receptora.appendChild(newPixels);
+  newPixels.id = 'pixel-board';
+  
+  
+  if (quant.value !== ""){
+    document.querySelector('#pixel-board').parentNode.removeChild(document.querySelector('#pixel-board'));
+    if (quant.value < 50 && quant.value > 5){
+          for (let i = 1; i<= quant.value; i++){
+            let linhas = document.createElement('div');
+            newPixels.appendChild(linhas)
+            linhas.className = 'new-divTableRow'
+            for (let j = 1; j <= quant.value; j++){
+              let colunas = document.createElement('div');
+              linhas.appendChild(colunas);
+              colunas.className = 'new-pixel';
+            }
+          }
+    }else{
+      if (quant.value <= 5){
+        for (let i = 1; i<= 5; i++){
+          let linhas = document.createElement('div');
+          newPixels.appendChild(linhas)
+          linhas.className = 'new-divTableRow'
+          for (let j = 1; j <= 5; j++){
+            let colunas = document.createElement('div');
+            linhas.appendChild(colunas);
+            colunas.className = 'new-pixel';
+          }
         }
-        //pPixel (pixels da paleta) vao receber no seu style a strintg de cores colors criada na linha 1
-        pPixel.style.backgroundColor = colors[pos];
-        //chama o container das paletas de cores e adiciona o pPixel (uma div que indica o pixels  da paleta)
-        document.getElementById("color-palette").appendChild(pPixel);
-        
+      }else{
+        for (let i = 1; i<= 50; i++){
+          let linhas = document.createElement('div');
+          newPixels.appendChild(linhas)
+          linhas.className = 'new-divTableRow'
+          for (let j = 1; j <= 50; j++){
+            let colunas = document.createElement('div');
+            linhas.appendChild(colunas);
+            colunas.className = 'new-pixel';
+          }
+        }
+      }
     }
-}
-//Criacao do quadro
-function createBoard(){
-    for (let pos = 0; pos < 25;pos++){
-        const pPixel = document.createElement('div');
-        //Os pixels do quadro possuem a classe chamada de pixel
-        pPixel.className = 'pixel';
-    document.getElementById("pixel-board").appendChild(pPixel); //Aqui eu tive que usar o getElementById pra localizar a tag que seria o pai ,dessa forma usei o append child pra dizer que "paletapixel" seria filha dessa
-
-        
+  }else{
+    alert("Board inválido!");
+  }
+    
+  newPixels.addEventListener('click', function (event) {
+    for (let i = 0; i < paletas.length; i++) {
+      if (paletas[i].className === 'color selected') {
+        event.target.style.backgroundColor = window.getComputedStyle(paletas[i], null).getPropertyValue('background-color')
+      }
     }
-}
-
-clearBtn.addEventListener('click',() => {
-    allPixels.forEach(function(){
-        pPixel.style.backgroundColor = 'white';
-    });
-});
-
-pixelPalette.addEventListener('click',(event) => {
-    let check = event.target.className;
-        colorSel.className = check.className('color')
-        event.target.className ='color selected';
-});
+  })
+})
